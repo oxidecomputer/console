137f4dabfc501ca91e444ed09616ed6d54bc7091
Paula Lavalle <lavalle.paula@gmail.com>
2021-02-08 11:36:02 -0800

    WIP Add gray and green colors. Add spacing helper.

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,15 +1,22 @@
-import { Theme } from './theme'
+import { Theme, ThemeColors } from './theme'
+
+const colors: ThemeColors = {
+  gray50: '#FAFAFA',
+  gray100: '#F4F4F5',
+  gray200: '#E4E4E7',
+  gray300: '#D4D4D8',
+  gray400: '#A1A1AA',
+  gray500: '#71717A',
+  gray600: '#52525B',
+  gray700: '#3F3F46',
+  gray800: '#27272A',
+  gray900: '#18181B',
+
+  green50: '#F0FDF4',
+  green500: '#22C55E',
+}
 
 export const defaultTheme: Theme = {
-  colors: {
-    primary: '#2BBB85',
-    secondary: '#A6A6A6',
-    background: '#0E1518',
-    navBackground: '#1D2225',
-  },
-  spacing: {
-    small: '0.5em',
-    medium: '1em',
-    large: '2em',
-  },
+  colors: colors,
+  spacing: (size) => `${size * 0.4}rem`,
 }
--- packages/theme/src/theme.ts
+++ packages/theme/src/theme.ts
@@ -1,14 +1,39 @@
-export interface Theme {
-  colors: {
-    primary: string
-    secondary: string
-    background: string
-    navBackground: string
-  }
+export interface ThemeColors {
+  gray50: string
+  gray100: string
+  gray200: string
+  gray300: string
+  gray400: string
+  gray500: string
+  gray600: string
+  gray700: string
+  gray800: string
+  gray900: string
 
-  spacing: {
-    small: string
-    medium: string
-    large: string
-  }
+  green50: string
+  green500: string
+}
+
+export interface Theme {
+  colors: ThemeColors
+  spacing: (
+    size:
+      | 1
+      | 2
+      | 3
+      | 4
+      | 5
+      | 6
+      | 8
+      | 10
+      | 12
+      | 16
+      | 20
+      | 24
+      | 32
+      | 40
+      | 48
+      | 56
+      | 64
+  ) => void
 }
