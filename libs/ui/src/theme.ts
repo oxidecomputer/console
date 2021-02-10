f22e64a4a6c840b27e065b96be1a8290d45dfd75
Paula Lavalle <paula.lavalle@formidable.com>
2021-02-10 11:32:25 -0800

    Add colors from figma

--- libs/ui/src/theme.ts
+++ libs/ui/src/theme.ts
@@ -13,8 +13,49 @@ export interface ThemeColors {
   gray800: string
   gray900: string
 
+  red50: string
+  red100: string
+  red200: string
+  red300: string
+  red400: string
+  red500: string
+  red600: string
+  red700: string
+  red800: string
+  red900: string
+
+  yellow50: string
+  yellow100: string
+  yellow200: string
+  yellow300: string
+  yellow400: string
+  yellow500: string
+  yellow600: string
+  yellow700: string
+  yellow800: string
+  yellow900: string
+
+  blue50: string
+  blue100: string
+  blue200: string
+  blue300: string
+  blue400: string
+  blue500: string
+  blue600: string
+  blue700: string
+  blue800: string
+  blue900: string
+
   green50: string
+  green100: string
+  green200: string
+  green300: string
+  green400: string
   green500: string
+  green600: string
+  green700: string
+  green800: string
+  green900: string
 }
 
 type SizingMultiplier =
@@ -69,8 +110,49 @@ export const colors: ThemeColors = {
   gray800: 'hsl(240, 4%, 16%)',
   gray900: 'hsl(240, 6%, 10%)',
 
-  green50: 'hsl(138, 76%, 97%)',
-  green500: 'hsl(142, 71%, 45%)',
+  red50: '#fdf8f9',
+  red100: '#fbf1f3',
+  red200: '#f5dbe1',
+  red300: '#efc6cf',
+  red400: '#e49bab',
+  red500: '#D87087',
+  red600: '#c2657a',
+  red700: '#a25465',
+  red800: '#824351',
+  red900: '#6a3742',
+
+  yellow50: '#fefdf8',
+  yellow100: '#fdfaf1',
+  yellow200: '#fbf4dd',
+  yellow300: '#f9edc8',
+  yellow400: '#f4df9f',
+  yellow500: '#EFD176',
+  yellow600: '#d7bc6a',
+  yellow700: '#b39d59',
+  yellow800: '#8f7d47',
+  yellow900: '#75663a',
+
+  blue50: '#f9f9fe',
+  blue100: '#f2f4fe',
+  blue200: '#dfe3fc',
+  blue300: '#cbd2fa',
+  blue400: '#a4b1f6',
+  blue500: '#7D8FF2',
+  blue600: '#7181da',
+  blue700: '#5e6bb6',
+  blue800: '#4b5691',
+  blue900: '#3d4677',
+
+  green50: '#f8fdfa',
+  green100: '#f1fbf5',
+  green200: '#dcf4e6',
+  green300: '#c7edd7',
+  green400: '#9de0ba',
+  green500: '#73D29C',
+  green600: '#68bd8c',
+  green700: '#569e75',
+  green800: '#457e5e',
+  green900: '#38674c',
 }
 
 type BaseTheme = Omit<Theme, 'colors'>
