496f6ef2c0f0368ff60f4304b30860cb680e3363
David Mills <daegren@users.noreply.github.com>
2021-02-10 15:31:18 -0500

    lint workflow (#12)
    Adds a GH workflow to run linting, build and testing on a pull request. Also fixing some tests and wrapping components in test with a theme provider

---
+++ libs/ui/src/test-utils.tsx
@@ -0,0 +1,16 @@
+import React, { FC, ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './theme'
+
+const Providers: FC = ({ children }) => (
+  <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+)
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'queries'>
+) => render(ui, { wrapper: Providers, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
