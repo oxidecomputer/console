eb59b1c0d2ae852046fa2fe0936f82164481f1a0
David Mills <dave@1morepx.com>
2021-02-10 15:22:56 -0500

    fix tests, wrap all renders with a theme provider

--- libs/ui/src/lib/text/Text.spec.tsx
+++ libs/ui/src/lib/text/Text.spec.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import { render } from '../../test-utils'
 
-import Text from './Text';
+import Text from './Text'
 
 describe('Text', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Text />);
-    expect(baseElement).toBeTruthy();
-  });
-});
+    const { baseElement } = render(<Text />)
+    expect(baseElement).toBeTruthy()
+  })
+})
