660f2b68f862d47dd67b4f4c786fb7afd7f9dedc
Paula Lavalle <paula.lavalle@formidable.com>
2021-02-11 11:15:59 -0800

    Add dashboard and server SVGs to assets. WIP Icon component.

--- libs/ui/src/lib/icon/Icon.tsx
+++ libs/ui/src/lib/icon/Icon.tsx
@@ -1,20 +1,38 @@
 import React from 'react'
-
 import styled from 'styled-components'
 
-/* eslint-disable-next-line */
-export interface IconProps {}
+import DASHBOARD from '../../assets/dashboard.svg'
+import SERVER from '../../assets/server.svg'
 
-const StyledIcon = styled.div`
-  color: pink;
+const icons = {
+  dashboard: DASHBOARD,
+  server: SERVER,
+}
+export interface IconProps {
+  name: 'dashboard' | 'server'
+  size: 'base'
+}
+
+const StyledIcon = styled.span`
+  width: ${(props: IconProps) => props.size};
+
+  > svg {
+    width: 100%;
+    height: auto;
+
+    fill: inherit;
+  }
 `
 
-export function Icon(props: IconProps) {
-  return (
-    <StyledIcon>
-      <h1>Welcome to Icon!</h1>
-    </StyledIcon>
-  )
+export function Icon<IconProps>({ name, ...props }) {
+  if (name) {
+    return <StyledIcon {...props}>{icons[name]}</StyledIcon>
+  }
+  return null
 }
 
 export default Icon
+
+Icon.defaultProps = {
+  size: 'base',
+}
