f1938fd4428cadc61b6b78596066e3e06636bde0
Paula Lavalle <paula.lavalle@formidable.com>
2021-02-12 10:10:07 -0800

    Add more icons

--- libs/ui/src/lib/icon/Icon.tsx
+++ libs/ui/src/lib/icon/Icon.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 
+import { ReactComponent as AddIcon } from '../../assets/add.svg'
+import { ReactComponent as DarkModeIcon } from '../../assets/dark-mode.svg'
 import { ReactComponent as DashboardIcon } from '../../assets/dashboard.svg'
 import { ReactComponent as ExtensionsIcon } from '../../assets/extensions.svg'
+import { ReactComponent as FilesIcon } from '../../assets/files.svg'
 import { ReactComponent as FoldersIcon } from '../../assets/folders.svg'
 import { ReactComponent as OpenFolderIcon } from '../../assets/open-folder.svg'
+import { ReactComponent as SearchIcon } from '../../assets/search.svg'
 import { ReactComponent as ServerIcon } from '../../assets/server.svg'
+import { ReactComponent as UsersIcon } from '../../assets/users.svg'
 
 export const icons = {
+  add: <AddIcon />,
+  darkMode: <DarkModeIcon />,
   dashboard: <DashboardIcon />,
   extensions: <ExtensionsIcon />,
+  files: <FilesIcon />,
   folders: <FoldersIcon />,
   openFolder: <OpenFolderIcon />,
+  search: <SearchIcon />,
   server: <ServerIcon />,
+  users: <UsersIcon />,
 }
 
 type SizeProp = 'base'
@@ -64,6 +74,6 @@ export const Icon = ({ name, ...props }: IconProps) => {
 export default Icon
 
 Icon.defaultProps = {
-  color: '#000',
+  color: '#f00',
   size: 'base',
 }
