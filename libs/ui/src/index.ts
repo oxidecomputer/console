1b4c66ea77aa66dabe76beb292676decbac1ea3d
Paula Lavalle <paula.lavalle@formidable.com>
2021-02-18 10:56:49 -0800

    Add breadcrumbs. Add comment about storybook controls.

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/breadcrumbs/Breadcrumbs'
 export * from './lib/button/Button'
 export * from './lib/icon/Icon'
 export * from './lib/text/Text'
---
+++ libs/ui/src/lib/breadcrumbs/Breadcrumbs.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import { Breadcrumbs, BreadcrumbsProps } from './Breadcrumbs'
+
+// Follow https://github.com/storybookjs/storybook/issues/12078
+// for allowing better controls for objects
+export default {
+  component: Breadcrumbs,
+  title: 'Components/Breadcrumbs',
+  argTypes: {
+    data: {
+      control: { type: 'object' },
+    },
+  },
+} as Meta
+
+const Template: Story<BreadcrumbsProps> = (args) => <Breadcrumbs {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  data: [
+    { href: '/', label: 'Home' },
+    { href: '/first', label: 'First page' },
+    { href: '/second', label: 'Second page' },
+  ],
+}
