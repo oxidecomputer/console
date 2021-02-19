bba18ca2f244120b24d123b02a1dd49db931e87d
Paula Lavalle <paula.lavalle@formidable.com>
2021-02-19 12:48:54 -0800

    lint

--- libs/ui/.storybook/preview.js
+++ libs/ui/.storybook/preview.js
@@ -3,18 +3,20 @@ import { DocsContainer } from '@storybook/addon-docs/blocks'
 import { ThemeProvider } from 'styled-components'
 import { colorPalette, defaultTheme, GlobalStyle } from '@oxide/theme'
 
-const values = (colors) =>
+const getBackgroundColors = (colors) =>
   Object.keys(colors).map((key) => {
     return { name: key, value: colors[key] }
   })
 
+// Global Parameters
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   backgrounds: {
     // Bug: background selector is broken for stories written in MDX
     // See: https://github.com/storybookjs/storybook/issues/7978#issuecomment-726797915
     default: 'gray900',
-    values: values(colorPalette),
+    // Add background color selector with these color options
+    values: getBackgroundColors(colorPalette),
   },
   docs: {
     // Default background does not apply to docs
