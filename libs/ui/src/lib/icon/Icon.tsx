4e0a07d6c2027742c8ae6ce037c12a9c39077fab
Jared Volpe <plainspace@gmail.com>
2021-02-24 11:42:48 -0500

    Add cpu, mem, storage icons

--- libs/ui/src/lib/icon/Icon.tsx
+++ libs/ui/src/lib/icon/Icon.tsx
@@ -6,10 +6,12 @@ import { ReactComponent as BookmarkIcon } from '../../assets/bookmark.svg'
 import { ReactComponent as ChevronIcon } from '../../assets/chevron.svg'
 import { ReactComponent as CloseIcon } from '../../assets/close.svg'
 import { ReactComponent as CommandIcon } from '../../assets/command.svg'
+import { ReactComponent as CpuIcon } from '../../assets/cpu.svg'
 import { ReactComponent as DashboardIcon } from '../../assets/dashboard.svg'
 import { ReactComponent as FileIcon } from '../../assets/file.svg'
 import { ReactComponent as InstanceIcon } from '../../assets/instance.svg'
 import { ReactComponent as InstancesIcon } from '../../assets/instances.svg'
+import { ReactComponent as MemoryIcon } from '../../assets/memory.svg'
 import { ReactComponent as MessageIcon } from '../../assets/message.svg'
 import { ReactComponent as MoreIcon } from '../../assets/more.svg'
 import { ReactComponent as OrganizationIcon } from '../../assets/org.svg'
@@ -20,6 +22,7 @@ import { ReactComponent as PulseIcon } from '../../assets/pulse.svg'
 import { ReactComponent as RackIcon } from '../../assets/rack.svg'
 import { ReactComponent as ResourcesIcon } from '../../assets/resources.svg'
 import { ReactComponent as SearchIcon } from '../../assets/search.svg'
+import { ReactComponent as StorageIcon } from '../../assets/storage.svg'
 import { ReactComponent as SupportIcon } from '../../assets/support.svg'
 import { ReactComponent as ThemeIcon } from '../../assets/theme.svg'
 import { ReactComponent as UserIcon } from '../../assets/user.svg'
@@ -30,10 +33,12 @@ export const icons = {
   chevron: <ChevronIcon />,
   close: <CloseIcon />,
   command: <CommandIcon />,
+  cpu: <CpuIcon />,
   dashboard: <DashboardIcon />,
   file: <FileIcon />,
   instance: <InstanceIcon />,
   instances: <InstancesIcon />,
+  memory: <MemoryIcon />,
   message: <MessageIcon />,
   more: <MoreIcon />,
   organization: <OrganizationIcon />,
@@ -44,6 +49,7 @@ export const icons = {
   rack: <RackIcon />,
   resources: <ResourcesIcon />,
   search: <SearchIcon />,
+  storage: <StorageIcon />,
   support: <SupportIcon />,
   theme: <ThemeIcon />,
   user: <UserIcon />,
