12b20c3ab40539f5aadd3cfa01a35e7fe9360419
Paula Lavalle <paula.lavalle@formidable.com>
2021-02-25 09:04:20 -0800

    Task: Add GlobalStyle to Storybook (#29)
    * Add GlobalStyle to storybook
    
    * lint
    
    * Override table classes so docs page dark theme works correctly
    
    * use tsconfig-paths-webpack-plugin in manager webpack config to find referenced projects
    
    Co-authored-by: David Mills <dave@1morepx.com>

--- libs/ui/.storybook/preview.js
+++ libs/ui/.storybook/preview.js
@@ -1,31 +1,56 @@
 import React from 'react'
 import { DocsContainer } from '@storybook/addon-docs/blocks'
-import { ThemeProvider } from 'styled-components'
-import { colorPalette, defaultTheme } from '@oxide/theme'
+import { darkUI } from './theme'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { colorPalette, defaultTheme, GlobalStyle } from '@oxide/theme'
 
-// FIXME: What background colors will be most valuable to designers? Presumably all the background colors used for each light/dark mode?
-const values = (colors) =>
+// Bug: https://github.com/storybookjs/storybook/issues/14029
+const DocsStyleOverrides = createGlobalStyle`
+  table.sbdocs tr {
+    background-color: inherit;
+    color: inherit;
+  }
+
+  table.sbdocs tr:nth-of-type(2n) {
+    background-color: inherit;
+    color: inherit;
+  }
+`
+
+const getBackgroundColors = (colors) =>
   Object.keys(colors).map((key) => {
     return { name: key, value: colors[key] }
   })
 
+// Global Parameters
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   backgrounds: {
-    values: values(colorPalette),
+    // Bug: background selector is broken for stories written in MDX
+    // See: https://github.com/storybookjs/storybook/issues/7978#issuecomment-726797915
+    default: 'gray900',
+    // Add background color selector with these color options
+    values: getBackgroundColors(colorPalette),
   },
   docs: {
+    // Default background does not apply to docs
     container: ({ children, context }) => (
       <DocsContainer context={context}>
-        <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+        <ThemeProvider theme={defaultTheme}>
+          <GlobalStyle />
+          <DocsStyleOverrides />
+          {children}
+        </ThemeProvider>
       </DocsContainer>
     ),
+    theme: darkUI,
   },
 }
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Story />
     </ThemeProvider>
   ),
