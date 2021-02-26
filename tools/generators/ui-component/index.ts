48d0244d9ace57bd80d38068e4f5c37036b70af3
David Mills <dave@1morepx.com>
2021-02-26 12:08:32 -0500

    replace `@testing-library/react` with local `test-utils` which includes ThemeProvider

--- tools/generators/ui-component/index.ts
+++ tools/generators/ui-component/index.ts
@@ -1,5 +1,6 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit'
 import { componentGenerator, componentStoryGenerator } from '@nrwl/react'
+import * as path from 'path'
 
 const REPLACE_EXPORT_REGEX = /^export function (\w*)(\(props: \w*\)) {$/gm
 
@@ -21,6 +22,28 @@ const replaceExportFunction = async (host: Tree) => {
           `Could not replace exports for ${file.path}. Error: ${e.message}`
         )
       }
+    })
+  )
+}
+
+const SPEC_FILE_REGEX = /spec\.tsx$/
+const replaceTestingUtils = async (host: Tree) => {
+  const files = new Set(
+    host.listChanges().filter((file) => SPEC_FILE_REGEX.test(file.path))
+  )
+
+  const pathToTestUtils = path.resolve('./libs/ui/src/test-utils')
+  return Promise.all(
+    Array.from(files).map(async (file) => {
+      const content = file.content.toString()
+      // use '.' to get full path relative to current dir, then '..' to remove the file from the path
+      const filePath = path.resolve('.', file.path, '..')
+      const relativePath = path.relative(filePath, pathToTestUtils)
+
+      host.write(
+        file.path,
+        content.replace('@testling-library/react', relativePath)
+      )
     })
   )
 }
@@ -53,6 +76,7 @@ export default async (host: Tree, schema: any) => {
   if (!schema.skipStories) {
     await generateStoryForComponent(host)
   }
+  await replaceTestingUtils(host)
   await replaceExportFunction(host)
   await formatFiles(host)
   return () => {
