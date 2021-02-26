ec3f2b29da7071d090aa36037b610dacb3f8a7ca
David Mills <dave@1morepx.com>
2021-02-26 10:57:05 -0500

    Mock *.svg files in test so they still work

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,8 @@
 const nxPreset = require('@nrwl/jest/preset')
 
-module.exports = { ...nxPreset }
+module.exports = {
+  ...nxPreset,
+  moduleNameMapper: {
+    '\\.(svg)$': '<rootDir>/__mocks__/svgMock.tsx',
+  },
+}
---
+++ libs/ui/__mocks__/svgMock.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default 'svg-file-stub'
+export const ReactComponent = ({ title }) => <div>Icon: {title}</div>
