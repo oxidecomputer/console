374b913d3377b6ec7bac153eef1f9cd31d1f258a
Paula Lavalle <paula.lavalle@formidable.com>
2021-03-02 11:50:25 -0800

    Task: Update Button to match design system (#47)
    * Use mdx for button stories
    
    * Fix story bug. Add design notes.
    
    * Update button sizes
    
    * Add subtle variant. Update docs.
    
    * Disable buttons are focusable. Update outline colors.
    
    * Update focus colors
    
    * Disabled buttons should still be focusable. Update cursor. Beef up docs.

--- libs/ui/src/lib/button/Button.tsx
+++ libs/ui/src/lib/button/Button.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 import styled, { css } from 'styled-components'
 
-export const buttonSizes = ['xs', 'sm', 'base', 'lg'] as const
-export const variants = ['solid', 'outline', 'ghost', 'link'] as const
+export const buttonSizes = ['xs', 'sm', 'base', 'lg', 'xl'] as const
+export const variants = ['solid', 'subtle', 'outline', 'ghost', 'link'] as const
 
 type ButtonSize = typeof buttonSizes[number]
 type Variant = typeof variants[number]
+
+const sizes = {
+  xs: { fontSize: 3, lineHeight: 1 / 0.75, padding: [2, 3] }, // total height: 32px
+  sm: { fontSize: 3.5, lineHeight: 1.25 / 0.875, padding: [2, 3] }, // total height: 36px
+  base: { fontSize: 3.5, lineHeight: 1.25 / 0.875, padding: [2.5, 4] }, // total height: 40px
+  lg: { fontSize: 4, lineHeight: 1.5, padding: [2.25, 4.5] }, // total height: 42px
+  xl: { fontSize: 4, lineHeight: 1.5, padding: [3, 6] }, // total height: 48px
+}
 
 export interface ButtonProps {
   /**
@@ -24,45 +32,62 @@ export interface ButtonProps {
 }
 
 const getSizeStyles = (size: ButtonSize) => {
-  switch (size) {
-    case 'xs':
-      return css`
-        font-size: 0.75rem;
-        line-height: ${1 / 0.75};
-        padding: ${({ theme }) => `${theme.spacing(1)} ${theme.spacing(3)}`};
-      `
-    case 'sm':
-      return css`
-        font-size: 0.75rem;
-        line-height: ${1 / 0.75};
-        padding: ${({ theme }) => `${theme.spacing(2)} ${theme.spacing(3)}`};
-      `
-    case 'lg':
-      return css`
-        font-size: 1.125rem;
-        line-height: ${1.75 / 1.125};
-        padding: ${({ theme }) => `${theme.spacing(3)} ${theme.spacing(6)}`};
-      `
-    case 'base':
-    default:
-      return css`
-        font-size: 0.875rem;
-        line-height: ${1.25 / 0.875};
-        padding: ${({ theme }) => `${theme.spacing(2)} ${theme.spacing(3)}`};
-      `
+  const getPadding = (x, y) => {
+    return ({ theme }) => `${theme.spacing(x)} ${theme.spacing(y)}`
   }
+
+  const buttonSize = sizes[size]
+
+  if (buttonSize) {
+    return css`
+      font-size: ${({ theme }) => theme.spacing(buttonSize.fontSize)};
+      line-height: ${buttonSize.lineHeight};
+      padding: ${getPadding(buttonSize.padding[0], buttonSize.padding[1])};
+    `
+  }
+
+  return css``
 }
 
 const getVariantStyles = (variant: Variant) => {
   switch (variant) {
-    case 'outline':
+    case 'subtle':
       return css`
-        background-color: ${({ theme }) => theme.themeColors.white};
-        box-shadow: inset 0 0 0 1px ${({ theme }) => theme.themeColors.green500};
-        color: ${({ theme }) => theme.themeColors.green500};
+        background-color: hsla(154, 50%, 50%, 0.16);
+        color: ${({ theme }) => theme.themeColors.green400};
 
         &:hover:not(:disabled):not([disabled]) {
-          background-color: ${({ theme }) => theme.themeColors.green50};
+          background-color: hsla(153, 63%, 69%, 0.24);
+        }
+
+        &:focus {
+          background-color: hsla(154, 63%, 56%, 0.16);
+          box-shadow: 0 0 0 ${({ theme }) => theme.spacing(1)}
+            rgba(65, 192, 136, 0.16);
+        }
+
+        &:disabled,
+        [disabled] {
+          opacity: 0.4;
+        }
+      `
+    case 'outline':
+      return css`
+        background-color: hsla(146, 51%, 64%, 0.16);
+        box-shadow: inset 0 0 0 1px ${({ theme }) => theme.themeColors.green500};
+        color: ${({ theme }) => theme.themeColors.green400};
+
+        &:hover:not(:disabled):not([disabled]) {
+          background-color: hsla(146, 51%, 64%, 0.08);
+        }
+
+        &:focus {
+          box-shadow: ${({ theme }) => `
+            inset 0 0 0 1px ${theme.themeColors.green500},
+            0px 1px ${theme.spacing(0.5)} rgba(0, 0, 0, 0.05),
+            0px 0px 0px ${theme.spacing(0.5)} hsla(146, 51%, 64%, 0.16),
+            0px 0px 0px ${theme.spacing(1)} hsla(145, 39%, 57%, 0.16)
+            `};
         }
 
         &:disabled,
@@ -73,10 +98,10 @@ const getVariantStyles = (variant: Variant) => {
     case 'ghost':
       return css`
         background-color: transparent;
-        color: ${({ theme }) => theme.themeColors.green500};
+        color: ${({ theme }) => theme.themeColors.green400};
 
-        &:hover:not(:disabled):not([disabled]) {
-          background-color: ${({ theme }) => theme.themeColors.green50};
+        &:focus {
+          background-color: hsla(146, 51%, 64%, 0.16);
         }
 
         &:disabled,
@@ -90,7 +115,8 @@ const getVariantStyles = (variant: Variant) => {
         background-color: transparent;
         color: ${({ theme }) => theme.themeColors.green500};
 
-        &:hover:not(:disabled):not([disabled]) {
+        &:hover:not(:disabled):not([disabled]),
+        &:focus {
           text-decoration: underline;
         }
 
@@ -106,7 +132,14 @@ const getVariantStyles = (variant: Variant) => {
         color: ${({ theme }) => theme.themeColors.green50};
 
         &:hover:not(:disabled):not([disabled]) {
-          background-color: ${({ theme }) => theme.themeColors.green600};
+          background-color: ${({ theme }) => theme.themeColors.green700};
+        }
+
+        &:focus {
+          box-shadow: ${({ theme }) => `
+            0 1px ${theme.spacing(0.5)} rgba(0, 0, 0, 0.05),
+            0 0 0 ${theme.spacing(0.5)} ${theme.themeColors.gray800},
+            0 0 0 ${theme.spacing(1)} ${theme.themeColors.green600}`};
         }
 
         &:disabled,
@@ -124,6 +157,11 @@ const StyledButton = styled.button<ButtonProps>`
 
   ${(props) => getSizeStyles(props.size)};
   ${(props) => getVariantStyles(props.variant)};
+
+  &:disabled,
+  [disabled] {
+    cursor: not-allowed;
+  }
 `
 
 export const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
