6425fa1960a3e990de0e928e0ee20d9f82754827
David Mills <dave@1morepx.com>
2021-03-08 21:33:00 -0500

    generate stories for a list of variants

--- libs/storybook-helpers/src/lib/story-builder.spec.tsx
+++ libs/storybook-helpers/src/lib/story-builder.spec.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import type { Story } from '@storybook/react'
 import { storyBuilder } from './story-builder'
 
+const enumValues = ['one', 'two', 'three'] as const
+type EnumValue = typeof enumValues[number]
 interface TemplateProps {
   required: string
 
   optional?: string
+  enum?: EnumValue
 }
 
 const Template: Story<TemplateProps> = ({ required, optional }) => (
@@ -39,5 +42,21 @@ describe('Story Builders', () => {
 
     expect(story.storyName).toBe('Test Story')
     expect(story.args).toMatchObject({ required: 'test' })
+  })
+
+  describe('storiesFor', () => {
+    it('takes a list of variants for a given prop', () => {
+      const stories = builder.storiesFor({ enum: enumValues })
+
+      expect(Object.keys(stories)).toHaveLength(3)
+      expect(stories.one.storyName).toBe('one')
+      expect(stories.one.args).toMatchObject({ enum: 'one' })
+
+      expect(stories.two.storyName).toBe('two')
+      expect(stories.two.args).toMatchObject({ enum: 'two' })
+
+      expect(stories.three.storyName).toBe('three')
+      expect(stories.three.args).toMatchObject({ enum: 'three' })
+    })
   })
 })
--- libs/storybook-helpers/src/lib/story-builder.ts
+++ libs/storybook-helpers/src/lib/story-builder.ts
@@ -1,15 +1,22 @@
 import type { PropsWithChildren } from 'react'
 import type { Args, Story } from '@storybook/react'
 
+type StoriesOf<A> = Record<string, Story<A>>
+
+type Variants<B> = readonly B[]
+
+type VariantMap<A> = Partial<Record<keyof A, Variants<A[keyof A]>>>
+
 interface StoryBuilder<A = Args> {
   build: (name: string, args?: Partial<A>) => Story<A>
+  storiesFor: (values: VariantMap<A>) => StoriesOf<A>
 }
 
 export const storyBuilder = <A extends Args>(
   baseStory: Story<A>,
   defaultArgs: PropsWithChildren<A>
-): StoryBuilder<A> => ({
-  build: (name, args = {}) => {
+): StoryBuilder<A> => {
+  const build: StoryBuilder<A>['build'] = (name, args = {}) => {
     const story: Story<A> = baseStory.bind({})
     story.storyName = name
     story.args = {
@@ -18,5 +25,28 @@ export const storyBuilder = <A extends Args>(
     }
 
     return story
-  },
-})
+  }
+
+  const storiesFor: StoryBuilder<A>['storiesFor'] = (values) => {
+    const args = Object.keys(values) as Array<keyof typeof values>
+
+    return args.reduce((rest, arg) => {
+      const argValues = values[arg]
+      return {
+        ...rest,
+        ...argValues.reduce((rest, v) => {
+          const args = { [arg]: v } as Partial<A>
+          return {
+            ...rest,
+            [v]: build(v, args),
+          }
+        }, {}),
+      }
+    }, {})
+  }
+
+  return {
+    build,
+    storiesFor,
+  }
+}
