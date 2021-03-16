3128411c4260f8b9314c56b94219f9de15b8738b
David Crespo <crespo.dm@gmail.com>
2021-03-16 11:44:28 -0400

    clean up storybook config

--- libs/ui/.storybook/webpack.config.js
+++ libs/ui/.storybook/webpack.config.js
@@ -1,20 +1,15 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
-const rootWebpackConfig = require('../../../.storybook/webpack.config')
 /**
  * Export a function. Accept the base config as the only param.
  *
  * @param {Parameters<typeof rootWebpackConfig>[0]} options
  */
 module.exports = async ({ config, mode }) => {
-  config = await rootWebpackConfig({ config, mode })
-
   const tsPaths = new TsconfigPathsPlugin({
     configFile: './tsconfig.json',
   })
 
-  config.resolve.plugins
-    ? config.resolve.plugins.push(tsPaths)
-    : (config.resolve.plugins = [tsPaths])
+  config.resolve.plugins = [tsPaths]
 
   // Found this here: https://github.com/nrwl/nx/issues/2859
   // And copied the part of the solution that made it work
