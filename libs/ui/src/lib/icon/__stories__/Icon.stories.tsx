893a7676def5626494df38924256fef6df802db9
Jared <jared@oxidecomputer.com>
2021-03-17 16:06:10 -0400

    Add more icons (#91)
    * Update chevron name
    
    * Update profile icon
    
    * Add arrowRight
    
    * Update style and layout of icons on story
    
    * Add trash icon
    
    * Add more icons
    
    * Use rotate instead of adding many icons
    
    * Update svg title
    
    * Update arrow const name
    
    * Prettier
    
    * Add notification icon and rename pen

--- libs/ui/src/lib/icon/__stories__/Icon.stories.tsx
+++ libs/ui/src/lib/icon/__stories__/Icon.stories.tsx
@@ -6,7 +6,7 @@ const Template: Story<IconProps> = (args) => <Icon {...args} />
 
 export const Default = Template.bind({})
 Default.storyName = 'Icon'
-Default.args = { name: 'bookmark', color: 'green500' }
+Default.args = { name: 'bookmark', color: 'green500', size: '3xl' }
 
 export const Rotate = Template.bind({})
 Rotate.args = { name: 'chevron', rotate: '45deg' }
