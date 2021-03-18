b09a01ced83ea27cdd1b396bdfb4739aaeda9b7e
David Mills <dave@1morepx.com>
2021-03-18 17:47:09 -0400

    proper typing

--- libs/ui/src/lib/layout/sidebar-navigation/operation-list/OperationList.stories.tsx
+++ libs/ui/src/lib/layout/sidebar-navigation/operation-list/OperationList.stories.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { OperationList } from './OperationList'
-import type { Story } from '@storybook/react'
+import type { Meta } from '@storybook/react'
 
 export default {
   component: OperationList,
   title: 'Layout/Sidebar Navigation/Operation List',
   decorators: [
-    (Story: Story) => (
+    (Story) => (
       <div style={{ maxWidth: '180px' }}>
         <Story />
       </div>
     ),
   ],
-}
+} as Meta
 
 export const primary = () => {
   return <OperationList />
