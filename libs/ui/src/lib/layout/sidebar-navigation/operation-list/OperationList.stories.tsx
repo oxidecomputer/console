8aac71b0bf4c7de8332c407e20ffef09a55d3419
David Mills <dave@1morepx.com>
2021-03-18 16:08:28 -0400

    operation-list

--- libs/ui/src/lib/layout/sidebar-navigation/operation-list/OperationList.stories.tsx
+++ libs/ui/src/lib/layout/sidebar-navigation/operation-list/OperationList.stories.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { OperationList } from './OperationList'
+import type { Story } from '@storybook/react'
 
 export default {
   component: OperationList,
   title: 'Layout/Sidebar Navigation/Operation List',
   decorators: [
-    (Story) => (
+    (Story: Story) => (
       <div style={{ maxWidth: '180px' }}>
         <Story />
       </div>
