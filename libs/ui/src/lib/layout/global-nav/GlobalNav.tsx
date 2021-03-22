29489ee48b533477a3232b00adca5ae9229c440d
David Crespo <crespo.dm@gmail.com>
2021-03-22 02:03:41 -0400

    redo global nav with tailwind

--- libs/ui/src/lib/layout/global-nav/GlobalNav.tsx
+++ libs/ui/src/lib/layout/global-nav/GlobalNav.tsx
@@ -1,72 +1,32 @@
 import React, { FC } from 'react'
 
-import styled from 'styled-components'
+import tw, { styled } from 'twin.macro'
 
-import { Text } from '../../text/Text'
 import { Icon } from '../../icon/Icon'
-
-/* eslint-disable-next-line */
-export interface GlobalNavProps {}
-
-const StyledGlobalNav = styled.div`
-  align-items: center;
-  display: flex;
-  width: 100%;
-  height: ${({ theme }) => theme.spacing(14)};
-  justify-content: right;
-  ${({ theme }) => theme.paddingY(4)}
-  ${({ theme }) => theme.paddingX(6)}
-`
-
-const StyledIcon = styled(Icon)`
-  width: ${({ theme }) => theme.spacing(6)};
-`
-
-const Link = styled.a`
-  margin-left: ${({ theme }) => theme.spacing(6)};
-  display: inline-flex;
-`
-
-const StyledText = styled(Text).attrs({
-  size: 'sm',
-  font: 'mono',
-  weight: 400,
-})`
-  text-transform: uppercase;
-  margin-left: ${({ theme }) => theme.spacing(8)};
-  margin-right: ${({ theme }) => theme.spacing(5)};
-`
 
 const TickBar = styled.div`
   // placeholder: url-encoded contents of warning-filled.svg
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' role='img' aria-labelledby='icon-warning-filled'%3E%3Ctitle id='icon-warning-filled'%3EWarning%3C/title%3E%3Cpath fill='gray' fill-rule='evenodd' clip-rule='evenodd' d='M0 19.75L10 .25l10 19.5H0zM11 14V6.5H9V14h2zm0 3.5v-2H9v2h2z'/%3E%3C/svg%3E%0A");
-  background-position: center right;
-  background-repeat: repeat-x;
   background-size: ${({ theme }) => theme.spacing(5)};
-  height: ${({ theme }) => theme.spacing(5)};
-  width: 100%;
+  ${tw`h-5 w-full bg-right bg-repeat-x`}
 `
+
+const Link = tw.a`ml-6 inline-flex font-mono text-sm uppercase`
 
 const Button = styled.button.attrs({ type: 'button' })`
-  background-color: transparent;
-  border: none;
-  color: ${({ theme }) => theme.themeColors.gray100};
-  display: inline-flex;
-  margin-left: ${({ theme }) => theme.spacing(6)};
-  padding: 0;
-
-  &:hover {
-    background-color: transparent;
-    color: ${({ theme }) => theme.themeColors.green500};
-  }
+  ${tw`border-0 inline-flex ml-6 p-0 hover:text-green-500`}
 `
 
-export const GlobalNav: FC<GlobalNavProps> = () => {
+const StyledIcon = styled(Icon)`
+  ${tw`w-6`}
+`
+
+export const GlobalNav: FC = () => {
   return (
-    <StyledGlobalNav>
+    <div tw="flex w-full h-14 items-center py-4 px-6">
       <TickBar />
-      <Link href="#">
-        <StyledText>Feedback?</StyledText>
+      <Link href="#" tw="ml-10 mr-4">
+        Feedback?
       </Link>
       <Button>
         <StyledIcon name="theme" />
@@ -84,6 +44,6 @@ export const GlobalNav: FC<GlobalNavProps> = () => {
         {/* placeholder for profile photo? where would we get that */}
         <StyledIcon name="profile" />
       </Button>
-    </StyledGlobalNav>
+    </div>
   )
 }
