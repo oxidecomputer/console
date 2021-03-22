f7b438d443824fb0045d6788917305e0f51913b1
David Mills <dave@1morepx.com>
2021-03-22 12:44:26 -0400

    fixing typing of SVG files

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,6 @@ type SvgrComponent = React.FC<React.SVGProps<SVGSVGElement>>
 
 declare module '*.svg' {
   const ReactComponent: SvgrComponent
-  const content: string
 
-  export { ReactComponent }
-  export default content
+  export default ReactComponent
 }
