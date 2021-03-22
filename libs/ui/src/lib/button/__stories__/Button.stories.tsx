e0ec889661f76cfc3215153b5d473e5dddb93873
David Mills <dave@1morepx.com>
2021-03-22 17:47:38 -0400

    linter fix

--- libs/ui/src/lib/button/__stories__/Button.stories.tsx
+++ libs/ui/src/lib/button/__stories__/Button.stories.tsx
@@ -1,4 +1,5 @@
-import React, { PropsWithChildren } from 'react'
+import type { PropsWithChildren } from 'react'
+import React from 'react'
 import type { Story } from '@storybook/react'
 import { Button, buttonSizes, variants } from '../Button'
 import type { ButtonProps, ButtonSize, Variant } from '../Button'
