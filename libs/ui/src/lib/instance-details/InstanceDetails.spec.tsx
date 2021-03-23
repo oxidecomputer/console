f543c54b1b9bc3c4755288b97135beeec327ffca
David Crespo <crespo.dm@gmail.com>
2021-03-23 17:18:59 -0400

    typechecking

--- libs/ui/src/lib/instance-details/InstanceDetails.spec.tsx
+++ libs/ui/src/lib/instance-details/InstanceDetails.spec.tsx
@@ -5,7 +5,16 @@ import { InstanceDetails } from './InstanceDetails'
 
 describe('InstanceDetails', () => {
   it('should render successfully', () => {
-    const { container } = render(<InstanceDetails />)
+    const { container } = render(
+      <InstanceDetails
+        cpu="2"
+        memory="8 GB"
+        storage="100 GB"
+        vm={{ os: 'Debian', version: '9.12', arch: 'x64' }}
+        hostname="db1.useast1.inst"
+        ip="10.10.16.7"
+      />
+    )
     expect(container).toBeTruthy()
   })
 })
