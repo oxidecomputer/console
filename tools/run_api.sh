1f68b3ab714396a31ac52a244c029192cb5c309c
David Crespo <crespo.dm@gmail.com>
2021-03-26 14:48:26 -0400

    poll instead of sleeping, separate tmux commands

--- tools/run_api.sh
+++ tools/run_api.sh
@@ -7,7 +7,19 @@
 
 # this script assumes it's being run from inside the omicron repo
 
-tmux \
-  new-session  'cargo run --bin=nexus -- examples/config.toml' \; \
-  split-window 'sleep 0.5 && cargo run --bin=sled_agent -- $(uuidgen) 127.0.0.1:12345 127.0.0.1:12221' \; \
-  split-window 'sleep 1 && ../console/tools/populate_omicron_data.sh; zsh'
+wait_for_up() {
+  while ! echo exit | nc localhost "$1"; do sleep 0.1; done
+}
+
+tmux new -d -s omicron-console
+
+tmux send-keys "cargo run --bin=nexus -- examples/config.toml" Enter
+wait_for_up 12220
+
+tmux split-window "cargo run --bin=sled_agent -- $(uuidgen) 127.0.0.1:12345 127.0.0.1:12221"
+wait_for_up 12221
+
+# zsh is to prevent the pane from closing when the script is done 
+tmux split-window "../console/tools/populate_omicron_data.sh; zsh"
+
+tmux attach-session -t omicron-console
