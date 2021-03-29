bc29afd0c463953527bda31771cb34bc357414f2
Paula Lavalle <paula.lavalle@formidable.com>
2021-03-29 13:38:20 -0700

    Match sidebar scrollbar behavior to stripe example

--- apps/web-console/src/app-layout/AppLayout.tsx
+++ apps/web-console/src/app-layout/AppLayout.tsx
@@ -38,49 +38,42 @@ const Wrapper = styled.div`
   grid-template-columns: 14rem auto;
   grid-template-rows: 3.5rem auto;
   grid-template-areas:
-    'sidebar topnav'
+    'logo globalnav'
     'sidebar content';
   height: 100vh;
+
+  background-color: ${({ theme }) => theme.color('gray800')};
 `
 
 const Sidebar = styled.div`
   grid-area: sidebar;
+  margin-left: ${({ theme }) => theme.spacing(4)};
+  margin-right: ${({ theme }) => theme.spacing(4)};
+  margin-bottom: ${({ theme }) => theme.spacing(6)};
   overflow: auto;
-  padding-left: ${({ theme }) => theme.spacing(4)};
-  padding-right: ${({ theme }) => theme.spacing(4)};
 
-  background-color: ${({ theme }) => theme.color('gray800')};
+  ${({ theme }) => theme.spaceBetweenY(6)};
 `
 
 const WordmarkWrapper = styled.div`
-  z-index: 1;
-  position: sticky;
-  top: 0;
-
-  display: flex;
   align-items: center;
+  grid-area: logo;
+  display: flex;
   height: ${({ theme }) => theme.spacing(14)};
-
-  background-color: ${({ theme }) => theme.color('gray800')};
-`
-
-const SidebarLists = styled.div`
-  ${({ theme }) => theme.spaceBetweenY(6)};
+  padding-left: ${({ theme }) => theme.spacing(4)};
 `
 
 const Content = styled.main`
   grid-area: content;
   overflow: auto;
-
   padding: ${({ theme }) => `${theme.spacing(2)} ${theme.spacing(6)};`};
+
+  background-color: ${({ theme }) => theme.color('gray900')};
 `
 
 const GlobalNavContainer = styled.header`
-  position: sticky;
-  top: 0;
-
   align-self: center;
-  grid-area: topnav;
+  grid-area: globalnav;
   padding: ${({ theme }) => `${theme.spacing(4)} ${theme.spacing(6)};`};
 
   background-color: ${({ theme }) => theme.color('gray900')};
@@ -89,18 +82,16 @@ const GlobalNavContainer = styled.header`
 export default ({ children }: AppLayoutProps) => {
   return (
     <Wrapper>
+      <WordmarkWrapper>
+        <Wordmark />
+      </WordmarkWrapper>
       <Sidebar>
-        <WordmarkWrapper>
-          <Wordmark />
-        </WordmarkWrapper>
-        <SidebarLists>
-          <ProjectList
-            projects={projects}
-            onProjectSelect={() => null}
-            onProjectCreate={() => null}
-          />
-          <OperationList />
-        </SidebarLists>
+        <ProjectList
+          projects={projects}
+          onProjectSelect={() => null}
+          onProjectCreate={() => null}
+        />
+        <OperationList />
       </Sidebar>
       <GlobalNavContainer>
         <GlobalNav />
