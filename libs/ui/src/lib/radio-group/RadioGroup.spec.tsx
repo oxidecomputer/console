ad530ca8b19ad39ce387e8fd7d4337f8cb774de9
Paula Lavalle <paula.lavalle@formidable.com>
2021-04-02 14:50:56 -0700

    Update test

--- libs/ui/src/lib/radio-group/RadioGroup.spec.tsx
+++ libs/ui/src/lib/radio-group/RadioGroup.spec.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 import { render } from '../../test-utils'
 
 import { RadioGroup } from './RadioGroup'
+import { RadioField } from '../radio-field/RadioField'
 
 describe('RadioGroup', () => {
   it('should render successfully', () => {
-    const { container } = render(<RadioGroup />)
+    const { container } = render(
+      <RadioGroup legend="Notifications" name="notifications-settings">
+        <RadioField value="all">Everything</RadioField>
+        <RadioField value="none">Nothing</RadioField>
+      </RadioGroup>
+    )
     expect(container).toBeTruthy()
   })
 })
