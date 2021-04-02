a959e28f3882f72617d22805dd517e933205684a
David Crespo <david-crespo@users.noreply.github.com>
2021-04-02 15:28:12 -0400

    Basic instance create page (#188)
    * basic instance create page
    
    * extract PageHeader component
    
    * bump the icon size

--- apps/web-console/src/pages/instance/InstanceCreatePage.tsx
+++ apps/web-console/src/pages/instance/InstanceCreatePage.tsx
@@ -1,33 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { useParams } from 'react-router-dom'
-
-import { Breadcrumbs, PageHeader, TextWithIcon } from '@oxide/ui'
+import { Breadcrumbs, Icon, PageHeader, TextWithIcon } from '@oxide/ui'
 
 const Title = styled(TextWithIcon).attrs({
   text: { variant: 'title', as: 'h1' },
   icon: { name: 'instances' },
-})``
+})`
+  ${Icon} {
+    font-size: ${({ theme }) => theme.spacing(8)};
+    margin-right: ${({ theme }) => theme.spacing(3)};
+  }
+`
 
 const breadcrumbs = [
   { href: '/', label: 'Maze War' },
-  { href: '/first', label: 'Projects' },
-  { href: '/second', label: 'prod-online' },
-  { label: 'Instances' },
+  { href: '/projects', label: 'Projects' },
+  { href: '/projects/prod-online', label: 'prod-online' },
+  { href: '/projects/prod-online/instances', label: 'Instances' },
+  { label: 'Create Instance' },
 ]
 
-type Params = {
-  projectName: string
-}
-
 const InstancesPage = () => {
-  const { projectName } = useParams<Params>()
   return (
     <>
       <Breadcrumbs data={breadcrumbs} />
       <PageHeader>
-        <Title>Instances for Project: {projectName}</Title>
+        <Title>Create Instance</Title>
       </PageHeader>
       <p style={{ marginTop: '2rem' }}>There is nothing here, sorry</p>
     </>
---
+++ libs/ui/src/lib/PageHeader.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const PageHeader = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+
+  margin-top: ${({ theme }) => theme.spacing(2)};
+`
