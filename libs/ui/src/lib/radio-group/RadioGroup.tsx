bb97d9f2c410557dd8777a83eac87e84175e1681
Paula Lavalle <paula.lavalle@formidable.com>
2021-04-02 14:19:28 -0700

    WIP Add RadioGroup stories

--- libs/ui/src/lib/radio-group/RadioGroup.tsx
+++ libs/ui/src/lib/radio-group/RadioGroup.tsx
@@ -1,19 +1,81 @@
-import type { FC } from 'react'
+import type { FC, ReactEventHandler, ReactElement } from 'react'
 import React from 'react'
 
 import styled from 'styled-components'
 
-/* eslint-disable-next-line */
-export interface RadioGroupProps {}
+import type { RadioFieldProps } from '../radio-field/RadioField'
+import { Text } from '../text/Text'
 
-const StyledRadioGroup = styled.div`
-  color: pink;
+export interface RadioGroupProps {
+  children: Array<ReactElement<RadioFieldProps>>
+  /**
+   * The default or initially checked option
+   */
+  defaultValue?: string
+  /**
+   * Required description of radio buttons. Helpful for accessibility.
+   */
+  legend: string
+  /**
+   * Required name to pass to Radio children
+   */
+  name: string
+  onChange?: ReactEventHandler
+  required?: boolean
+}
+
+const StyledFieldset = styled.fieldset`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+
+  margin: 0;
+  border: 0;
+
+  /* Once Safari supports 'gap' with flex layouts, this can be removed */
+  /* gap: ${({ theme }) => theme.spacing(5)}; */
+  & > * + * {
+    margin-right: ${({ theme }) => theme.spacing(5)};
+  }
+`
+const StyledLegend = styled(Text).attrs({
+  as: 'legend',
+  color: 'white',
+  size: 'lg',
+})`
+  display: block;
+  margin-bottom: ${({ theme }) => theme.spacing(4)};
+  width: 100%;
 `
 
-export const RadioGroup: FC<RadioGroupProps> = (props) => {
+export const RadioGroup: FC<RadioGroupProps> = ({
+  children,
+  defaultValue = null,
+  legend,
+  name,
+}) => {
+  // Set checked of each child based on state
+  const [checked, setChecked] = React.useState(defaultValue)
+  const handleChange = React.useCallback(
+    (event) => {
+      setChecked(event.target.value)
+    },
+    [setChecked]
+  )
   return (
-    <StyledRadioGroup>
-      <h1>Welcome to RadioGroup!</h1>
-    </StyledRadioGroup>
+    <StyledFieldset>
+      <StyledLegend>{legend}</StyledLegend>
+      {React.Children.map(children, (radioField) => {
+        const isChecked = checked === radioField.props.value
+        // Render controlled inputs with checked state
+        // Add name prop to group them semantically and add event listener
+        return React.cloneElement(radioField, {
+          name: name,
+          checked: isChecked,
+          onChange: handleChange,
+        })
+      })}
+    </StyledFieldset>
   )
 }
--- libs/ui/src/lib/radio-group/__stories__/RadioGroup.stories.tsx
+++ libs/ui/src/lib/radio-group/__stories__/RadioGroup.stories.tsx
@@ -1,15 +1,47 @@
 import React from 'react'
+import type { Story } from '@storybook/react'
+
 import type { RadioGroupProps } from '../RadioGroup'
 import { RadioGroup } from '../RadioGroup'
+import { RadioField } from '../../radio-field/RadioField'
 
-export default {
-  component: RadioGroup,
-  title: 'RadioGroup',
+const Template: Story<RadioGroupProps> = (args) => (
+  <RadioGroup {...args}>
+    <RadioField
+      value="notify"
+      hint="Get notified when someones posts a comment on a posting."
+    >
+      Comments
+    </RadioField>
+    <RadioField value="do-not-notify" hint="Turn off notifications.">
+      Nothing
+    </RadioField>
+  </RadioGroup>
+)
+
+const CardTemplate: Story<RadioGroupProps> = (args) => (
+  <RadioGroup {...args}>
+    <RadioField variant="card" value="100">
+      100 GB
+    </RadioField>
+    <RadioField variant="card" value="200">
+      200 GB
+    </RadioField>
+    <RadioField variant="card" value="300">
+      300 GB
+    </RadioField>
+  </RadioGroup>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  legend: 'Notifications',
+  name: 'group1',
 }
 
-export const Default = () => {
-  /* eslint-disable-next-line */
-  const props: RadioGroupProps = {}
-
-  return <RadioGroup />
+export const withInitialChecked = CardTemplate.bind({})
+withInitialChecked.args = {
+  legend: 'Add storage',
+  name: 'group2',
+  defaultValue: '100',
 }
