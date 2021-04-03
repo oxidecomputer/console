ec74309258140bc2688740cfa80c0177411163d4
Jess Frazelle <jess@oxide.computer>
2021-04-02 20:50:16 -0700

    update script
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- tools/gcp_instance_startup_script.sh
+++ tools/gcp_instance_startup_script.sh
@@ -6,6 +6,10 @@ set -o pipefail
 export TAILSCALE_MACHINE_KEY=$(cat /etc/tailscale/machine_key | tr -d '[:space:]')
 tailscale up --authkey=${TAILSCALE_MACHINE_KEY}
 
+# Get the IP of the instance.
+# This _should_ be the second column.
+export TAILSCALE_IP=$(tailscale status | grep console-git-BRANCH_NAME | awk '{print $2}')
+
 # Run the console.
 docker run -d \
 	--restart=always \
@@ -13,3 +17,5 @@ docker run -d \
 	-p 0.0.0.0:80:80 \
 	--entrypoint=sled_agent \
 	ghcr.io/oxidecomputer/console:BRANCH_NAME
+
+# Let's make sure cloudflare knows the right IP for this instance.
