b0793f78fcb3498120850c24bff737523c9f56c3
Jess Frazelle <jess@oxide.computer>
2021-04-02 22:48:59 -0700

    use new image
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- tools/create_gcp_instance.sh
+++ tools/create_gcp_instance.sh
@@ -20,7 +20,7 @@ gcloud compute instances create "$INSTANCE_NAME" \
 	--description="Machine automatically generated from branch ${BRANCH_NAME} of the oxidecomputer/console git repo." \
 	--hostname="${INSTANCE_NAME}.internal.oxide.computer" \
 	--zone=$ZONE \
-	--image=packer-1617421321 \
+	--image=packer-1617428227 \
 	--maintenance-policy=TERMINATE \
 	--restart-on-failure \
 	--machine-type=$INSTANCE_TYPE \
