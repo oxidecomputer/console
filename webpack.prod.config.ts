d5feae0d7a8d227807b9790d0cba3c3246c902f0
David Crespo <crespo.dm@gmail.com>
2021-04-06 09:11:36 -0400

    don't nuke function names during minification

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -1,10 +1,11 @@
 import path from 'path'
 import webpack from 'webpack'
-import { merge } from 'webpack-merge'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 import sharedConfig from './webpack.shared.config'
 
-const config = merge<webpack.Configuration>(sharedConfig, {
+const config = {
+  ...sharedConfig,
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist/apps/web-console'),
@@ -12,11 +13,24 @@ const config = merge<webpack.Configuration>(sharedConfig, {
     publicPath: '',
   },
   plugins: [
+    ...sharedConfig.plugins,
     new webpack.DefinePlugin({
       'process.env.API_URL': JSON.stringify(process.env.API_URL),
     }),
     new CleanWebpackPlugin(),
   ],
-})
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          // HACK: temporary fix to make API hook relying on function name work
+          // in production. remove to decrease bundle size once hook is improved
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
+}
 
 export default config
