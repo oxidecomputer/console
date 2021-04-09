7a1f6acc698194269d5eeef482398f4fbe5d834f
Paula Lavalle <paula.lavalle@formidable.com>
2021-04-09 16:31:09 -0700

    Add react-popper for usePopper hook instead

--- libs/ui/src/lib/tooltip/Tooltip.tsx
+++ libs/ui/src/lib/tooltip/Tooltip.tsx
@@ -1,21 +1,40 @@
 import type { FC } from 'react'
-import React from 'react'
+import React, { useState } from 'react'
 
 import styled from 'styled-components'
-import Tippy from '@tippyjs/react'
+import { usePopper } from 'react-popper'
 
 export interface TooltipProps {
   size?: 'sm' | 'lg'
 }
 
-const Wrapper = styled.div``
+const TooltipButton = styled.button``
+
+const TooltipContent = styled.div``
 
 export const Tooltip: FC<TooltipProps> = () => {
+  const [referenceElement, setReferenceElement] = useState(null)
+  const [popperElement, setPopperElement] = useState(null)
+  const [arrowElement, setArrowElement] = useState(null)
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
+  })
+
   return (
-    <Wrapper>
-      <Tippy content="Hello" visible>
-        <button>trigger</button>
-      </Tippy>
-    </Wrapper>
+    <>
+      <TooltipButton type="button" ref={setReferenceElement}>
+        Reference element
+      </TooltipButton>
+
+      <TooltipContent
+        ref={setPopperElement}
+        style={styles.popper}
+        {...attributes.popper}
+        role="tooltip"
+      >
+        Popper element
+        <div ref={setArrowElement} style={styles.arrow} />
+      </TooltipContent>
+    </>
   )
 }
