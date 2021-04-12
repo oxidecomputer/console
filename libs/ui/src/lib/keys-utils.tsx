0b247af4408a9de8fb8c399545c68bfc2efb362d
Paula Lavalle <paula.lavalle@formidable.com>
2021-04-12 11:10:54 -0700

    keyCode is deprecated. Use key instead

--- libs/ui/src/lib/keys-utils.tsx
+++ libs/ui/src/lib/keys-utils.tsx
@@ -1,12 +1,13 @@
 // For easy reference in components
 export const KEYS = {
-  end: 35,
-  home: 36,
-  left: 37,
-  up: 38,
-  right: 39,
-  down: 40,
-  delete: 46,
-  enter: 13,
-  space: 32,
+  end: 'End',
+  home: 'Home',
+  left: 'ArrowLeft' || 'Left',
+  up: 'ArrowUp' || 'Up',
+  right: 'ArrowRight' || 'Right',
+  down: 'ArrowDown' || 'Down',
+  delete: 'Del' || 'Delete',
+  enter: 'Enter',
+  space: 'Spacebar' || ' ',
+  escape: 'Escape' || 'Esc',
 }
