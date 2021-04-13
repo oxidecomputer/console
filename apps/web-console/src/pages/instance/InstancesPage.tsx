1b9c19fa103130bbc89a074dff56edee7ad1e79d
David Crespo <crespo.dm@gmail.com>
2021-04-13 13:18:46 -0400

    experiment with create and update on same page with SWR mutate

--- apps/web-console/src/pages/instance/InstancesPage.tsx
+++ apps/web-console/src/pages/instance/InstancesPage.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import { v4 as uuid } from 'uuid'
 
 import { useParams, Link } from 'react-router-dom'
 
-import { useApi } from '@oxide/api'
-import { Breadcrumbs, PageHeader, TextWithIcon } from '@oxide/ui'
+import { useApi, api } from '@oxide/api'
+import { Breadcrumbs, Button, PageHeader, TextWithIcon } from '@oxide/ui'
 import { useBreadcrumbs } from '../../hooks'
 
 const Title = styled(TextWithIcon).attrs({
   text: { variant: 'title', as: 'h1' },
   icon: { name: 'instances' },
 })``
+
+const createInstance = (projectName: string) =>
+  api.apiProjectInstancesPost({
+    projectName,
+    apiInstanceCreateParams: {
+      bootDiskSize: 1,
+      description: `An instance in project: ${projectName}`,
+      hostname: 'oxide.com',
+      memory: 10,
+      name: `i-${uuid().substr(0, 8)}`,
+      ncpus: 2,
+    },
+  })
 
 type Params = {
   projectName: string
@@ -20,7 +34,12 @@ const InstancesPage = () => {
   const breadcrumbs = useBreadcrumbs()
 
   const { projectName } = useParams<Params>()
-  const { data } = useApi('apiProjectInstancesGet', { projectName })
+  const { data, mutate } = useApi('apiProjectInstancesGet', { projectName })
+
+  const onCreateClick = async () => {
+    await createInstance(projectName)
+    mutate()
+  }
 
   if (!data) return <div>loading</div>
 
@@ -40,6 +59,9 @@ const InstancesPage = () => {
         ))}
         {data.items.length === 0 && <p>No instances!</p>}
       </ul>
+      <Button onClick={onCreateClick} style={{ marginTop: '1rem' }}>
+        Create instance
+      </Button>
     </>
   )
 }
