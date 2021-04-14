f3fa147f5c72f6b9cdf45dcd892b8b1d01b2ccba
David Crespo <crespo.dm@gmail.com>
2021-04-14 17:42:45 -0400

    fix stop_api.sh leaving cockroach running

--- tools/populate_omicron_data.sh
+++ tools/populate_omicron_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # generally you don't want to run this manually. it's meant to be used by
 # start_api.sh. if you do run it manually, note that it's meant to be run
 # from inside the omicron repo and it assumes nexus and sled agent are running
--- tools/stop_api.sh
+++ tools/stop_api.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cockroach quit --insecure --url postgresql://127.0.0.1:32221
 tmux kill-session -t omicron-console
