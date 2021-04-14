c1390f652de21166ba9a138b2ac0b4a2d8e95e32
David Crespo <david-crespo@users.noreply.github.com>
2021-04-14 11:01:28 -0400

    Page generator (#243)
    * page generator
    
    * readme

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
-const ROOT_DIR = 'libs/ui/src/lib'
+const UI_ROOT_DIR = 'libs/ui/src/lib'
+const PAGES_ROOT_DIR = 'apps/web-console/src/pages'
 
 module.exports = (plop) => {
   plop.setGenerator('ui-component', {
@@ -14,7 +15,7 @@ module.exports = (plop) => {
       {
         type: 'input',
         name: 'subdir',
-        message: `Directory within ${ROOT_DIR}? (leave blank for root)`,
+        message: `Directory within ${UI_ROOT_DIR}? (leave blank for root)`,
       },
       {
         type: 'confirm',
@@ -24,7 +25,7 @@ module.exports = (plop) => {
       },
     ],
     actions: (args) => {
-      const dir = path.join(ROOT_DIR, args.subdir)
+      const dir = path.join(UI_ROOT_DIR, args.subdir)
       const testUtilsRelPath = path.relative(
         path.join('./', args.subdir, 'just-need', 'two-things-here'),
         './test-utils'
@@ -62,6 +63,37 @@ module.exports = (plop) => {
           templateFile: 'plop-templates/component-stories.hbs',
           data: { dir },
           skip: () => !args.mdx && 'MDX skipped, using CSF',
+        },
+      ]
+    },
+  })
+
+  plop.setGenerator('page', {
+    description: 'Generate a Page component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Page name? (minus the "Page")',
+      },
+      {
+        type: 'input',
+        name: 'subdir',
+        message: `Directory within ${PAGES_ROOT_DIR}? (leave blank for root)`,
+      },
+    ],
+    actions: (args) => {
+      const dir = path.join(PAGES_ROOT_DIR, args.subdir)
+      const hooksRelPath = path.relative(
+        path.join('./', args.subdir, 'Page.tsx'),
+        './hooks'
+      )
+      return [
+        {
+          type: 'add',
+          path: '{{dir}}/{{name}}Page.tsx',
+          templateFile: 'plop-templates/page.hbs',
+          data: { dir, hooksRelPath },
         },
       ]
     },
