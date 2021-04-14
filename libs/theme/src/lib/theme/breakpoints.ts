a7cefab1ec671689c369ad29e7b2501d9f6f3ecc
David Mills <dave.mills@formidable.com>
2021-04-14 13:31:13 -0400

    breakpoint helper

---
+++ libs/theme/src/lib/theme/breakpoints.ts
@@ -0,0 +1,34 @@
+import { css } from 'styled-components'
+import type { Breakpoint, BreakpointHelper } from '../types'
+
+const breakpoints: Record<Breakpoint, number> = {
+  base: 320,
+  sm: 640,
+  lg: 1024,
+  xl: 1280,
+  '2xl': 1536,
+}
+
+/**
+ * This helper works simlarly to the `css` helper from `styled-components` however, it first takes a breakpoint and only applies the styles for that given breakpoint.
+ *
+ * ## Usage
+ *
+ * ```typescript
+ * const Container = styled.div`
+ *   display: flex;
+ *   flex-direction: column;
+ *
+ *   ${({ theme }) => theme.breakpoint('sm')`
+ *     flex-direction: row;
+ *   `}
+ * `
+ * ```
+ * @param breakpoint The breakpoint to use for the media query
+ */
+export const breakpoint: BreakpointHelper = (breakpoint) => (first, ...args) =>
+  css`
+    @media (min-width: ${breakpoints[breakpoint]}px) {
+      ${css(first, ...args)}
+    }
+  `
