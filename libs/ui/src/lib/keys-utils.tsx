3494307f20926e54225b9d3e8e48f0d12fbfcbd1
Paula Lavalle <paula.lavalle@formidable.com>
2021-04-14 12:13:54 -0700

    Remove support for older browsers

--- libs/ui/src/lib/keys-utils.tsx
+++ libs/ui/src/lib/keys-utils.tsx
@@ -1,13 +1,14 @@
 // For easy reference in components
+// See list of values here: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
 export const KEYS = {
   end: 'End',
   home: 'Home',
-  left: 'ArrowLeft' || 'Left',
-  up: 'ArrowUp' || 'Up',
-  right: 'ArrowRight' || 'Right',
-  down: 'ArrowDown' || 'Down',
-  delete: 'Del' || 'Delete',
+  left: 'ArrowLeft',
+  up: 'ArrowUp',
+  right: 'ArrowRight',
+  down: 'ArrowDown',
+  delete: 'Delete',
   enter: 'Enter',
-  space: 'Spacebar' || ' ',
-  escape: 'Escape' || 'Esc',
+  space: ' ',
+  escape: 'Escape',
 }
