2f09cd269540f12800547be766a48e70f9279f54
David Crespo <crespo.dm@gmail.com>
2021-04-15 10:36:01 -0400

    move demo over to create form

--- apps/web-console/src/pages/instance/InstanceCreatePage.tsx
+++ apps/web-console/src/pages/instance/InstanceCreatePage.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import type { ChangeEvent } from 'react'
+import { useEffect } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useParams, useHistory } from 'react-router-dom'
 
-import { Breadcrumbs, Icon, PageHeader, TextWithIcon } from '@oxide/ui'
+import {
+  Breadcrumbs,
+  Button,
+  Icon,
+  PageHeader,
+  NumberField,
+  TextField,
+  TextWithIcon,
+} from '@oxide/ui'
 import { useBreadcrumbs } from '../../hooks'
+import { api, useAsync } from '@oxide/api'
 
 const Title = styled(TextWithIcon).attrs({
   text: { variant: 'title', as: 'h1' },
@@ -14,15 +26,84 @@ const Title = styled(TextWithIcon).attrs({
   }
 `
 
+const Box = styled.div`
+  margin-top: 1rem;
+  border: 1px solid white;
+  padding: 1rem;
+`
+
+type Params = {
+  projectName: string
+}
+
 const InstancesPage = () => {
   const breadcrumbs = useBreadcrumbs()
+
+  const history = useHistory()
+  const { projectName } = useParams<Params>()
+
+  // form state
+  const [instanceName, setInstanceName] = useState('')
+  const [ncpus, setNcpus] = useState(1)
+
+  const createInstance = useAsync(() =>
+    api.apiProjectInstancesPost({
+      projectName,
+      apiInstanceCreateParams: {
+        bootDiskSize: 1,
+        description: `An instance in project: ${projectName}`,
+        hostname: 'oxide.com',
+        memory: 10,
+        name: instanceName,
+        ncpus,
+      },
+    })
+  )
+
+  const onCreateClick = async () => {
+    // TODO: validate client-side before attempting post
+    await createInstance.execute()
+  }
+
+  // redirect on successful post
+  useEffect(() => {
+    if (createInstance.result) {
+      history.push(`/projects/${projectName}/instances`)
+    }
+  }, [createInstance.result, history, projectName])
+
   return (
     <>
       <Breadcrumbs data={breadcrumbs} />
       <PageHeader>
         <Title>Create Instance</Title>
       </PageHeader>
-      <p style={{ marginTop: '2rem' }}>There is nothing here, sorry</p>
+      <Box>Post error: {JSON.stringify(createInstance.error)}</Box>
+      <div style={{ marginTop: '1rem' }}>
+        <TextField
+          value={instanceName}
+          required
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setInstanceName(e.target.value)
+          }
+          placeholder="db1"
+        >
+          Instance name
+        </TextField>
+      </div>
+      <div style={{ marginTop: '1rem' }}>
+        <NumberField value={ncpus} required handleChange={setNcpus}>
+          Number of CPUs
+        </NumberField>
+      </div>
+
+      <Button
+        onClick={onCreateClick}
+        style={{ marginTop: '1rem' }}
+        disabled={createInstance.pending}
+      >
+        Create instance
+      </Button>
     </>
   )
 }
--- apps/web-console/src/pages/instance/InstancesPage.tsx
+++ apps/web-console/src/pages/instance/InstancesPage.tsx
@@ -1,18 +1,10 @@
-import type { ChangeEvent } from 'react'
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { v4 as uuid } from 'uuid'
 
 import { useParams, Link } from 'react-router-dom'
 
-import { useApi, api, useAsync } from '@oxide/api'
-import {
-  Breadcrumbs,
-  Button,
-  PageHeader,
-  TextField,
-  TextWithIcon,
-} from '@oxide/ui'
+import { useApi } from '@oxide/api'
+import { Breadcrumbs, PageHeader, TextWithIcon } from '@oxide/ui'
 import { useBreadcrumbs } from '../../hooks'
 
 const Title = styled(TextWithIcon).attrs({
@@ -24,41 +16,11 @@ type Params = {
   projectName: string
 }
 
-const Box = styled.div`
-  margin-top: 1rem;
-  border: 1px solid white;
-  padding: 1rem;
-`
-
 const InstancesPage = () => {
   const breadcrumbs = useBreadcrumbs()
 
   const { projectName } = useParams<Params>()
-  const { data, mutate } = useApi('apiProjectInstancesGet', { projectName })
-
-  // form state
-  const [instanceName, setInstanceName] = useState('db1')
-  const [ncpus, setNcpus] = useState('2')
-
-  const createInstance = useAsync(() =>
-    api.apiProjectInstancesPost({
-      projectName,
-      apiInstanceCreateParams: {
-        bootDiskSize: 1,
-        description: `An instance in project: ${projectName}`,
-        hostname: 'oxide.com',
-        memory: 10,
-        name: instanceName,
-        // deliberately allow passing strings here to trigger 400s
-        ncpus: /^\d+$/.test(ncpus) ? parseInt(ncpus, 10) : ncpus,
-      },
-    })
-  )
-
-  const onCreateClick = async () => {
-    await createInstance.execute()
-    mutate()
-  }
+  const { data } = useApi('apiProjectInstancesGet', { projectName })
 
   if (!data) return <div>loading</div>
 
@@ -78,38 +40,12 @@ const InstancesPage = () => {
         ))}
         {data.items.length === 0 && <p>No instances!</p>}
       </ul>
-      <Box>Post response: {JSON.stringify(createInstance.result)}</Box>
-      <Box>Post error: {JSON.stringify(createInstance.error)}</Box>
-      <div style={{ marginTop: '1rem' }}>
-        <TextField
-          value={instanceName}
-          required
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setInstanceName(e.target.value)
-          }
-        >
-          Instance name
-        </TextField>
-      </div>
-      <div style={{ marginTop: '1rem' }}>
-        <TextField
-          value={ncpus}
-          required
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setNcpus(e.target.value)
-          }
-        >
-          Number of CPUs
-        </TextField>
-      </div>
-
-      <Button
-        onClick={onCreateClick}
+      <Link
         style={{ marginTop: '1rem' }}
-        disabled={createInstance.pending}
+        to={`/projects/${projectName}/instances/new`}
       >
         Create instance
-      </Button>
+      </Link>
     </>
   )
 }
