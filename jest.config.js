801ed0cd22f24291c79720d1909651705e826d5f
David Crespo <crespo.dm@gmail.com>
2021-04-22 14:09:51 -0400

    don't even need projects

--- jest.config.js
+++ jest.config.js
@@ -3,38 +3,13 @@ const { compilerOptions } = require('./tsconfig')
 const mapValues = (obj, f) =>
   Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, f(v)]))
 
-const libPaths = mapValues(compilerOptions.paths, (paths) => {
-  const dir = paths[0].replace('/index.ts', '')
-  return `<rootDir>/../../${dir}`
-})
-
-const moduleNameMapper = {
-  '\\.(svg)$': '<rootDir>/../../__mocks__/svgMock.tsx',
-  '\\.(css|less)$': 'identity-obj-proxy',
-  ...libPaths,
-}
+const libs = mapValues(compilerOptions.paths, ([path]) => `<rootDir>/${path}`)
 
 module.exports = {
-  projects: [
-    {
-      displayName: 'web-console',
-      rootDir: '<rootDir>/apps/web-console',
-      moduleNameMapper,
-    },
-    {
-      displayName: 'ui',
-      rootDir: '<rootDir>/libs/ui',
-      moduleNameMapper,
-    },
-    {
-      displayName: 'theme',
-      rootDir: '<rootDir>/libs/theme',
-      moduleNameMapper,
-    },
-    {
-      displayName: 'api',
-      rootDir: '<rootDir>/libs/api',
-      moduleNameMapper,
-    },
-  ],
+  moduleNameMapper: {
+    '\\.(svg)$': '<rootDir>/__mocks__/svgMock.tsx',
+    '\\.(css|less)$': 'identity-obj-proxy',
+    ...libs,
+  },
+  testPathIgnorePatterns: ['/node_modules/', 'apps/web-console-e2e'],
 }
