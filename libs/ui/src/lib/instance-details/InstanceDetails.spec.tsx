ec665e9c43a5cb612bc7d68659c13b8084ca4cb2
David Crespo <david-crespo@users.noreply.github.com>
2021-04-25 22:14:15 -0400

    use real instance data, add lib directory for test fixtures (#290)

--- libs/ui/src/lib/instance-details/InstanceDetails.spec.tsx
+++ libs/ui/src/lib/instance-details/InstanceDetails.spec.tsx
@@ -3,18 +3,13 @@ import { render } from '../../test-utils'
 
 import { InstanceDetails } from './InstanceDetails'
 
+import { instance } from '@oxide/api-mocks'
+
 describe('InstanceDetails', () => {
   it('should render successfully', () => {
-    const { container } = render(
-      <InstanceDetails
-        cpu="2"
-        memory="8 GB"
-        storage="100 GB"
-        vm={{ os: 'Debian', version: '9.12', arch: 'x64' }}
-        hostname="db1.useast1.inst"
-        ip="10.10.16.7"
-      />
-    )
-    expect(container).toBeTruthy()
+    const { getByText } = render(<InstanceDetails instance={instance} />)
+    expect(getByText('7 vCPU')).toBeTruthy()
+    expect(getByText('256 MB RAM')).toBeTruthy()
+    expect(getByText('running')).toBeTruthy()
   })
 })
