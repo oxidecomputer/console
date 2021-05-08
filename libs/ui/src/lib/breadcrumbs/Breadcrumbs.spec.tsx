e45d78c0f520744b111ce5fb6e8119a4a3239657
David Crespo <crespo.dm@gmail.com>
2021-05-07 23:11:28 -0500

    breadcrumbs tw cleanup

--- libs/ui/src/lib/breadcrumbs/Breadcrumbs.spec.tsx
+++ libs/ui/src/lib/breadcrumbs/Breadcrumbs.spec.tsx
@@ -5,7 +5,7 @@ import { Breadcrumbs } from './Breadcrumbs'
 
 describe('Breadcrumbs', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Breadcrumbs />)
+    const { baseElement } = render(<Breadcrumbs data={[]} />)
     expect(baseElement).toBeTruthy()
   })
 })
