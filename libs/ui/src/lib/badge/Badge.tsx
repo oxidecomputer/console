09fd7c264c6e76348df8b5abace321a46f34f04f
David Crespo <crespo.dm@gmail.com>
2021-05-12 12:37:00 -0500

    clean up Badge

--- libs/ui/src/lib/badge/Badge.tsx
+++ libs/ui/src/lib/badge/Badge.tsx
@@ -1,8 +1,6 @@
-import { spacing } from '@oxide/css-helpers'
 import React from 'react'
 
-import type { TwStyle } from 'twin.macro'
-import tw, { styled } from 'twin.macro'
+import tw from 'twin.macro'
 import Icon from '../icon/Icon'
 
 export const badgeColors = [
@@ -19,115 +17,61 @@ export const badgeSizes = ['sm', 'base', 'xl'] as const
 type Size = typeof badgeSizes[number]
 
 export const badgeVariants = ['base', 'notification', 'closable'] as const
-// This separation helps keep the sizeMap simpler, so we don't have to define icon positionings for variants with no icons
-type VariantWithIcon = 'notification' | 'closable'
-type Variant = 'base' | VariantWithIcon
+type Variant = 'base' | 'notification' | 'closable'
 
 export interface BadgeProps {
   color?: BadgeColor
   size?: Size
   variant?: Variant
-
   className?: string
-
   onClose?: () => void
   children: React.ReactNode
 }
 
-const sizeMap: Record<
-  Size,
-  {
-    textSize: TwStyle
-    icons: Record<VariantWithIcon, { size: number; margin: number }>
-    padding: Record<Variant, number[]>
-  }
-> = {
+const wrapper = {
+  sm: tw`h-4 text-xxs`,
+  base: tw`h-6 text-xs`,
+  xl: tw`h-8 text-sm`,
+}
+
+const text = {
   sm: {
-    textSize: tw`text-xxs`,
-    icons: {
-      notification: { size: 2, margin: 1.25 },
-      closable: { size: 2, margin: 1.25 },
-    },
-    padding: {
-      base: [0.5, 1.25, 0.75],
-      notification: [0.5, 2, 0.75, 1.75],
-      closable: [0.5, 2, 0.75],
-    },
+    base: tw`margin[5px]`,
+    closable: tw`ml-2 mr-1.5`,
+    notification: tw`mx-1.5`,
   },
   base: {
-    textSize: tw`text-xs`,
-    icons: {
-      notification: { size: 2, margin: 1.75 },
-      closable: { size: 3, margin: 1.5 },
-    },
-    padding: {
-      base: [1.5, 3],
-      notification: [1.5, 3, 1.5, 2],
-      closable: [1.5, 2.5],
-    },
+    base: tw`mx-3`,
+    closable: tw`ml-3 mr-1.5`,
+    notification: tw`mr-3 ml-1.5`,
   },
   xl: {
-    textSize: tw`text-sm`,
-    icons: {
-      notification: { size: 2, margin: 2.25 },
-      closable: { size: 4, margin: 2.75 },
-    },
-    padding: {
-      base: [2.75, 4],
-      notification: [2.75, 4, 2.75, 3.25],
-      closable: [2.5, 3.5, 2.5, 4],
-    },
+    base: tw`mx-4`,
+    closable: tw`ml-4 mr-2`,
+    notification: tw`ml-2 mr-4`,
   },
 }
 
-const colorMap = {
-  gray: { bgColor: tw`bg-gray-600`, textColor: tw`text-white` },
-  red: { bgColor: tw`bg-dark-red`, textColor: tw`text-red-500` },
-  yellow: { bgColor: tw`bg-dark-yellow`, textColor: tw`text-yellow-500` },
-  green: { bgColor: tw`bg-dark-green-800`, textColor: tw`text-green-500` },
-  blue: { bgColor: tw`bg-dark-blue`, textColor: tw`text-blue-500` },
-  purple: { bgColor: tw`bg-dark-purple`, textColor: tw`text-purple-400` },
+const notificationIcon = {
+  sm: tw`w-2 ml-1.5`,
+  base: tw`w-2 margin-left[7px]`,
+  xl: tw`w-2 ml-3`,
 }
 
-const StyledBadge = styled.span<{
-  padding: number[]
-}>`
-  position: relative;
+const closeIcon = {
+  sm: tw`width[9px] mr-1.5`,
+  base: tw`w-3 mr-2`,
+  xl: tw`w-4 mr-3`,
+}
 
-  display: inline-flex;
-  justify-content: center;
-
-  padding: ${({ padding }) => spacing(...padding)};
-
-  border-radius: 9999px;
-`
-
-const BadgeText = tw.span`uppercase line-height[1]!`
-
-const StyledIcon = styled(Icon)<{ iconSize?: number }>`
-  ${({ iconSize }) => iconSize && `width: ${spacing(iconSize)};`}
-`
-
-const NotificationIcon = styled(StyledIcon)<{ margin: number }>`
-  margin-right: ${({ margin }) => spacing(margin)};
-`
-
-const StyledButton = styled.button<{
-  size: number
-  margin: number
-}>`
-  background: none;
-  border: none;
-  margin: 0;
-  padding: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  font-size: ${({ size }) => spacing(size)};
-  margin-left: ${({ margin }) => spacing(margin)};
-`
+const colors = {
+  gray: tw`bg-gray-600 text-white`,
+  red: tw`bg-dark-red text-red-500`,
+  yellow: tw`bg-dark-yellow text-yellow-500`,
+  green: tw`bg-dark-green-800 text-green-500`,
+  blue: tw`bg-dark-blue text-blue-500`,
+  purple: tw`bg-dark-purple text-purple-400`,
+}
 
 export const Badge = ({
   className,
@@ -136,35 +80,20 @@ export const Badge = ({
   size = 'base',
   variant = 'base',
   onClose,
-}: BadgeProps) => {
-  const { bgColor, textColor } = colorMap[color]
-  const {
-    textSize,
-    icons: { notification, closable },
-    padding,
-  } = sizeMap[size]
-
-  return (
-    <StyledBadge className={className} css={bgColor} padding={padding[variant]}>
-      {variant === 'notification' && (
-        <NotificationIcon
-          name="dot"
-          margin={notification.margin}
-          iconSize={notification.size}
-          css={[textColor]}
-        />
-      )}
-      <BadgeText css={[textSize, textColor]}>{children}</BadgeText>
-      {variant === 'closable' && (
-        <StyledButton
-          size={closable.size}
-          margin={closable.margin}
-          type="button"
-          onClick={() => onClose && onClose()}
-        >
-          <StyledIcon css={textColor} name="close" />
-        </StyledButton>
-      )}
-    </StyledBadge>
-  )
-}
+}: BadgeProps) => (
+  <span
+    tw="inline-flex items-center rounded-full uppercase"
+    css={[colors[color], wrapper[size]]}
+    className={className}
+  >
+    {variant === 'notification' && (
+      <Icon css={notificationIcon[size]} name="dot" />
+    )}
+    <span css={text[size][variant]}>{children}</span>
+    {variant === 'closable' && (
+      <button type="button" tw="flex cursor-pointer" onClick={onClose}>
+        <Icon name="close" css={closeIcon[size]} />
+      </button>
+    )}
+  </span>
+)
