e6c94450d35e5aa29f8fa4b768c809d8a2cd4ba5
David Crespo <crespo.dm@gmail.com>
2021-05-25 17:04:46 -0500

    blow up if API_URL is not set

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -3,6 +3,12 @@ import webpack from 'webpack'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 import sharedConfig from './webpack.shared.config'
 
+if (!process.env.API_URL) {
+  throw Error(
+    'API_URL cannot be undefined. You are probably attempting to run `yarn build` without it.'
+  )
+}
+
 const config = {
   ...sharedConfig,
   mode: 'production',
