6de1febd4ba5e0806d23d1c4c445a593baa352c0
David Crespo <crespo.dm@gmail.com>
2021-06-09 13:09:17 -0500

    clean up colors stories page

--- libs/ui/src/lib/__stories__/Colors.stories.tsx
+++ libs/ui/src/lib/__stories__/Colors.stories.tsx
@@ -1,101 +1,57 @@
 import React from 'react'
-import tw, { styled } from 'twin.macro'
+import 'twin.macro'
 import type { Color } from '@oxide/css-helpers'
 import { colorPalette, colorGroups, colorNames } from '@oxide/css-helpers'
-import { color, spacing } from '@oxide/css-helpers'
 
 interface ColorProps {
   name: Color
   value: string
 }
 
-const ColorContainer = styled.div`
-  display: flex;
-  flex: 0 1 auto;
-  align-items: stretch;
-  margin-bottom: ${spacing(4)};
-`
-const ColorVisualizer = styled.div<Pick<ColorProps, 'value'>>`
-  background-color: ${({ value }) => value};
-  width: ${spacing(12)};
-  height: ${spacing(12)};
-  margin-right: ${spacing(3)};
-  border: 1px solid ${color('gray800')};
-`
-const ColorInfo = styled.code`
-  display: flex;
-  :first-of-type {
-    margin-bottom: ${spacing(3)};
-  }
-`
-
-const ColorComponent: React.FC<ColorProps> = ({ name, value }) => (
-  <ColorContainer>
-    <ColorVisualizer value={value} />
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      <ColorInfo>{name}</ColorInfo>
-      <ColorInfo>{value}</ColorInfo>
-    </div>
-  </ColorContainer>
-)
-
-const ColorGroupContainer = styled.div<{ isSingleColor?: boolean }>`
-  flex: ${({ isSingleColor }) => (isSingleColor ? '1 1 100%' : '0 0 25%')};
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-`
-
-const ColorGroupTitle = tw.h3`capitalize my-4 font-sans`
-
-const ColorGroupComponent: React.FC<{
-  group: string
-  colors: ColorProps[]
-}> = ({ group, colors }) => (
-  <ColorGroupContainer isSingleColor={colors.length === 1}>
-    <ColorGroupTitle>{group}</ColorGroupTitle>
-    {colors.map(({ name, value }) => (
-      <ColorComponent key={name} name={name} value={value} />
-    ))}
-  </ColorGroupContainer>
-)
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  flex-wrap: wrap;
-`
-
-const findColors = (v: string): ColorProps[] =>
-  colorNames
-    .filter((color) => color.startsWith(v))
-    .map((k) => ({
-      name: k,
-      value: colorPalette[k],
-    }))
-
-const mapColors = (group: string): [string, ColorProps[]] => [
+const colorGroup = (group: string): [string, ColorProps[]] => [
   group,
-  findColors(group),
+  colorNames
+    .filter((color) => color.startsWith(group))
+    .map((k) => ({ name: k, value: colorPalette[k] })),
 ]
 
 const groups: [string, ColorProps[]][] = [
-  mapColors('white'),
-  mapColors('black'),
-  ...colorGroups.map(mapColors),
-  mapColors('darkBg'),
+  colorGroup('white'),
+  colorGroup('black'),
+  ...colorGroups.map(colorGroup),
+  colorGroup('darkBg'),
 ]
 
+const ColorSquare = (props: { value: string }) => (
+  <div
+    tw="w-12 h-12 mr-3 border border-gray-800"
+    css={{ backgroundColor: props.value }}
+  />
+)
+
+const ColorLabel = ({ name, value }: ColorProps) => (
+  <div tw="flex flex-col">
+    <code tw="mb-3">{name}</code>
+    <code>{value}</code>
+  </div>
+)
+
 export const AllColors: React.FC = () => (
-  <Container>
+  <div tw="flex flex-wrap">
     {groups.map(([group, colors]) => (
-      <ColorGroupComponent key={group} group={group} colors={colors} />
+      <div
+        key={group}
+        tw="flex flex-col flex-wrap"
+        css={{ flex: colors.length === 1 ? '1 1 100%' : '0 0 25%' }}
+      >
+        <h3 tw="capitalize my-4 font-sans">{group}</h3>
+        {colors.map(({ name, value }) => (
+          <div key={name} tw="flex mb-4">
+            <ColorSquare value={value} />
+            <ColorLabel value={value} name={name} />
+          </div>
+        ))}
+      </div>
     ))}
-  </Container>
+  </div>
 )
