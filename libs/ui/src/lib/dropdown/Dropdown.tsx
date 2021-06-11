56f7dadc758a934939eac2a34b7ad55b3e1260b3
David Crespo <crespo.dm@gmail.com>
2021-06-10 21:33:43 -0500

    use tw theme instead of color() in dropdown

--- libs/ui/src/lib/dropdown/Dropdown.tsx
+++ libs/ui/src/lib/dropdown/Dropdown.tsx
@@ -1,12 +1,11 @@
 import type { FC } from 'react'
 import React from 'react'
 
-import tw, { css, styled } from 'twin.macro'
+import tw, { css, theme } from 'twin.macro'
 import { useSelect } from 'downshift'
 import { AnimatePresence, motion } from 'framer-motion'
 
 import { Icon } from '../icon/Icon'
-import { color, spacing } from '@oxide/css-helpers'
 
 type SizeType = 'sm' | 'lg'
 type OptionType = { value: string; label: string }
@@ -26,107 +25,11 @@ export interface DropdownProps {
   size?: SizeType
 }
 
-const Wrapper = styled.div`
-  position: relative;
-`
-
-type ButtonProps = React.ComponentProps<'button'> & { hasPlaceholder: boolean }
-
-const StyledButton = styled.button<ButtonProps>`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
-  margin-top: ${spacing(1)};
-  padding: ${spacing(2, 4)};
-  vertical-align: top;
-  width: 100%;
-
-  background-color: ${color('gray800')};
-  border: none;
-  color: ${color('gray50')};
-  font-size: ${spacing(4)};
-
-  font-weight: ${({ hasPlaceholder }) => (hasPlaceholder ? 400 : 500)};
-  line-height: 1.5;
-
-  &:hover {
-    background-color: ${color('gray700')};
-  }
-
-  &:focus {
-    box-shadow: 0 0 0 1px ${color('green500')};
-    outline: none;
-  }
-`
-
-const StyledMenu = styled(motion.ul)`
-  z-index: 1;
-  position: absolute;
-  left: 0;
-  right: 0;
-
-  overflow-y: auto;
-  margin: 0;
-  margin-top: ${spacing(1)};
-  padding: 0;
-
-  background-color: ${color('gray800')};
-  box-shadow: ${spacing(0, 3, 6)} ${color('black', 0.16)};
-
-  list-style: none;
-
-  &:focus {
-    box-shadow: 0 0 0 1px ${color('green500')};
-    outline: none;
-  }
-`
-
-const getOptionStyles = (size: SizeType) => {
-  switch (size) {
-    case 'lg':
-      return css`
-        padding: ${spacing(2.5, 4)};
-      `
-    default:
-    case 'sm':
-      return css`
-        padding: ${spacing(1.5, 4)};
-      `
-  }
-}
-
-const StyledOption = styled.li<{ size: SizeType; isHighlighted: boolean }>`
-  color: ${color('gray200')};
-  font-size: ${spacing(3.5)};
-  font-weight: 400;
-  line-height: 1.5;
-
-  &:hover,
-  &:focus {
-    background-color: ${color('gray700')};
-  }
-
+const focusRing = css`
   &:focus {
     outline: none;
-    box-shadow: 0 0 0 1px ${color('green500')};
+    box-shadow: 0 0 0 1px ${theme`colors.green.500`};
   }
-
-  &:first-of-type {
-    margin-top: ${spacing(2)};
-  }
-
-  &:last-child {
-    margin-bottom: ${spacing(2)};
-  }
-
-  ${({ size }) => getOptionStyles(size)};
-  ${({ isHighlighted }) =>
-    isHighlighted &&
-    css`
-      background-color: ${color('gray700')};
-    `};
 `
 
 const HintText = tw.div`text-sm mt-1 text-gray-300`
@@ -169,38 +72,49 @@ export const Dropdown: FC<DropdownProps> = ({
     ? placeholder
     : label
 
-  const renderOptions = options.map((option, index) => (
-    <StyledOption
-      key={option.value}
-      value={option.value}
-      size={size}
-      {...select.getItemProps({ item: option, index })}
-      isHighlighted={select.highlightedIndex === index}
-    >
-      {option.label}
-    </StyledOption>
-  ))
+  const renderOptions = options.map((option, index) => {
+    const highlighted = select.highlightedIndex === index
+    return (
+      <li
+        tw="py-2 px-4 text-sm text-gray-200 hocus:bg-gray-700"
+        key={option.value}
+        value={option.value}
+        {...select.getItemProps({ item: option, index })}
+        css={[
+          focusRing,
+          size === 'lg' && tw`py-2.5`,
+          highlighted && tw`bg-gray-700`,
+        ]}
+      >
+        {option.label}
+      </li>
+    )
+  })
 
   return (
-    <Wrapper>
+    <div tw="relative">
       <label
         css={showLabel ? tw`text-white text-sm` : tw`sr-only`}
         {...select.getLabelProps()}
       >
         {label}
       </label>
-      <StyledButton
+      <button
+        tw="flex items-center justify-between mt-1 py-2 px-4 w-full
+            text-base text-gray-50 bg-gray-800 hover:bg-gray-700"
+        css={focusRing}
         type="button"
-        hasPlaceholder={!select.selectedItem}
         {...select.getToggleButtonProps()}
         {...ariaProps}
       >
         {renderButtonText}
         <Icon name="chevron" tw="ml-5 transform -rotate-90" />
-      </StyledButton>
+      </button>
       <AnimatePresence>
         {select.isOpen && (
-          <StyledMenu
+          <motion.ul
+            tw="z-10 py-2 mt-1 absolute left-0 right-0 overflow-y-auto bg-gray-800"
+            css={focusRing}
             variants={FRAMER_VARIANTS}
             initial={'closed'}
             animate={'open'}
@@ -208,12 +122,12 @@ export const Dropdown: FC<DropdownProps> = ({
             {...select.getMenuProps()}
           >
             {renderOptions}
-          </StyledMenu>
+          </motion.ul>
         )}
       </AnimatePresence>
       {/* if you Tab from menu, focus goes on button, and it shouldn't. only happens here. */}
       <div tabIndex={0} />
       {hint && <HintText id={hintId}>{hint}</HintText>}
-    </Wrapper>
+    </div>
   )
 }
