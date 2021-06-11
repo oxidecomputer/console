c121a60fb139ad6ca191da3f3cdbb148f8b4059c
David Crespo <crespo.dm@gmail.com>
2021-06-10 19:49:14 -0500

    storybook colors use TW config

--- libs/ui/src/lib/__stories__/Colors.stories.tsx
+++ libs/ui/src/lib/__stories__/Colors.stories.tsx
@@ -1,38 +1,31 @@
 import React from 'react'
-import 'twin.macro'
-import type { Color } from '@oxide/css-helpers'
-import { colorPalette, colorGroups, colorNames } from '@oxide/css-helpers'
+import { theme } from 'twin.macro'
 
 interface ColorProps {
-  name: Color
+  name?: string
   value: string
 }
 
-const colorGroup = (group: string): [string, ColorProps[]] => [
-  group,
-  colorNames
-    .filter((color) => color.startsWith(group))
-    .map((k) => ({ name: k, value: colorPalette[k] })),
-]
+const EXCLUDE_GROUPS = ['current', 'transparent', 'black', 'white']
 
-const groups: [string, ColorProps[]][] = [
-  colorGroup('white'),
-  colorGroup('black'),
-  ...colorGroups.map(colorGroup),
-  colorGroup('darkBg'),
-]
+type ColorGroup = string | Record<string, string>
 
-const ColorSquare = (props: { value: string }) => (
-  <div
-    tw="w-12 h-12 mr-3 border border-gray-800"
-    css={{ backgroundColor: props.value }}
-  />
+const colorConfig = theme`colors` as Record<string, ColorGroup>
+
+const groups = Object.entries(colorConfig).filter(
+  ([group, _]) => !EXCLUDE_GROUPS.includes(group)
 )
 
-const ColorLabel = ({ name, value }: ColorProps) => (
-  <div tw="flex flex-col">
-    <code tw="mb-3">{name}</code>
-    <code>{value}</code>
+const Color = ({ name, value }: ColorProps) => (
+  <div key={name} tw="flex mb-4">
+    <div
+      tw="w-12 h-12 mr-3 border border-gray-800"
+      css={{ backgroundColor: value }}
+    />
+    <div tw="flex flex-col">
+      {name && <code tw="mb-3">{name}</code>}
+      <code>{value}</code>
+    </div>
   </div>
 )
 
@@ -44,13 +37,14 @@ export const AllColors: React.FC = () => (
         tw="flex flex-col flex-wrap"
         css={{ flex: colors.length === 1 ? '1 1 100%' : '0 0 25%' }}
       >
-        <h3 tw="capitalize my-4 font-sans">{group}</h3>
-        {colors.map(({ name, value }) => (
-          <div key={name} tw="flex mb-4">
-            <ColorSquare value={value} />
-            <ColorLabel value={value} name={name} />
-          </div>
-        ))}
+        <h3 tw="my-4 font-sans font-medium text-lg">{group}</h3>
+        {typeof colors === 'string' ? (
+          <Color value={colors} />
+        ) : (
+          Object.entries(colors).map(([name, value]) => (
+            <Color key={name} value={value} name={name} />
+          ))
+        )}
       </div>
     ))}
   </div>
