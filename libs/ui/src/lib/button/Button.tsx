f873f692d0dcdf699fb72bb1d9db128d6cbff1d6
David Crespo <crespo.dm@gmail.com>
2021-06-25 14:17:48 -0500

    convert buttons to plain TW

--- libs/ui/src/lib/button/Button.tsx
+++ libs/ui/src/lib/button/Button.tsx
@@ -1,7 +1,6 @@
 import type { PropsWithChildren } from 'react'
 import React, { forwardRef } from 'react'
-import type { TwStyle } from 'twin.macro'
-import tw from 'twin.macro'
+import cn from 'classnames'
 
 export const buttonSizes = ['xs', 'sm', 'base', 'lg', 'xl'] as const
 export const variants = ['solid', 'dim', 'ghost', 'link'] as const
@@ -9,30 +8,33 @@ export const variants = ['solid', 'dim', 'ghost', 'link'] as const
 export type ButtonSize = typeof buttonSizes[number]
 export type Variant = typeof variants[number]
 
-const sizeStyle: Record<ButtonSize, TwStyle> = {
-  xs: tw`h-8 px-3 text-sm svg:w-4`,
-  sm: tw`h-9 px-3 text-sm svg:w-4`,
-  base: tw`h-10 px-4 text-sm svg:w-5`,
-  lg: tw`h-11 px-5 text-base svg:w-5`,
-  xl: tw`h-12 px-6 text-base svg:w-6`,
+const sizeStyle: Record<ButtonSize, string> = {
+  xs: 'h-8 px-3 text-sm svg:w-4',
+  sm: 'h-9 px-3 text-sm svg:w-4',
+  base: 'h-10 px-4 text-sm svg:w-5',
+  lg: 'h-11 px-5 text-base svg:w-5',
+  xl: 'h-12 px-6 text-base svg:w-6',
 }
 
-const variantStyle: Record<Variant, TwStyle> = {
-  solid: tw`bg-green-500 text-black hover:bg-green-600 disabled:bg-gray-200`,
-  dim: tw`
-    bg-green-900 text-green-500 hover:bg-green-950
+const variantStyle: Record<Variant, string> = {
+  solid: `
+    bg-green-500 border-transparent text-black
+    hover:bg-green-600 disabled:bg-gray-200
+  `,
+  dim: `
+    bg-green-900 border-transparent text-green-500 hover:bg-green-950
     disabled:text-green-700 disabled:bg-green-900
   `,
-  ghost: tw`
+  ghost: `
     text-white border-green-500 hover:bg-green-900 
     disabled:border-green-700 disabled:bg-black disabled:text-gray-100
   `,
   // note h-auto overriding size style
-  link: tw`text-green-500 h-auto leading-5 px-0.5 hover:underline`,
+  link: 'border-transparent text-green-500 !h-auto !leading-5 !px-0.5 hover:underline',
 }
 
-const baseStyle = tw`
-  border border-transparent rounded-px uppercase
+const baseStyle = `
+  border rounded-px uppercase
   inline-flex items-center justify-center align-top
   disabled:cursor-not-allowed
   focus:ring-2 focus:ring-green-700
@@ -51,17 +53,25 @@ export const Button = forwardRef<
   PropsWithChildren<ButtonProps>
 >(
   (
-    { children, fullWidth = false, size = 'base', variant = 'solid', ...rest },
+    {
+      children,
+      fullWidth = false,
+      size = 'base',
+      variant = 'solid',
+      className,
+      ...rest
+    },
     ref
   ) => {
     return (
       <button
-        css={[
+        className={cn(
           baseStyle,
           sizeStyle[size],
           variantStyle[variant],
-          fullWidth && tw`w-full`,
-        ]}
+          fullWidth && 'w-full',
+          className
+        )}
         ref={ref}
         type="button"
         {...rest}
