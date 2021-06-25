88f8b15141a8429bd57382fbdf96f7767e9653b4
David Crespo <crespo.dm@gmail.com>
2021-06-24 21:22:55 -0500

    Badge

--- libs/ui/src/lib/badge/Badge.tsx
+++ libs/ui/src/lib/badge/Badge.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import cn from 'classnames'
 
-import tw from 'twin.macro'
 import { Icon } from '../icon/Icon'
 
 export const badgeColors = ['gray', 'red', 'yellow', 'green', 'blue'] as const
@@ -22,47 +22,47 @@ export interface BadgeProps {
 }
 
 const wrapper = {
-  sm: tw`h-4 text-xxs`,
-  base: tw`h-6 text-xs`,
-  xl: tw`h-8 text-sm`,
+  sm: 'h-4 text-xxs',
+  base: 'h-6 text-xs',
+  xl: 'h-8 text-sm',
 }
 
 const text = {
   sm: {
-    base: tw`margin[5px]`,
-    closable: tw`ml-2 mr-1.5`,
-    notification: tw`mx-1.5`,
+    base: 'm-[5px]',
+    closable: 'ml-2 mr-1.5',
+    notification: 'mx-1.5',
   },
   base: {
-    base: tw`mx-3`,
-    closable: tw`ml-3 mr-1.5`,
-    notification: tw`mr-3 ml-1.5`,
+    base: 'mx-3',
+    closable: 'ml-3 mr-1.5',
+    notification: 'mr-3 ml-1.5',
   },
   xl: {
-    base: tw`mx-4`,
-    closable: tw`ml-4 mr-2`,
-    notification: tw`ml-2 mr-4`,
+    base: 'mx-4',
+    closable: 'ml-4 mr-2',
+    notification: 'ml-2 mr-4',
   },
 }
 
 const notificationIcon = {
-  sm: tw`w-2 ml-1.5`,
-  base: tw`w-2 margin-left[7px]`,
-  xl: tw`w-2 ml-3`,
+  sm: '!w-2 ml-1.5',
+  base: '!w-2 ml-[7px]',
+  xl: '!w-2 ml-3',
 }
 
 const closeIcon = {
-  sm: tw`width[9px] mr-1.5`,
-  base: tw`w-3 mr-2`,
-  xl: tw`w-4 mr-3`,
+  sm: '!w-[9px] mr-1.5',
+  base: '!w-3 mr-2',
+  xl: '!w-4 mr-3',
 }
 
 const colors = {
-  gray: tw`bg-gray-400 text-white`,
-  red: tw`bg-red-900 text-red-500`,
-  yellow: tw`bg-yellow-900 text-yellow-500`,
-  green: tw`bg-green-900 text-green-500`,
-  blue: tw`bg-blue-900 text-blue-500`,
+  gray: 'bg-gray-400 text-white',
+  red: 'bg-red-900 text-red-500',
+  yellow: 'bg-yellow-900 text-yellow-500',
+  green: 'bg-green-900 text-green-500',
+  blue: 'bg-blue-900 text-blue-500',
 }
 
 export const Badge = ({
@@ -74,17 +74,20 @@ export const Badge = ({
   onClose,
 }: BadgeProps) => (
   <span
-    tw="inline-flex items-center rounded-full uppercase"
-    css={[colors[color], wrapper[size]]}
-    className={className}
+    className={cn(
+      'inline-flex items-center rounded-full uppercase',
+      colors[color],
+      wrapper[size],
+      className
+    )}
   >
     {variant === 'notification' && (
-      <Icon css={notificationIcon[size]} name="dot" />
+      <Icon className={notificationIcon[size]} name="dot" />
     )}
-    <span css={text[size][variant]}>{children}</span>
+    <span className={text[size][variant]}>{children}</span>
     {variant === 'closable' && (
-      <button type="button" tw="flex cursor-pointer" onClick={onClose}>
-        <Icon name="close" css={closeIcon[size]} />
+      <button type="button" className="flex cursor-pointer" onClick={onClose}>
+        <Icon name="close" className={closeIcon[size]} />
       </button>
     )}
   </span>
