6e4b583189bc0e7e5aac1d21e650e68a1fe82d12
David Crespo <crespo.dm@gmail.com>
2021-07-02 11:55:31 -0500

    define API_URL in production

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,41 @@
-import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    sourcemap: 'inline',
-  },
-  // TODO: add titleProp: true when svgr plugin supports it
-  // https://github.com/pd4d10/vite-plugin-svgr/blob/83b07cb/src/index.ts#L6
-  plugins: [reactRefresh(), svgr()],
-  resolve: {
-    alias: {
-      '@oxide/ui': `${__dirname}/libs/ui/src`,
-      '@oxide/css-helpers': `${__dirname}/libs/css-helpers`,
-      '@oxide/api': `${__dirname}/libs/api`,
-      '@oxide/api-mocks': `${__dirname}/libs/api-mocks`,
+// see https://vitejs.dev/config/
+
+export default ({ mode }) => {
+  if (mode === 'production' && !process.env.API_URL) {
+    throw Error(
+      '\n\nAPI_URL env var must be defined for production builds. You are probably attempting to run `yarn build` without it. See Vite config.\n'
+    )
+  }
+
+  return {
+    build: {
+      sourcemap: 'inline',
     },
-  },
-  server: {
-    port: 4000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:12220',
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    define: {
+      'process.env.API_URL': JSON.stringify(process.env.API_URL),
+    },
+    // TODO: add titleProp: true when svgr plugin supports it
+    // https://github.com/pd4d10/vite-plugin-svgr/blob/83b07cb/src/index.ts#L6
+    plugins: [reactRefresh(), svgr()],
+    resolve: {
+      alias: {
+        '@oxide/ui': `${__dirname}/libs/ui/src`,
+        '@oxide/css-helpers': `${__dirname}/libs/css-helpers`,
+        '@oxide/api': `${__dirname}/libs/api`,
+        '@oxide/api-mocks': `${__dirname}/libs/api-mocks`,
       },
     },
-  },
-})
+    server: {
+      port: 4000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:12220',
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
+}
