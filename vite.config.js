f57bc3b9977c56b732e9a276dd19d6682eed3eff
David Crespo <crespo.dm@gmail.com>
2021-07-09 17:47:56 -0500

    Revert "delete .babelrc and .browserslistrc"
    b467a3ead7f8e2c56fd49e4f767514d65ddb1184

--- vite.config.js
+++ vite.config.js
@@ -40,6 +40,7 @@ export default ({ mode }) => {
           docs: resolve(__dirname, 'app/docs/index.html'),
         },
       },
+      minify: false,
     },
     define: {
       'process.env.API_URL': JSON.stringify(process.env.API_URL),
