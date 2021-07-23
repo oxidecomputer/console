f2316ae80ece12165b36ab8ac48e2fe3e1674bdb
David Crespo <crespo.dm@gmail.com>
2021-07-23 13:50:32 -0500

    ProjectList doesn't need its own file

--- app/layouts/AppLayout.tsx
+++ app/layouts/AppLayout.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, NavLink, Outlet } from 'react-router-dom'
+import cn from 'classnames'
+
+import { useApiQuery } from '@oxide/api'
+import { Icon } from '@oxide/ui'
 
 import { GlobalNav } from '../components/GlobalNav'
-import { ProjectList } from '../components/ProjectList'
 import Wordmark from '../assets/wordmark.svg'
+
+const ProjectList = (props: { className?: string }) => {
+  const { data: projects } = useApiQuery('projectsGet', {})
+  return (
+    <section className={cn('space-y-2', props.className)}>
+      <header className="p-1 space-x-2 uppercase text-xs font-mono text-green-500">
+        Projects
+      </header>
+      <ul className="space-y-0.5 text-gray-200 font-light text-sm">
+        {projects?.items.map((p) => (
+          <li className="hover:bg-gray-400" key={p.id}>
+            <NavLink
+              className="inline-flex w-full p-1"
+              to={`/projects/${p.name}`}
+              activeClassName="text-white"
+              end
+            >
+              {p.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <footer className="p-1 flex">
+        <Link
+          className="text-xs inline-flex uppercase text-gray-100 font-mono"
+          to="/projects/new"
+        >
+          <Icon className="text-sm mr-1" name="plus" />
+          New project
+        </Link>
+      </footer>
+    </section>
+  )
+}
 
 export default () => (
   <div className="grid h-screen grid-cols-[14rem,auto] grid-rows-[4.5rem,auto]">
