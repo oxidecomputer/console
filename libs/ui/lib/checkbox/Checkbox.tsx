df675cb29dcfedcceb81f3f61da1b35789906c4b
David Crespo <crespo.dm@gmail.com>
2021-08-20 15:59:50 -0500

    take the forwardRef thing off of Checkbox

--- libs/ui/lib/checkbox/Checkbox.tsx
+++ libs/ui/lib/checkbox/Checkbox.tsx
@@ -3,11 +3,6 @@ import React from 'react'
 import CheckIcon from '../../assets/check.svg'
 import { classed } from '../../util/classed'
 
-type InputProps = {
-  indeterminate?: boolean
-  label?: string | React.ReactFragment
-} & React.ComponentProps<'input'>
-
 const Check = () => (
   <CheckIcon className="absolute w-2.5 h-2 left-[3px] top-1 fill-current text-green-500" />
 )
@@ -23,49 +18,44 @@ const inputStyle = `
   indeterminate:bg-green-900 indeterminate:border-green-500 hover:indeterminate:bg-green-950
 `
 
-// TODO: we may end up wanting to call this IndeterminateCheckbox (ew) if
-// we need to distinguish it from one without that state. or not
-export const Checkbox = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ indeterminate, label, ...inputProps }, outerRef) => {
-    // null, not blank (undefined), otherwise TS is mad when ref passed to input
-    const defaultRef = React.useRef<HTMLInputElement>(null)
-    const ref = outerRef || defaultRef
+type Props = {
+  indeterminate?: boolean
+  label?: React.ReactNode
+} & React.ComponentProps<'input'>
 
-    React.useEffect(() => {
-      // TODO: this makes types pass by basically ignoring callback refs. see more
-      // sophisticated approach here:
-      // https://github.com/tannerlinsley/react-table/discussions/1989#discussioncomment-1488
-      if (typeof ref !== 'function' && ref?.current) {
-        ref.current.indeterminate = indeterminate ?? false
-      }
-    }, [ref, indeterminate])
+export const Checkbox = ({ indeterminate, label, ...inputProps }: Props) => {
+  // null, not blank (undefined), otherwise TS is mad when ref passed to input
+  const ref = React.useRef<HTMLInputElement>(null)
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.indeterminate = indeterminate ?? false
+    }
+  }, [ref, indeterminate])
 
-    // everything above is to make the native input work with indeterminate. you
-    // can't pass indeterminate as a prop, it has to be set on a ref which is
-    // passed to the input. We could skip the forwardRef and outerRef bit
-    // altogether (just use defaultRef), but this makes it so we can pass a ref
-    // from outside if we want
+  // the above makes the native input work with indeterminate. you can't pass
+  // indeterminate as a prop; it has to be set on a ref, which is then passed to
+  // the input. see more elaborate examples here using forwardRef to allow
+  // passing ref from outside:
+  // https://github.com/tannerlinsley/react-table/discussions/1989
 
-    return (
-      <label className="inline-flex items-center">
-        <span className="h-4 w-4 relative">
-          <input
-            className={inputStyle}
-            type="checkbox"
-            ref={ref}
-            {...inputProps}
-          />
-          {inputProps.checked && !indeterminate && <Check />}
-          {indeterminate && <Indeterminate />}
+  return (
+    <label className="inline-flex items-center">
+      <span className="h-4 w-4 relative">
+        <input
+          className={inputStyle}
+          type="checkbox"
+          ref={ref}
+          {...inputProps}
+        />
+        {inputProps.checked && !indeterminate && <Check />}
+        {indeterminate && <Indeterminate />}
+      </span>
+
+      {label && (
+        <span className="text-xs text-gray-200 uppercase font-mono ml-2.5">
+          {label}
         </span>
-
-        {/* TODO: if no label, we may not want to wrap the whole input in <label> */}
-        {label && (
-          <span className="text-xs text-gray-200 uppercase font-mono ml-2.5">
-            {label}
-          </span>
-        )}
-      </label>
-    )
-  }
-)
+      )}
+    </label>
+  )
+}
