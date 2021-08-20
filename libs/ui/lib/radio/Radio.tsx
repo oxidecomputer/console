fe48503aabaae4a93a030e7779c9cd305252de0d
David Crespo <david-crespo@users.noreply.github.com>
2021-08-20 11:43:45 -0500

    Put input inside label, some other reorg (#436)
    * put input inside label
    
    * use checkbox in table row select, fix input alignment with left: 0
    
    * add TODO comment
    
    * give radios the same treatment
    
    * rest -> inputProps

--- libs/ui/lib/radio/Radio.tsx
+++ libs/ui/lib/radio/Radio.tsx
@@ -1,52 +1,30 @@
 import React from 'react'
 
-type InputProps = {
-  id: string
-  label?: string | React.ReactFragment
+import { classed } from '../../util/classed'
+
+type Props = {
+  label: React.ReactNode
 } & React.ComponentProps<'input'>
 
-type LabelProps = React.ComponentProps<'label'>
-const Label = ({ children, ...labelProps }: LabelProps) => (
-  <label {...labelProps} className="flex items-baseline font-mono ml-2.5">
-    <span className="flex items-baseline text-xs text-gray-200 uppercase">
-      {children}
+const Dot = classed.div`absolute w-2 h-2 left-1 top-1 rounded-full bg-green-500`
+
+const baseStyle = `
+  appearance-none border border-gray-300 h-4 w-4 rounded-full absolute outline-none
+  disabled:cursor-not-allowed
+  focus:ring-2 focus:ring-green-700
+  hover:bg-gray-400
+  checked:bg-green-900 checked:border-green-500 hover:checked:bg-green-950
+`
+
+export const Radio = ({ label, ...inputProps }: Props) => (
+  <label className="inline-flex items-center">
+    <span className="h-4 w-4 relative">
+      <input className={baseStyle} type="radio" {...inputProps} />
+      {inputProps.checked && <Dot />}
+    </span>
+
+    <span className="text-xs text-gray-200 uppercase font-mono ml-2.5">
+      {label}
     </span>
   </label>
-)
-
-// TODO: we may end up wanting to call this IndeterminateCheckbox (ew) if
-// we need to distinguish it from one without that state. or not
-export const Radio = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ id, label, ...rest }, outerRef) => {
-    // null, not blank (undefined), otherwise TS is mad when ref passed to input
-    const defaultRef = React.useRef<HTMLInputElement>(null)
-    const ref = outerRef || defaultRef
-
-    const baseStyle = `
-      appearance-none border border-gray-300 h-4 w-4 rounded-full absolute outline-none
-      disabled:cursor-not-allowed
-      focus:ring-2 focus:ring-green-700
-      hover:bg-gray-400
-      checked:bg-green-900 checked:border-green-500 hover:checked:bg-green-950
-    `
-
-    return (
-      <div className="flex">
-        <div className="h-4 w-4 relative">
-          <input className={baseStyle} type="radio" ref={ref} {...rest} />
-
-          {rest.checked && (
-            <div
-              className="
-                absolute w-[8px] h-[8px] left-[4px] top-[4px] rounded-full
-                bg-green-500
-              "
-            />
-          )}
-        </div>
-
-        {label && <Label htmlFor={id}>{label}</Label>}
-      </div>
-    )
-  }
 )
