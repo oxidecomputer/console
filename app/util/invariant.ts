be7163ff8e30d1a1f46d4fc07c27698b8fc5695c
David Crespo <crespo.dm@gmail.com>
2021-09-12 22:01:24 -0500

    jsdoc comment format so VS Code picks it up

--- app/util/invariant.ts
+++ app/util/invariant.ts
@@ -1,10 +1,7 @@
-// Used to assert runtime invariants in development and test. Miraculously,
-// Rollup not only minifies out the inside of the function when `NODE_ENV` is
-// 'production', it actually removes the entire function call from the calling
-// code, which means arbitrarily long error messages don't end up in the
-// production bundle. This was manually tested in the production build to make
-// sure, but here's an example in Rollup REPL: https://bit.ly/3z5cbQG
-
+/**
+ * Throw with mesasage if condition is falsy. Entire call stripped out by
+ * Rollup in prod.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function invariant(condition: any, message: string) {
   if (process.env.NODE_ENV !== 'production') {
@@ -13,3 +10,9 @@ export function invariant(condition: any, message: string) {
     }
   }
 }
+
+// Miraculously, Rollup not only minifies out the inside of the function when
+// `NODE_ENV` is 'production', it actually removes the entire function call from
+// the calling code, which means long error messages don't end up in the
+// production bundle. This was manually tested in the production build to make
+// sure, but here's an example in Rollup REPL: https://bit.ly/3z5cbQG
