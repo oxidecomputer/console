c5df1383bd9c1fbcd441c07365f571efcfb6c5f0
Justin Bennett <oss@just-be.dev>
2021-09-23 23:42:52 -0400

    Remove unneeded styles

--- libs/ui/lib/skip-link/SkipLink.tsx
+++ libs/ui/lib/skip-link/SkipLink.tsx
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from 'react'
 
 const skipLinkStyles = `
     absolute -top-10 z-10 p-1
-    w-full border rounded uppercase font-mono 
+    w-full uppercase font-mono 
     inline-flex items-center justify-center 
-    align-top focus:ring-2 focus:ring-green-700 
+    focus:ring-2 focus:ring-green-700 
     bg-green-500 border-transparent text-black 
-    hover:bg-green-600 transition-all motion-reduce:transform-none
+    transition-all motion-reduce:transform-none
 `
 
 interface SkipLinkProps {
