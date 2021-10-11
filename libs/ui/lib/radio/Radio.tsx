f1434e8d7f848afb377f876e6ecb73164c1f0615
Justin Bennett <oss@just-be.dev>
2021-10-10 21:54:24 -0400

    Ensure radio disabled hover border doesn't change

--- libs/ui/lib/radio/Radio.tsx
+++ libs/ui/lib/radio/Radio.tsx
@@ -39,7 +39,7 @@ const cardLabelStyles = `
   text-sm bg-gray-500 border rounded border-gray-400 
   hover:text-green-500 hover:border-green-500 peer-focus:ring-2 peer-focus:ring-green-700
   peer-checked:bg-green-900 peer-checked:border-green-500 peer-checked:text-green-500
-  peer-disabled:hover:text-gray-100 peer-disabled:text-gray-100
+  peer-disabled:hover:text-gray-100 peer-disabled:text-gray-100 peer-disabled:hover:border-gray-400
 `
 
 export const RadioCard = ({ children, className, ...inputProps }: Props) => (
