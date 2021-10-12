c1ceb075649720e13686b27ac3b4f2dbc30b013d
Justin Bennett <oss@just-be.dev>
2021-10-12 15:50:15 -0400

    Correct radio styles to match latest design

--- libs/ui/lib/radio/Radio.tsx
+++ libs/ui/lib/radio/Radio.tsx
@@ -36,10 +36,12 @@ export const Radio = ({ children, ...inputProps }: Props) => (
 )
 
 const cardLabelStyles = `
-  text-sm bg-gray-500 border rounded border-gray-400 
-  hover:text-green-500 hover:border-green-500 peer-focus:ring-2 peer-focus:ring-green-700
-  peer-checked:bg-green-900 peer-checked:border-green-500 peer-checked:text-green-500
-  peer-disabled:hover:text-gray-100 peer-disabled:text-gray-100 peer-disabled:hover:border-gray-400
+  text-sm border rounded border-gray-400 
+  bg-gray-500 hover:bg-gray-550
+  peer-focus:ring-2 peer-focus:ring-green-700
+  peer-checked:bg-green-900 peer-checked:hover:bg-green-950 peer-checked:border-green-500 peer-checked:text-green-500
+  peer-disabled:hover:text-gray-100 peer-disabled:text-gray-100 peer-disabled:hover:border-gray-400 
+  peer-disabled:hover:bg-gray-500
 `
 
 export const RadioCard = ({ children, className, ...inputProps }: Props) => (
