e34dac9081ff5ed61e4578477b26b4d5ee5d060f
Justin Bennett <oss@just-be.dev>
2021-10-15 16:20:47 -0400

    Convert to using new side modal component

--- app/pages/instances/create/new-disk-modal.tsx
+++ app/pages/instances/create/new-disk-modal.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Dialog } from '@reach/dialog'
 import { Formik, Form } from 'formik'
 
-import { Button, Icon, Radio, RadioGroup } from '@oxide/ui'
+import { Button, Radio, RadioGroup, SideModal } from '@oxide/ui'
 
 const headingStyle = 'font-medium mt-6 mb-3'
 
@@ -13,52 +12,43 @@ type Props = {
 
 export function NewDiskModal({ isOpen, onDismiss }: Props) {
   return (
-    <Dialog
-      className="SideModal"
+    <SideModal
+      id="new-disk-modal"
+      title="Add new disk"
       isOpen={isOpen}
       onDismiss={onDismiss}
-      aria-labelledby="new-disk-modal-title"
     >
-      <Formik initialValues={{ 'deletion-rule': '' }} onSubmit={() => {}}>
-        <Form>
-          <div className="modal-body">
-            <div className="p-8">
-              <div className="flex justify-between mt-2 mb-14">
-                <h2 className="text-display-xl" id="new-disk-modal-title">
-                  Add new disk
-                </h2>
-                <Button variant="link" onClick={onDismiss}>
-                  <Icon name="close" />
-                </Button>
-              </div>
-              <h3 className={headingStyle}>Name</h3>
-              <h3 className={headingStyle}>Description</h3>
-              <h3 className={headingStyle}>Type</h3>
-              <h3 className={headingStyle}>Source type</h3>
-              <fieldset>
-                <legend className={headingStyle}>Deletion rule</legend>
-                <RadioGroup name="deletion-rule" column>
-                  <Radio value="keep">Keep disk</Radio>
-                  <Radio value="delete">Delete disk</Radio>
-                </RadioGroup>
-              </fieldset>
-              <h3 className={headingStyle}>Size (GiB)</h3>
-              <h3 className={headingStyle}>Configuration options</h3>
-            </div>
-            <hr className="border-gray-400" />
-            <div className="p-8">
-              <h3 className={headingStyle}>Relevant docs</h3>
-            </div>
-          </div>
-          <footer className="modal-footer">
-            {/* TODO: not supposed to be a ghost button */}
-            <Button variant="ghost" className="mr-2.5" onClick={onDismiss}>
-              Cancel
-            </Button>
-            <Button>Add disk</Button>
-          </footer>
-        </Form>
-      </Formik>
-    </Dialog>
+      <SideModal.Section>
+        <Formik initialValues={{ 'deletion-rule': '' }} onSubmit={() => {}}>
+          <Form id="new-disk-form">
+            <h3 className={headingStyle}>Name</h3>
+            <h3 className={headingStyle}>Description</h3>
+            <h3 className={headingStyle}>Type</h3>
+            <h3 className={headingStyle}>Source type</h3>
+            <fieldset>
+              <legend className={headingStyle}>Deletion rule</legend>
+              <RadioGroup name="deletion-rule" column>
+                <Radio value="keep">Keep disk</Radio>
+                <Radio value="delete">Delete disk</Radio>
+              </RadioGroup>
+            </fieldset>
+            <h3 className={headingStyle}>Size (GiB)</h3>
+            <h3 className={headingStyle}>Configuration options</h3>
+          </Form>
+        </Formik>
+      </SideModal.Section>
+      <SideModal.Docs>
+        <a href="#/">Formatting a persistent disk</a>
+        <a href="#/">Deletion Rules</a>
+        <a href="#/">Disk naming</a>
+      </SideModal.Docs>
+      <SideModal.Footer>
+        {/* TODO: not supposed to be a ghost button */}
+        <Button variant="ghost" className="mr-2.5" onClick={onDismiss}>
+          Cancel
+        </Button>
+        <Button form="new-disk-form">Add disk</Button>
+      </SideModal.Footer>
+    </SideModal>
   )
 }
