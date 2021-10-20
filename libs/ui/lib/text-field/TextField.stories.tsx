2c9c52048606f4d0741935b8a2f8ecbef24c694f
Justin Bennett <oss@just-be.dev>
2021-10-20 19:05:02 -0400

    Fix text field

--- libs/ui/lib/text-field/TextField.stories.tsx
+++ libs/ui/lib/text-field/TextField.stories.tsx
@@ -1,7 +1,9 @@
+import { FormikDecorator } from '../../util/formik-decorator'
 import { TextField } from './TextField'
 
 export default {
   component: TextField,
+  decorators: [FormikDecorator('')],
 }
 
 export const Default = {}
