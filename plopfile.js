3a4f601ed29c263218c314658745a53230a5f5c1
David Crespo <david-crespo@users.noreply.github.com>
2021-10-21 21:43:06 -0500

    Bring back plop codegen script for Storybook stories (#449)
    * add plop script for stories
    
    * Update plop, storybook format
    
    Co-authored-by: Justin Bennett <oss@just-be.dev>

---
+++ plopfile.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+/**
+ * @typedef { import("plop").NodePlopAPI } Plop
+ */
+
+const path = require('path')
+
+const UI_ROOT_DIR = 'libs/ui/lib'
+
+/** @type {(plop: Plop) => void} */
+module.exports = (plop) => {
+  plop.setGenerator('stories', {
+    description: 'Generate a React component, a test file, and a story',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name?',
+      },
+      {
+        type: 'input',
+        name: 'subdir',
+        message: `Directory within ${UI_ROOT_DIR}? (leave blank for root)`,
+      },
+    ],
+    actions: (args) => {
+      const dir = path.join(UI_ROOT_DIR, args.subdir)
+      return [
+        {
+          type: 'add',
+          path: '{{dir}}/{{name}}.stories.tsx',
+          templateFile: 'plop/stories-csf.hbs',
+          data: { dir },
+        },
+      ]
+    },
+  })
+}
