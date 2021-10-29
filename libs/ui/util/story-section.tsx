003b77d0faa93bcd90fbfedd15bc78f8bf1dfe25
Justin Bennett <oss@just-be.dev>
2021-10-29 16:01:50 -0400

    Add icons story (#493)

--- libs/ui/util/story-section.tsx
+++ libs/ui/util/story-section.tsx
@@ -1,6 +1,7 @@
 import { capitalize } from 'app/util/str'
 import type { ReactNode } from 'react'
 import React from 'react'
+import cn from 'classnames'
 
 /**
  * This is a utility component that helps prettify sections of stories when there are a lot of
@@ -9,12 +10,14 @@ import React from 'react'
 export const Section = ({
   title,
   children,
+  className,
 }: {
   title: string
   children: ReactNode
+  className?: string
 }) => (
-  <section className="mb-8 mr-8">
-    <h2 className="text-display-2xl text-green-500 border-green-800 border-b pb-4 mb-4">
+  <section className={cn(className, 'mb-8 mr-8')}>
+    <h2 className="text-display-xl text-green-500 border-green-800 border-b pb-4 mb-4">
       {capitalize(title)}
     </h2>
     {children}
