99e24717095dde5074cd6f2055419cace2609e83
David Crespo <crespo.dm@gmail.com>
2021-11-16 11:32:57 -0600

    make it look like the design sort of

--- app/pages/instances/Metrics.tsx
+++ app/pages/instances/Metrics.tsx
@@ -1,43 +1,62 @@
 import React from 'react'
-import { VictoryChart } from 'victory-chart/es'
-import { VictoryArea } from 'victory-area/es'
-import { VictoryLine } from 'victory-line/es'
-import { VictoryTheme } from 'victory-core/es'
+import { VictoryChart } from 'victory-chart'
+import { VictoryArea } from 'victory-area'
+import { VictoryLine } from 'victory-line'
 
 const data = [
-  { name: '9:50', amt: 200, limit: 600 },
-  { name: '10:00', amt: 300, limit: 600 },
-  { name: '10:10', amt: 500, limit: 600 },
-  { name: '10:20', amt: 450, limit: 600 },
-  { name: '10:30', amt: 650, limit: 800 },
-  { name: '10:40', amt: 550, limit: 800 },
-  { name: '10:50', amt: 600, limit: 800 },
+  { x: '9:50', y: 200 },
+  { x: '10:00', y: 300 },
+  { x: '10:10', y: 500 },
+  { x: '10:20', y: 450 },
+  { x: '10:30', y: 650 },
+  { x: '10:40', y: 550 },
+  { x: '10:50', y: 600 },
+]
+
+const capData = [
+  { x: '9:50', y: 600 },
+  { x: '10:00', y: 600 },
+  { x: '10:10', y: 600 },
+  { x: '10:20', y: 600 },
+  { x: '10:30', y: 800 },
+  { x: '10:40', y: 800 },
+  { x: '10:50', y: 800 },
 ]
 
 export default function InstanceMetrics() {
   return (
-    // <ComposedChart
-    //   width={600}
-    //   height={300}
-    //   data={data}
-    //   margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-    //   className="mt-16"
-    // >
-    //   <CartesianGrid stroke="#1D2427" vertical={false} />
-    //   <Area dataKey="amt" stroke="#2F8865" fillOpacity={1} fill="#112725" />
-    //   <Line
-    //     dataKey="limit"
-    //     stroke="#48D597"
-    //     strokeDasharray="3 3"
-    //     dot={false}
-    //     type="stepBefore"
-    //   />
-    //   <XAxis dataKey="name" />
-    //   <YAxis orientation="right" />
-    // </ComposedChart>
-    <VictoryChart theme={VictoryTheme.material}>
-      <VictoryArea />
-      <VictoryLine />
-    </VictoryChart>
+    <div style={{ width: 480, height: 360 }}>
+      <VictoryChart
+        theme={{
+          dependentAxis: {
+            orientation: 'right',
+            style: {
+              grid: { stroke: '#1D2427' },
+              tickLabels: { fill: '#ccc' },
+              axis: { stroke: '#1D2427' },
+            },
+          },
+          independentAxis: {
+            style: {
+              grid: { stroke: 'none' },
+              tickLabels: { fill: '#ccc' },
+              axis: { stroke: '#1D2427' },
+            },
+          },
+        }}
+      >
+        <VictoryArea
+          data={data}
+          style={{ data: { fill: '#112725', stroke: '#2F8865' } }}
+        />
+        <VictoryLine
+          data={capData}
+          style={{
+            data: { stroke: '#48D597', strokeWidth: 1, strokeDasharray: '4,4' },
+          }}
+          interpolation="stepBefore"
+        />
+      </VictoryChart>
+    </div>
   )
 }
