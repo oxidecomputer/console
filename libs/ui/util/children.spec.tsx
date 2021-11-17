83b1dcd4c6687643c016272d3a8a1e11373dfbb9
Justin Bennett <oss@just-be.dev>
2021-11-17 12:01:37 -0500

    Add pluckAllOfType and flattenChildren

--- libs/ui/util/children.spec.tsx
+++ libs/ui/util/children.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { isOneOf, pluckType } from './children'
+import { flattenChildren, isOneOf, pluckAllOfType, pluckType } from './children'
 
 const TestA = () => (
   <div>
@@ -11,6 +11,44 @@ const TestB = () => (
     <span>Test B </span>
   </div>
 )
+
+describe('flattenChildren', () => {
+  it('should not alter children without fragments', () => {
+    const input = (
+      <div id="wrapper">
+        <TestA />
+        <TestB />
+      </div>
+    )
+    const flattened = flattenChildren(input)
+    const childArray = React.Children.toArray(input)
+    expect(flattened).toEqual(childArray)
+  })
+
+  it('should unwrap children from fragments', () => {
+    const input = (
+      <>
+        <TestA />
+        <TestB />
+      </>
+    )
+    const flattened = flattenChildren(input)
+    const childArray = React.Children.toArray(input.props.children)
+    expect(flattened).toEqual(childArray)
+  })
+})
+
+it('should fail to match an element wrapped in a fragment', () => {
+  const input = (
+    <>
+      <TestA />
+      <TestB />
+    </>
+  )
+  const flattened = flattenChildren(input)
+  const childArray = React.Children.toArray(input)
+  expect(flattened).not.toEqual(childArray)
+})
 describe('pluckType', () => {
   it('Should remove a component of a given type from its children', () => {
     const testA = <TestA />
@@ -25,6 +63,32 @@ describe('pluckType', () => {
     const childArray: JSX.Element[] = []
     const plucked = pluckType(childArray, TestA)
     expect(plucked).toEqual(null)
+    expect(childArray).toEqual([])
+  })
+})
+
+describe('pluckAllOfType', () => {
+  it('should return and remove all components of a given set of children', () => {
+    const testA = <TestA />
+    const testB = <TestB />
+    const childArray = [testA, testB, testA, 'hello']
+    const plucked = pluckAllOfType(childArray, TestA)
+    expect(plucked).toEqual([testA, testA])
+    expect(childArray).toEqual([testB, 'hello'])
+  })
+
+  it('should return empty array when there are no matches', () => {
+    const testA = <TestA />
+    const childArray = [testA, 'test', testA, 'hello']
+    const plucked = pluckAllOfType(childArray, TestB)
+    expect(plucked).toEqual([])
+    expect(childArray).toEqual([testA, 'test', testA, 'hello'])
+  })
+
+  it('should not fail when children is empty', () => {
+    const childArray: JSX.Element[] = []
+    const plucked = pluckAllOfType(childArray, TestA)
+    expect(plucked).toEqual([])
     expect(childArray).toEqual([])
   })
 })
