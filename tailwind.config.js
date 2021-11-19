3a54cff441157d3226ff33ec1f28a77337003434
Justin Bennett <oss@just-be.dev>
2021-11-19 16:21:43 -0500

    Swap the order of responsive variants (#523)
    * Swap the order of responsive variants
    
    * try having explicit breakpoint directions
    
    * Correct sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
   content: ['./libs/ui/**/*.{ts,tsx,mdx}', './app/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      screens: {
+        'xl-': { max: '1535px' },
+        'lg-': { max: '1279px' },
+        'md-': { max: '1023px' },
+        'sm-': { max: '767px' },
+        'sm+': { min: '640px' },
+        'md+': { min: '768px' },
+        'lg+': { min: '1024px' },
+        'xl+': { min: '1280px' },
+        '2xl+': { min: '1536px' },
+        sm: { min: '640px', max: '767px' },
+        md: { min: '768px', max: '1023px' },
+        lg: { min: '1024px', max: '1279px' },
+        xl: { min: '1280px', max: '1535px' },
+      },
       borderRadius: {
         DEFAULT: '.0625rem',
       },
