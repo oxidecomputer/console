24804c2acadab9d29734f3465a3c4e7aaaedbeab
Justin Bennett <oss@just-be.dev>
2021-12-03 01:01:07 -0500

    Bump border color a tiny bit

--- app/layouts/helpers.tsx
+++ app/layouts/helpers.tsx
@@ -5,7 +5,7 @@ import { classed } from '@oxide/ui'
 import cn from 'classnames'
 
 export const PageContainer = classed.div`grid h-screen grid-cols-[14rem,auto]`
-export const Sidebar = classed.div`pb-6 pt-6 overflow-auto bg-gray-800 border-r border-gray-400 px-3`
+export const Sidebar = classed.div`pb-6 pt-6 overflow-auto bg-gray-800 border-r border-gray-450 px-3`
 export const ContentPane = classed.div`overflow-auto pt-14 pb-2 grid grid-cols-[2.5rem,auto,2.5rem] children:grid-col-2 auto-rows-min gap-y-2`
 
 export const NavList = (props: { children: React.ReactNode }) => (
