e9ca14fe04d0486391dfab976481d95f09fdb66a
Justin Bennett <oss@just-be.dev>
2021-12-07 10:10:39 -0500

    Enable page or component level pagination

--- app/layouts/helpers.tsx
+++ app/layouts/helpers.tsx
@@ -2,4 +2,4 @@ import { classed } from '@oxide/ui'
 
 export const PageContainer = classed.div`grid h-screen grid-cols-[14rem,auto]`
 export const Sidebar = classed.div`pb-6 pt-6 overflow-auto bg-gray-800 border-r border-gray-450 px-3`
-export const ContentPane = classed.div`overflow-auto pt-14 pb-2 grid grid-cols-[2.5rem,auto,2.5rem] children:grid-col-2 auto-rows-min`
+export const ContentPane = classed.div`overflow-auto pt-14 pb-2 grid grid-cols-[2.5rem,auto,2.5rem] children:grid-col-2 auto-rows-min relative`
--- libs/ui/lib/pagination/Pagination.tsx
+++ libs/ui/lib/pagination/Pagination.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { DirectionLeftIcon, DirectionRightIcon } from '../icons'
+import cn from 'classnames'
 
 interface PageInputProps {
   number: number
+  className?: string
 }
-const PageInput = ({ number }: PageInputProps) => {
+const PageInput = ({ number, className }: PageInputProps) => {
   return (
-    <span className="ring-1 ring-inset ring-gray-400 text-gray-100 inline-flex items-baseline uppercase text-mono-sm rounded-sm h-4 py-[1px] px-[3px] whitespace-nowrap">
+    <span
+      className={cn(
+        'ring-1 ring-inset ring-gray-400 text-gray-100 uppercase text-mono-sm rounded-sm h-4 pb-[3px] pt-[1px] px-[3px] whitespace-nowrap',
+        className
+      )}
+    >
       {number}
     </span>
   )
 }
 
 interface PaginationProps {
-  fullWidth?: boolean
+  pageLevel?: boolean
   pageSize: number
   currentPage: number
   numPages: number
@@ -21,6 +28,7 @@ interface PaginationProps {
   onPrev: () => void
 }
 export const Pagination = ({
+  pageLevel,
   pageSize,
   currentPage,
   numPages,
@@ -30,36 +38,48 @@ export const Pagination = ({
   const hasPrev = currentPage > 1
   const hasNext = numPages > currentPage
   return (
-    <div className="flex space-between text-mono-sm uppercase text-gray-100">
-      <span className="flex-grow text-gray-200">
-        rows per page <PageInput number={pageSize} />
-      </span>
-      <span className="space-x-2 flex items-center">
-        <button
-          className="flex items-center"
-          disabled={!hasPrev}
-          onClick={onPrev}
-        >
-          <DirectionLeftIcon
-            title="previous page"
-            className={hasPrev ? 'text-gray-50' : 'text-gray-400'}
-          />
-        </button>
-        <span>
-          <PageInput number={currentPage} /> of{' '}
-          <span className="text-gray-50">{numPages}</span>
+    <>
+      {pageLevel && (
+        <hr className="absolute bottom-14 border-t w-full !col-span-3 border-gray-500" />
+      )}
+      <div
+        className={cn(
+          'flex space-between text-mono-sm uppercase text-gray-100',
+          (pageLevel &&
+            'absolute bottom-0 w-full !col-start-2 !col-end-2 py-5') ||
+            'mt-2.5'
+        )}
+      >
+        <span className="flex-grow text-gray-200 flex-inline">
+          rows per page <PageInput number={pageSize} />
         </span>
-        <button
-          className="flex items-center"
-          disabled={!hasNext}
-          onClick={onNext}
-        >
-          <DirectionRightIcon
-            title="next page"
-            className={hasNext ? 'text-gray-50' : 'text-gray-400'}
-          />
-        </button>
-      </span>
-    </div>
+        <span className="space-x-2 flex items-center">
+          <button
+            className="flex items-center"
+            disabled={!hasPrev}
+            onClick={onPrev}
+          >
+            <DirectionLeftIcon
+              title="previous page"
+              className={hasPrev ? 'text-gray-50' : 'text-gray-400'}
+            />
+          </button>
+          <span>
+            <PageInput number={currentPage} /> of{' '}
+            <span className="text-gray-50">{numPages}</span>
+          </span>
+          <button
+            className="flex items-center"
+            disabled={!hasNext}
+            onClick={onNext}
+          >
+            <DirectionRightIcon
+              title="next page"
+              className={hasNext ? 'text-gray-50' : 'text-gray-400'}
+            />
+          </button>
+        </span>
+      </div>
+    </>
   )
 }
