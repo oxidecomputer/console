27424fb9b438a68ee695f4ab4441476b52fb0f10
Justin Bennett <oss@just-be.dev>
2021-12-09 14:57:06 -0500

    revert most of the helpers back to using classed

--- app/layouts/helpers.tsx
+++ app/layouts/helpers.tsx
@@ -2,6 +2,7 @@ import './helpers.css'
 import React from 'react'
 import cn from 'classnames'
 import { PaginationProvider } from '@oxide/pagination'
+import { classed } from '@oxide/ui'
 
 type Div = React.FC<JSX.IntrinsicElements['div']>
 
@@ -11,12 +12,6 @@ export const PageContainer: Div = ({ className, children, ...props }) => (
   </div>
 )
 
-export const Sidebar: Div = ({ className, ...props }) => (
-  <div className={cn('ox-sidebar', className)} {...props} />
-)
-export const ContentPane: Div = ({ className, ...props }) => (
-  <div className={cn('ox-content-pane', className)} {...props} />
-)
-export const PaginationContainer: Div = ({ className, ...props }) => (
-  <div className={cn('ox-pagination-container', className)} {...props} />
-)
+export const Sidebar = classed.div`ox-sidebar`
+export const ContentPane = classed.div`ox-content-pane`
+export const PaginationContainer = classed.div`ox-pagination-container`
