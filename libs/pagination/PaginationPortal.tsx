577ee08f0e376fbdda5d558cf946e3e2b01c2ceb
Justin Bennett <git@just-be.dev>
2021-12-09 23:29:55 -0500

    remove unused import

--- libs/pagination/PaginationPortal.tsx
+++ libs/pagination/PaginationPortal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 
 interface PaginationContextType {
   paginationComponent: JSX.Element | null
