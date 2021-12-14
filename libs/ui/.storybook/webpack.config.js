5333c30f62424347be4ef5788d690746fbd73bd5
David Crespo <david.crespo@oxidecomputer.com>
2021-12-13 22:42:32 -0600

    dummy change to trigger another gcp deploy

--- libs/ui/.storybook/webpack.config.js
+++ libs/ui/.storybook/webpack.config.js
@@ -11,7 +11,6 @@ module.exports = async ({ config }) => {
   )
   config.resolve.extensions.push('.mdx')
   config.resolve.plugins = resolvePlugins
-
   config.resolve.fallback = {
     ...(config.resolve.fallback || {}),
     path: require.resolve('path-browserify'),
