9f69a483717a797462f5bcdf77862f0d38add969
Justin Bennett <oss@just-be.dev>
2021-12-17 13:50:28 -0500

    Clean up styles slightly

--- app/layouts/helpers.tsx
+++ app/layouts/helpers.tsx
@@ -4,5 +4,5 @@ import { classed } from '@oxide/ui'
 export const PageContainer = classed.div`ox-page-container`
 export const Sidebar = classed.div`ox-sidebar`
 export const ContentPaneWrapper = classed.div`ox-content-pane-wrapper`
-export const ContentPane = classed.div`ox-content-pane with-content-gutters`
-export const PaginationContainer = classed.div`ox-pagination-container with-content-gutters`
+export const ContentPane = classed.div`ox-content-pane`
+export const PaginationContainer = classed.div`ox-pagination-container`
