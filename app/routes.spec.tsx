438e56bdb7302c05018ece8012673f532e533047
David Crespo <david-crespo@users.noreply.github.com>
2022-01-04 16:42:14 -0600

    Render the whole app in page-level tests (#565)
    * proof of concept for making all page-level tests into integration tests
    
    * same treatment for project create
    
    * tweaks, eliminate all warnings
    
    * automatically update packer-id
    
    * tip from good guy KCD cleans things up quite a bit
    
    https://github.com/kentcdodds/bookshelf/blob/8cbbd999d/src/test/app-test-utils.js#L15
    
    * no longer need to separate *CreatePage and *CreateForm
    
    * minor test tweaks
    
    * automatically update packer-id
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- app/routes.spec.tsx
+++ app/routes.spec.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { matchRoutes } from 'react-router'
-import { renderWithRouter } from './test-utils'
+import { renderAppAt } from './test-utils'
 import fetchMock from 'fetch-mock'
 
 import { projects } from '@oxide/api-mocks'
 
-import { routes, getRouteConfig } from './routes'
+import { getRouteConfig } from './routes'
 
 describe('routes', () => {
   afterEach(() => {
@@ -15,7 +15,7 @@ describe('routes', () => {
 
   it('should render successfully', async () => {
     fetchMock.get('/api/organizations/maze-war/projects', projects)
-    const { findAllByText } = renderWithRouter(routes)
+    const { findAllByText } = renderAppAt('/')
     await findAllByText(projects.items[0].name)
   })
 })
