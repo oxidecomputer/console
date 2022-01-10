574e1646613a57e08376ec53a51c0b746d1f6ca4
David Crespo <david-crespo@users.noreply.github.com>
2022-01-10 10:51:42 -0600

    `yarn gen-api` + some tweaks to API client gen (#600)
    * yarn gen-api and some tweaks to API client gen
    
    * automatically update packer-id
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/generate_api_client.sh
+++ tools/generate_api_client.sh
@@ -15,10 +15,13 @@ git -C '../omicron' checkout "$API_VERSION"
 cp ../omicron/openapi/nexus.json $SPEC_FILE
 cp ../omicron/tools/oxapi_demo packer/oxapi_demo
 
-yarn swagger-typescript-api -p $SPEC_FILE -o $GEN_DIR \
-  --union-enums \
-  --extract-request-params \
-  --extract-request-body # weirdly this doesn't do anything
+# copy nexus config with some minor modifications...
+cat ../omicron/nexus/examples/config.toml |
+  sed 's/127.0.0.1:12220/0.0.0.0:8888/' |
+  sed 's/127.0.0.1:12221/0.0.0.0:12221/' |
+  sed 's/127.0.0.1:32221/0.0.0.0:26257/' > packer/omicron.toml 
+
+yarn swagger-typescript-api -p $SPEC_FILE -o $GEN_DIR --union-enums --extract-request-params
 
 sed -i '' 's/organizationName/orgName/g' "$GEN_DIR/Api.ts"
 yarn fmt --loglevel error "{$GEN_DIR,$SPEC_FILE}"
