3940dd00820c58b7458318e88b60b78e77a015ea
David Crespo <david.crespo@oxidecomputer.com>
2022-01-12 12:07:10 -0600

    accidentally got rid of a comment I wanted

--- app/main.tsx
+++ app/main.tsx
@@ -47,6 +47,8 @@ async function startMockAPI() {
 }
 
 if (process.env.NODE_ENV !== 'production' && process.env.MSW) {
+  // MSW has NODE_ENV !== prod built into it, but let's be extra safe
+  // need to defer requests until after the mock server starts up
   startMockAPI().then(render)
 } else {
   render()
