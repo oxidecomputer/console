0dc832a72503d0dda32557c2186ee5ee5e521bb3
Justin Bennett <oss@just-be.dev>
2022-01-24 22:15:23 -0500

    Clean up theme files

--- tools/build_themes.sh
+++ tools/build_themes.sh
@@ -6,7 +6,7 @@ STYLES_PATH=libs/ui/styles
 TOKENS_PATH=$STYLES_PATH/.tokens
 
 npx token-transformer $STYLES_PATH/tokens.json $TOKENS_PATH/main.json global,colors,main
-npx token-transformer $STYLES_PATH/tokens.json $TOKENS_PATH/operator-mode.json global,colors,operator-mode
+npx token-transformer $STYLES_PATH/tokens.json $TOKENS_PATH/operator-mode.json colors,operator-mode
 
 node -r esbuild-register tools/convert-tokens
 prettier -w $STYLES_PATH
