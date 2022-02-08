eec36d716a9bc41069e7775e6e856ac7971514ca
Justin Bennett <oss@just-be.dev>
2022-02-08 13:19:22 -0500

    Do formatting

--- libs/ui/lib/table/Table.tsx
+++ libs/ui/lib/table/Table.tsx
@@ -28,7 +28,7 @@ Table.HeadCell = ({ className, children, ...props }: TableHeadCellProps) => (
   <th
     className={cn(
       className,
-      'border border-default border-x-0 bg-secondary text-left text-mono-sm children:first:border-0 pl-0'
+      'border border-x-0 pl-0 text-left text-mono-sm bg-secondary border-default children:first:border-0'
     )}
     {...props}
   >
@@ -79,11 +79,11 @@ Table.Cell = ({ className, children, ...props }: TableCellProps) => (
   <td
     className={cn(
       className,
-      'border-default h-16 children:first:border-l-0 children:last:-mr-[1px] selected:bg-accent-secondary selected:text-accent pl-0'
+      'selected:bg-accent-secondary h-16 pl-0 border-default children:first:border-l-0 children:last:-mr-[1px] selected:text-accent'
     )}
     {...props}
   >
-    <div className="flex items-center border-l border-b border-secondary h-16 -my-[1px] -mr-[2px] px-3  selected:border-accent-secondary">
+    <div className="-my-[1px] -mr-[2px] flex h-16 items-center border-l border-b px-3 border-secondary  selected:border-accent-secondary">
       {children}
     </div>
   </td>
