3552173f14d40fa6263dec316b9f31d5f1471d65
Justin Bennett <oss@just-be.dev>
2022-02-08 13:47:53 -0500

    Remove font-mono references

--- libs/ui/lib/radio/Radio.tsx
+++ libs/ui/lib/radio/Radio.tsx
@@ -56,7 +56,7 @@ export function RadioCard({ children, className, ...inputProps }: RadioProps) {
   // HACK: This forces the focus states for storybook stories
   const focus = className?.includes(':focus') ? ':focus' : ''
   return (
-    <label className="font-mono inline-flex items-center">
+    <label className="inline-flex items-center">
       <Field
         className={cn(focus, 'peer sr-only')}
         type="radio"
