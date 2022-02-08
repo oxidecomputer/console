286a1fcd079d9c326a2d09ff89bf266584c70f09
David Crespo <david.crespo@oxidecomputer.com>
2022-02-08 11:53:42 -0500

    memoize tableData more goodly

--- libs/table/QueryTable.tsx
+++ libs/table/QueryTable.tsx
@@ -123,11 +123,7 @@ const makeQueryTable = <A extends ApiListMethods, M extends keyof A>(
       options
     )
 
-    const tableData = useMemo(
-      () => (data as any)?.items || [],
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [(data as any)?.items]
-    )
+    const tableData = useMemo(() => (data as any)?.items || [], [data])
 
     const getRowId = useCallback(
       (row, relativeIndex, parent) => {
