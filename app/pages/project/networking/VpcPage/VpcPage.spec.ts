e94407074aaddd0cad2adb3efce617eb879af45d
David Crespo <david.crespo@oxidecomputer.com>
2022-02-14 12:29:27 -0500

    extend timeout. sad

--- app/pages/project/networking/VpcPage/VpcPage.spec.ts
+++ app/pages/project/networking/VpcPage/VpcPage.spec.ts
@@ -121,7 +121,7 @@ describe('VpcPage', () => {
       for (const { name } of defaultFirewallRules) {
         screen.getByText(name)
       }
-    }, 20000)
+    }, 25000)
 
     it('edit works', async () => {
       renderAppAt('/orgs/maze-war/projects/mock-project/vpcs/mock-vpc')
