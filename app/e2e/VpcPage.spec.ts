bf692cef4722ce79b6b11232e4b0221c23cb8e99
David Crespo <david.crespo@oxidecomputer.com>
2022-02-17 18:37:43 -0500

    move last vpc firewall e2e test to playwright

--- app/e2e/VpcPage.spec.ts
+++ app/e2e/VpcPage.spec.ts
@@ -1,9 +1,7 @@
-import type { Page } from '@playwright/test'
 import { test, expect } from '@playwright/test'
-import { roundToNearestMinutesWithOptions } from 'date-fns/fp'
 
 test.describe('VpcPage', () => {
-  test('can nav from root by clicking', async ({ page }) => {
+  test('can nav to VpcPage from /', async ({ page }) => {
     await page.goto('/')
     await page.click('table :text("mock-project")')
     await page.click('a:has-text("Networking")')
@@ -48,11 +46,11 @@ test.describe('VpcPage', () => {
     for (const name of defaultRules) {
       await expect(page.locator(`text="${name}"`)).toBeVisible()
     }
-    const rows = await page.locator('tbody >> tr')
+    const rows = page.locator('tbody >> tr')
     await expect(rows).toHaveCount(4)
 
     const modal = page.locator('text="Create firewall rule"')
-    expect(modal).not.toBeVisible()
+    await expect(modal).not.toBeVisible()
 
     // open modal
     await page.locator('text="New rule"').click()
@@ -112,6 +110,86 @@ test.describe('VpcPage', () => {
 
     await expect(rows).toHaveCount(5)
     for (const name of defaultRules) {
+      await expect(page.locator(`text="${name}"`)).toBeVisible()
+    }
+  })
+
+  test('can update firewall rule', async ({ page }) => {
+    await page.goto('/orgs/maze-war/projects/mock-project/vpcs/mock-vpc')
+    await page.locator('text="Firewall Rules"').click()
+
+    const rows = await page.locator('tbody >> tr')
+    await expect(rows).toHaveCount(4)
+
+    // allow-icmp is the one we're doing to change
+    const oldNameCell = page.locator('td >> text="allow-icmp"')
+    expect(oldNameCell).toBeVisible()
+
+    const newNameCell = page.locator('td >> text="new-rule-name"')
+    expect(newNameCell).not.toBeVisible()
+
+    const modal = page.locator('text="Edit firewall rule"')
+    await expect(modal).not.toBeVisible()
+
+    // click more button on allow-icmp row to get menu, then click Edit
+    page.locator('tr:has-text("allow-icmp") button:has-text("More")').click()
+    // filter visible to distinguish from all the hidden menus' Edit button
+    await page.locator('text="Edit" >> visible=true').click()
+
+    // modal is now open
+    await expect(modal).toBeVisible()
+
+    // TODO: get these by their label when that becomes easier to do
+
+    // name is populated
+    await expect(page.locator('input[name=name]')).toHaveValue('allow-icmp')
+
+    // priority is populated
+    await expect(page.locator('input[name=priority]')).toHaveValue('65534')
+
+    // protocol is populated
+    await expect(page.locator('label >> text=ICMP')).toBeChecked()
+    await expect(page.locator('label >> text=TCP')).not.toBeChecked()
+    await expect(page.locator('label >> text=UDP')).not.toBeChecked()
+
+    // targets default vpc
+    // screen.getByRole('cell', { name: 'vpc' })
+    // screen.getByRole('cell', { name: 'default' })
+
+    // update name
+    await page.fill('input[name=name]', 'new-rule-name')
+
+    // add host filter
+    await page.locator('button:has-text("Host type")').click()
+    await page.locator('[role=option] >> text="Instance"').click()
+    await page.fill('text="Value"', 'edit-filter-instance')
+    await page.locator('text="Add host filter"').click()
+
+    // new host is added to hosts table
+    await expect(
+      page.locator('td:has-text("edit-filter-instance")')
+    ).toBeVisible()
+
+    // submit the form
+    await page.locator('text="Update rule"').click()
+
+    // modal closes again
+    await expect(modal).not.toBeVisible()
+
+    // table refetches and now includes the updated rule name, not the old name
+    await expect(newNameCell).toBeVisible()
+    expect(oldNameCell).not.toBeVisible()
+
+    await expect(rows).toHaveCount(4)
+
+    // new target shows up in target cell
+    await expect(
+      page.locator('text=instanceedit-filter-instanceICMP')
+    ).toBeVisible()
+
+    // other 3 rules are still there
+    const rest = defaultRules.filter((r) => r !== 'allow-icmp')
+    for (const name of rest) {
       await expect(page.locator(`text="${name}"`)).toBeVisible()
     }
   })
