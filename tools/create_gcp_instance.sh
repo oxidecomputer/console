c4735719761d282e7069db1f2779fadc74d93684
David Crespo <david-crespo@users.noreply.github.com>
2022-03-07 15:55:54 -0500

    Force packer bump to get new certs (#687)
    * force packer bump to get new certs
    
    * automatically update packer-id
    
    * format it back. sob
    
    * automatically update packer-id
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/create_gcp_instance.sh
+++ tools/create_gcp_instance.sh
@@ -41,7 +41,7 @@ retry 2 gcloud compute instances create "$INSTANCE_NAME" \
 	--description="Machine automatically generated from branch ${BRANCH_NAME} of the oxidecomputer/console git repo." \
 	--hostname="${INSTANCE_NAME}.internal.oxide.computer" \
 	--zone=$ZONE \
-	--image=packer-1644440038 \
+	--image=packer-1646679287 \
 	--maintenance-policy=TERMINATE \
 	--restart-on-failure \
 	--machine-type=$INSTANCE_TYPE \
