88401d2fc356f2658a867fe162f4b551e0dd61cf
David Crespo <david.crespo@oxidecomputer.com>
2022-03-16 15:44:11 -0500

    use zustand instead of plain context for quick actions global state

--- app/main.tsx
+++ app/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { SkipLink } from '@oxide/ui'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { routes } from './routes'
-import { QuickActionsProvider, ToastProvider } from './hooks'
+import { QuickActions, ToastProvider } from './hooks'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,10 +24,9 @@ function render() {
       <ToastProvider>
         <QueryClientProvider client={queryClient}>
           <ErrorBoundary>
-            <QuickActionsProvider>
-              <SkipLink id="skip-nav" />
-              <Router>{routes}</Router>
-            </QuickActionsProvider>
+            <QuickActions />
+            <SkipLink id="skip-nav" />
+            <Router>{routes}</Router>
           </ErrorBoundary>
         </QueryClientProvider>
       </ToastProvider>
