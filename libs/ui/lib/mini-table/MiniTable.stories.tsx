4b17b6d4bc81909a40ec3b47fa3123b912b58f76
Justin Bennett <oss@just-be.dev>
2022-03-21 11:16:51 -0400

    Add MiniTable component

---
+++ libs/ui/lib/mini-table/MiniTable.stories.tsx
@@ -0,0 +1,34 @@
+import { MiniTable } from './MiniTable'
+import type { ComponentProps } from 'react'
+import type { StoryObj } from '@storybook/react'
+import React from 'react'
+
+type Story = StoryObj<ComponentProps<typeof MiniTable>>
+
+export default {
+  component: MiniTable,
+} as Story
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <MiniTable.Header>
+          <MiniTable.HeadCell>Name</MiniTable.HeadCell>
+          <MiniTable.HeadCell>Source Type</MiniTable.HeadCell>
+          <MiniTable.HeadCell>Size</MiniTable.HeadCell>
+        </MiniTable.Header>
+        <MiniTable.Row>
+          <MiniTable.Cell>disk-1</MiniTable.Cell>
+          <MiniTable.Cell>Blank</MiniTable.Cell>
+          <MiniTable.Cell>128 GiB</MiniTable.Cell>
+        </MiniTable.Row>
+        <MiniTable.Row>
+          <MiniTable.Cell>disk-2</MiniTable.Cell>
+          <MiniTable.Cell>Blank</MiniTable.Cell>
+          <MiniTable.Cell>128 GiB</MiniTable.Cell>
+        </MiniTable.Row>
+      </>
+    ),
+  },
+}
---
+++ libs/ui/lib/mini-table/MiniTable.tsx
@@ -0,0 +1,51 @@
+import { flattenChildren, pluckFirstOfType } from '@oxide/util'
+import React from 'react'
+import { Table } from '../table/Table'
+import './mini-table.css'
+
+export interface MiniTableProps {
+  children: React.ReactNode
+}
+
+export function MiniTable({ children }: MiniTableProps) {
+  const childArray = flattenChildren(children)
+  const header = pluckFirstOfType(childArray, MiniTable.Header)
+  return (
+    <table className="ox-mini-table w-full border-separate">
+      {header}
+      <tbody>{childArray}</tbody>
+    </table>
+  )
+}
+
+interface MiniTableHeaderProps {
+  children: React.ReactNode
+}
+MiniTable.Header = ({ children }: MiniTableHeaderProps) => (
+  <Table.Header>
+    <Table.HeaderRow>{children}</Table.HeaderRow>
+  </Table.Header>
+)
+MiniTable.HeadCell = Table.HeadCell
+
+interface MiniTableRowProps {
+  children: React.ReactNode
+}
+MiniTable.Row = ({ children }: MiniTableRowProps) => {
+  return (
+    <tr className="is-selected children:border-default first:children:border-l children:last:border-b last:children:border-r">
+      {children}
+    </tr>
+  )
+}
+
+interface MiniTableCell {
+  children: React.ReactNode
+}
+MiniTable.Cell = ({ children }: MiniTableCell) => {
+  return (
+    <td>
+      <div>{children}</div>
+    </td>
+  )
+}
