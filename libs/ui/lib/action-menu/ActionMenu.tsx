0c3b64949a0bea61cc956870191223f7a8976750
Benjamin Leonard <hello@benleonard.co.uk>
2022-03-23 09:19:12 +0000

    Remove unnecessary cn()
    Co-authored-by: David Crespo <david-crespo@users.noreply.github.com>

--- libs/ui/lib/action-menu/ActionMenu.tsx
+++ libs/ui/lib/action-menu/ActionMenu.tsx
@@ -148,9 +148,7 @@ export function ActionMenu(props: ActionMenuProps) {
                   <React.Fragment key={label}>
                     <div>
                       <h3
-                        className={cn(
-                          'sticky top-0 z-20 px-4 py-2 text-mono-sm text-secondary bg-secondary'
-                        )}
+                        className="sticky top-0 z-20 px-4 py-2 text-mono-sm text-secondary bg-secondary"
                       >
                         {label}
                       </h3>
