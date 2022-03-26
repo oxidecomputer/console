470511b772cee18b2c1a76320c4ab641502880fb
Justin Bennett <oss@just-be.dev>
2022-03-25 22:48:37 -0400

    Fixup some issues with the page header

--- libs/ui/lib/page-header/PageHeader.tsx
+++ libs/ui/lib/page-header/PageHeader.tsx
@@ -1,22 +1,21 @@
 import type { ReactElement } from 'react'
-import { cloneElement } from 'react'
 import React from 'react'
 
 import { classed } from '@oxide/util'
 
-export const PageHeader = classed.header`flex items-center justify-between mb-16 mt-4`
+export const PageHeader = classed.header`flex items-center justify-between mb-24 mt-4`
 
 interface Props {
-  icon: ReactElement
-  children: React.ReactNode
+  icon?: ReactElement
+  children: string
 }
 
-export const PageTitle = ({ children, icon }: Props) => {
+export const PageTitle = ({ children: title, icon }: Props) => {
   return (
-    <h1 className="inline-flex items-center space-x-2 text-sans-3xl text-accent">
+    <h1 className="inline-flex items-center space-x-2 text-sans-3xl text-accent-secondary">
       <span className="mb-1 h-2.5 w-8 border-l border-b border-default" />
-      {cloneElement(icon, { className: 'text-accent-secondary' })}
-      <span>{children}</span>
+      {icon}
+      <span className="text-accent">{title}</span>
     </h1>
   )
 }
