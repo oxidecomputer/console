72258e7cd4cc6b834dfcfbce5eecb08eb03b4a01
Justin Bennett <oss@just-be.dev>
2022-03-28 18:43:33 -0400

    Convert org page

--- app/pages/OrgsPage.tsx
+++ app/pages/OrgsPage.tsx
@@ -1,6 +1,12 @@
 import React, { useMemo } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { buttonStyle, PageHeader, PageTitle, Folder24Icon } from '@oxide/ui'
+import {
+  buttonStyle,
+  PageHeader,
+  PageTitle,
+  Folder24Icon,
+  PageHeaderActions,
+} from '@oxide/ui'
 import { useQuickActions } from '../hooks'
 import { DateCell, linkCell, useQueryTable } from '@oxide/table'
 import { useApiQuery } from '@oxide/api'
@@ -29,10 +35,7 @@ const OrgsPage = () => {
 
   return (
     <>
-      <PageHeader className="mb-10">
-        <PageTitle icon={<Folder24Icon title="Organizations" />}>
-          Organizations
-        </PageTitle>
+      <PageHeaderActions>
         <div className="flex items-center">
           <Link
             to="new"
@@ -41,7 +44,7 @@ const OrgsPage = () => {
             New Organization
           </Link>
         </div>
-      </PageHeader>
+      </PageHeaderActions>
       <Table>
         <Column id="name" cell={linkCell((name) => `/orgs/${name}`)} />
         <Column id="description" />
