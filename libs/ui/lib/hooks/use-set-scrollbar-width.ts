b7603967d9751ac2f56d0bb74b74977fa36feda6
Justin Bennett <oss@just-be.dev>
2022-03-28 16:21:15 -0400

    Initialize hasScrollbar, add better docs

--- libs/ui/lib/hooks/use-set-scrollbar-width.ts
+++ libs/ui/lib/hooks/use-set-scrollbar-width.ts
@@ -3,6 +3,12 @@ import type { RefObject } from 'react'
 import { useLayoutEffect, useState } from 'react'
 import { debounce } from 'debounce'
 
+/**
+ * Hook to set the `--scrollbar-width` css variable. It's intended
+ * to be used once globally, likely in your main or app component.
+ *
+ * {@see ScrollbarWidth} for a shorthand for using this hook directly in JSX
+ */
 export const useSetScrollbarWidth = () => {
   const getScrollbarWidth = useGetScrollbarWidth()
   useLayoutEffect(() => {
@@ -14,6 +20,12 @@ export const useSetScrollbarWidth = () => {
   }, [])
 }
 
+/**
+ * Returns `true` if the provided ref has a scrollbar. It will
+ * re-check when the window is resized. It _won't_ re-check if
+ * the container size changes due to another element growing or
+ * shrinking in size around it though.
+ */
 export const useHasScrollbar = <C>(ref: RefObject<C>) => {
   const [hasScrollbar, setHasScrollbar] = useState(false)
   useLayoutEffect(() => {
@@ -23,13 +35,19 @@ export const useHasScrollbar = <C>(ref: RefObject<C>) => {
       const { scrollHeight, offsetHeight } = ref.current
       setHasScrollbar(scrollHeight > offsetHeight)
     }, 100)
+
+    // Go ahead and set the initial value
+    handleResize()
+
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [ref, setHasScrollbar])
   return hasScrollbar
 }
 
-/** Helper component to fire the hook */
+/**
+ * Helper component that calls {@link useSetScrollbarWidth} and returns null.
+ */
 export const ScrollbarWidth = () => {
   useSetScrollbarWidth()
   return null
