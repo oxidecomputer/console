d4f49e108dcb4fef659a4980bb8fe62700c961fe
Justin Bennett <oss@just-be.dev>
2022-03-28 15:36:05 -0400

    Add hook to detect presense of scrollbar

--- libs/ui/lib/hooks/use-set-scrollbar-width.ts
+++ libs/ui/lib/hooks/use-set-scrollbar-width.ts
@@ -1,5 +1,7 @@
 import useGetScrollbarWidth from '@utilityjs/use-get-scrollbar-width'
-import { useLayoutEffect } from 'react'
+import type { RefObject } from 'react'
+import { useLayoutEffect, useState } from 'react'
+import { debounce } from 'debounce'
 
 export const useSetScrollbarWidth = () => {
   const getScrollbarWidth = useGetScrollbarWidth()
@@ -10,6 +12,21 @@ export const useSetScrollbarWidth = () => {
       ?.style.setProperty('--scrollbar-width', getScrollbarWidth())
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+}
+
+export const useHasScrollbar = <C>(ref: RefObject<C>) => {
+  const [hasScrollbar, setHasScrollbar] = useState(false)
+  useLayoutEffect(() => {
+    const handleResize = debounce(() => {
+      if (!ref.current) return
+      // @ts-expect-error TODO figure out the proper types for `C` to make this work
+      const { scrollHeight, offsetHeight } = ref.current
+      setHasScrollbar(scrollHeight > offsetHeight)
+    }, 100)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [ref, setHasScrollbar])
+  return hasScrollbar
 }
 
 /** Helper component to fire the hook */
