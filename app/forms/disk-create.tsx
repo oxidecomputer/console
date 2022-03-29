a57ae303f9283e2c210935e1872b3e5fa3bca385
Justin Bennett <oss@just-be.dev>
2022-03-29 03:41:48 -0400

    Port new disk modal

--- app/forms/disk-create.tsx
+++ app/forms/disk-create.tsx
@@ -1,13 +1,16 @@
 import { DescriptionField, Form, NameField, TextField } from '@oxide/form'
 import { Divider } from '@oxide/ui'
+import { RadioField, Radio } from '@oxide/form'
 import React from 'react'
 import type { BaseFormProps } from './helpers/form-types'
 
 const values = {
   name: '',
   description: '',
-  ipv4Block: '',
-  ipv6Block: '',
+  size: 0,
+  type: '',
+  sourceType: '',
+  deletionRule: '',
 }
 
 CreateDiskForm.defaultProps = {
@@ -35,6 +38,16 @@ export function CreateDiskForm({
       <DescriptionField id="disk-description" />
       <Divider />
       <TextField id="disk-type" name="type" />
+      <RadioField id="disk-source-type" name="sourceType">
+        <Radio value="blank">Blank disk</Radio>
+        <Radio value="image">Image</Radio>
+        <Radio value="snapshot">Snapshot</Radio>
+      </RadioField>
+      <RadioField id="disk-deletion-rule" name="deletionRule">
+        <Radio value="keep">Keep disk</Radio>
+        <Radio value="delete">Delete disk</Radio>
+      </RadioField>
+      <TextField id="disk-size" name="size" label="Size (GiB)" type="number" />
       <Form.Actions>
         <Form.Submit>{title!}</Form.Submit>
         <Form.Cancel />
