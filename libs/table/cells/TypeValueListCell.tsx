30c9e7a620fc7c3b1984c1a5762ca33d049d54cc
David Crespo <david.crespo@oxidecomputer.com>
2022-04-01 15:38:21 -0500

    api bisect test 2

--- libs/table/cells/TypeValueListCell.tsx
+++ libs/table/cells/TypeValueListCell.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import type { Cell } from '.'
+import type { Cell, TypeValue } from '.'
 import { TypeValueCell } from '.'
 
-export const TypeValueListCell = ({
-  value,
-}: Cell<Array<{ type: string; value: string }>>) => (
+export const TypeValueListCell = ({ value }: Cell<TypeValue[]>) => (
   <div>
     {value.map((v, i) => (
       <TypeValueCell key={i} value={v} />
