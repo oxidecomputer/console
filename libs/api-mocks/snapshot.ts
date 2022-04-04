10faeeb213bdb73e6830fe086b121e121d943b1f
David Crespo <david.crespo@oxidecomputer.com>
2022-04-04 14:16:13 -0500

    add image and snapshot index endpoints to MSW

---
+++ libs/api-mocks/snapshot.ts
@@ -0,0 +1,46 @@
+import type { Snapshot } from '@oxide/api'
+import type { Json } from './json-type'
+import { project } from './project'
+
+export const snapshots: Json<Snapshot>[] = [
+  {
+    id: 'snapshot-id-1',
+    name: 'snapshot-1',
+    description: "it's a snapshot",
+    project_id: project.id,
+    time_created: new Date().toISOString(),
+    time_modified: new Date().toISOString(),
+    size: 1000,
+    disk_id: 'disk-id-1',
+  },
+  {
+    id: 'snapshot-id-2',
+    name: 'snapshot-2',
+    description: "it's a second snapshot",
+    project_id: project.id,
+    time_created: new Date().toISOString(),
+    time_modified: new Date().toISOString(),
+    size: 2000,
+    disk_id: 'disk-id-1',
+  },
+  {
+    id: 'snapshot-id-3',
+    name: 'snapshot-3',
+    description: "it's a third snapshot",
+    project_id: project.id,
+    time_created: new Date().toISOString(),
+    time_modified: new Date().toISOString(),
+    size: 3000,
+    disk_id: 'disk-id-1',
+  },
+  {
+    id: 'snapshot-id-4',
+    name: 'snapshot-4',
+    description: "it's a fourth snapshot",
+    project_id: project.id,
+    time_created: new Date().toISOString(),
+    time_modified: new Date().toISOString(),
+    size: 4000,
+    disk_id: 'disk-id-1',
+  },
+]
