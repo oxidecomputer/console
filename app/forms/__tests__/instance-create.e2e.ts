459548666b18aa78b14c2597826fdabe25139dcc
Justin Bennett <oss@just-be.dev>
2022-04-07 23:40:00 -0400

    Add e2e test for instance create

---
+++ app/forms/__tests__/instance-create.e2e.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Instance Create Form', () => {
+  test('can invoke instance create form from instances page', async ({
+    page,
+  }) => {
+    await page.goto('/orgs/maze-war/projects/mock-project/instances')
+    await page.locator('text="New Instance"').click()
+    await expect(page.locator('h1:has-text("Create instance")')).toBeVisible()
+
+    await page.fill('input[name=name]', 'mock-instance')
+    await page.locator('.ox-radio-card').nth(0).click()
+
+    await page.locator('input[value=ubuntu] ~ .ox-radio-card').click()
+
+    await page.locator('button:has-text("Create instance")').click()
+
+    await page.waitForNavigation()
+
+    expect(page.url()).toContain(
+      '/orgs/maze-war/projects/mock-project/instances/mock-instance'
+    )
+
+    await expect(page.locator('h1:has-text("mock-instance")')).toBeVisible()
+  })
+})
