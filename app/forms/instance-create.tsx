9466d70d28512cf476f7f4d9ee3a42c6a7ace7f9
David Crespo <david.crespo@oxidecomputer.com>
2022-04-08 21:50:53 -0500

    move DisksTableField to its own file to reduce noise in instant create

--- app/forms/instance-create.tsx
+++ app/forms/instance-create.tsx
@@ -1,9 +1,7 @@
-import { useField } from 'formik'
+import React from 'react'
 import type { Instance } from '@oxide/api'
 import { useApiMutation, useApiQueryClient } from '@oxide/api'
 import type { PrebuiltFormProps } from 'app/forms'
-import { CreateDiskForm } from 'app/forms/disk-create'
-import { AttachDiskForm } from 'app/forms/disk-attach'
 import {
   DescriptionField,
   Form,
@@ -13,17 +11,12 @@ import {
   TextField,
 } from '@oxide/form'
 import {
-  Button,
   CentOSResponsiveIcon,
   DebianResponsiveIcon,
   Divider,
-  Error16Icon,
   FedoraResponsiveIcon,
-  FieldLabel,
   FreeBSDResponsiveIcon,
-  MiniTable,
   RadioCard,
-  SideModal,
   Success16Icon,
   Tab,
   Tabs,
@@ -32,105 +25,8 @@ import {
   WindowsResponsiveIcon,
 } from '@oxide/ui'
 import { useParams, useToast } from 'app/hooks'
+import { DisksTableField } from 'app/components/DisksTableField'
 import filesize from 'filesize'
-import React, { useState } from 'react'
-import type { FormValues } from '.'
-
-type DiskTableItem =
-  | (FormValues<'disk-create'> & { type: 'create' })
-  | (FormValues<'disk-attach'> & { type: 'attach' })
-
-function DisksTableField() {
-  const [showDiskCreate, setShowDiskCreate] = useState(false)
-  const [showDiskAttach, setShowDiskAttach] = useState(false)
-
-  const [, { value: items = [] }, { setValue: setItems }] = useField<
-    DiskTableItem[]
-  >({ name: 'disks' })
-
-  return (
-    <>
-      <div className="max-w-lg">
-        <FieldLabel id="new-disks-label">{/* this was empty */}</FieldLabel>
-        {!!items.length && (
-          <MiniTable className="mb-4">
-            <MiniTable.Header>
-              <MiniTable.HeadCell>Name</MiniTable.HeadCell>
-              <MiniTable.HeadCell>Type</MiniTable.HeadCell>
-              {/* For remove button */}
-              <MiniTable.HeadCell className="w-12" />
-            </MiniTable.Header>
-            <MiniTable.Body>
-              {items.map((item, index) => (
-                <MiniTable.Row
-                  tabindex="0"
-                  aria-rowindex={index + 1}
-                  aria-label={`Name: ${item.name}, Type: ${item.type}`}
-                  key={item.name}
-                >
-                  <MiniTable.Cell>{item.name}</MiniTable.Cell>
-                  <MiniTable.Cell>{item.type}</MiniTable.Cell>
-                  <MiniTable.Cell>
-                    <Button
-                      variant="link"
-                      onClick={() =>
-                        setItems(items.filter((i) => i.name !== item.name))
-                      }
-                    >
-                      <Error16Icon title={`remove ${item.name}`} />
-                    </Button>
-                  </MiniTable.Cell>
-                </MiniTable.Row>
-              ))}
-            </MiniTable.Body>
-          </MiniTable>
-        )}
-
-        <div className="space-x-3">
-          <Button
-            variant="secondary"
-            size="sm"
-            onClick={() => setShowDiskCreate(true)}
-          >
-            Create new disk
-          </Button>
-          <Button
-            variant="secondary"
-            size="sm"
-            onClick={() => setShowDiskAttach(true)}
-          >
-            Attach existing disk
-          </Button>
-        </div>
-      </div>
-
-      <SideModal
-        id="create-disk-modal"
-        isOpen={showDiskCreate}
-        onDismiss={() => setShowDiskCreate(false)}
-      >
-        <CreateDiskForm
-          onSubmit={(values) => {
-            setItems([...items, { type: 'create', ...values }])
-            setShowDiskCreate(false)
-          }}
-        />
-      </SideModal>
-      <SideModal
-        id="attach-disk-modal"
-        isOpen={showDiskAttach}
-        onDismiss={() => setShowDiskAttach(false)}
-      >
-        <AttachDiskForm
-          onSubmit={(values) => {
-            setItems([...items, { type: 'attach', ...values }])
-            setShowDiskAttach(false)
-          }}
-        />
-      </SideModal>
-    </>
-  )
-}
 
 const values = {
   name: '',
