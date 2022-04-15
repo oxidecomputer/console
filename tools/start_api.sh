ee80eabb597ea28c1929892d8ac833bb717c0a10
Benjamin Naecker <ben@oxide.computer>
2022-04-15 16:08:35 +0000

    Use IPv6 localhost address for launching simulated sled agent

--- tools/start_api.sh
+++ tools/start_api.sh
@@ -66,7 +66,7 @@ run_in_pane 2 "$UTILS"
 run_in_pane 2 "set_pane_title sled-agent-sim"
 run_in_pane 2 "wait_for_up 12220"
 run_in_pane 2 "wait_for_up 12221"
-run_in_pane 2 "cargo run --bin=sled-agent-sim -- $(uuidgen) 127.0.0.1:12345 127.0.0.1:12221"
+run_in_pane 2 "cargo run --bin=sled-agent-sim -- $(uuidgen) [::1]:12345 127.0.0.1:12221"
 
 run_in_pane 3 "$UTILS"
 run_in_pane 3 "set_pane_title 'seed data'"
