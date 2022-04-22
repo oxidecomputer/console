6a283d040fbdf71493393b82d990ee39f7879239
Justin Bennett <git@just-be.dev>
2022-04-22 04:59:08 -0500

    Ensure boot disk is created

--- app/components/form/fields/DiskSizeField.tsx
+++ app/components/form/fields/DiskSizeField.tsx
@@ -5,17 +5,24 @@ import { useFormikContext } from 'formik'
 import { GiB } from '@oxide/util'
 import invariant from 'tiny-invariant'
 
-interface DiskSizeProps extends Omit<TextFieldProps, 'validate'> {}
+interface DiskSizeProps extends Omit<TextFieldProps, 'validate'> {
+  blockSizeField?: string
+}
 
-export function DiskSizeField({ required = true, name = 'size', ...props }: DiskSizeProps) {
-  const { values } = useFormikContext<{ blockSize: number }>()
-  const { blockSize } = values
-  invariant(blockSize, 'Expected form to have a field with the name `blockSize`')
+export function DiskSizeField({
+  required = true,
+  name = 'size',
+  blockSizeField = 'blockSize',
+  ...props
+}: DiskSizeProps) {
+  const { values } = useFormikContext<Record<string, number>>()
+  invariant(blockSizeField in values, `expected form values to contain ${blockSizeField}`)
+  const blockSize = values[blockSizeField]
   return (
     <TextField
+      units="GiB"
       validate={getDiskValidator(blockSize)}
       type="number"
-      min={1}
       required={required}
       name={name}
       {...props}
