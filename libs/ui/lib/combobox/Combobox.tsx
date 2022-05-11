f0beb77fdfee9688d2c8590325cc86408163712c
David Crespo <david.crespo@oxidecomputer.com>
2022-05-11 16:40:20 -0500

    fix the weird behavior (don't pass value), spruce up styling

--- libs/ui/lib/combobox/Combobox.tsx
+++ libs/ui/lib/combobox/Combobox.tsx
@@ -16,8 +16,6 @@ type ComboboxProps = {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-// TODO: styling on everything looks terrible
-
 /**
  * Reach Combobox with match filtering and sorting powered by match-sorter.
  */
@@ -25,7 +23,7 @@ export function Combobox({ items, onChange, onSelect, value }: ComboboxProps) {
   return (
     <RCombobox
       openOnFocus
-      className="rounded border bg-default border-default"
+      className="rounded border bg-default border-default focus-within:border-accent hover:focus-within:border-accent"
       onSelect={onSelect}
     >
       <ComboboxInput
@@ -33,12 +31,21 @@ export function Combobox({ items, onChange, onSelect, value }: ComboboxProps) {
         py-[0.5625rem] px-3
         text-sans-md text-default focus:outline-none`}
         onChange={onChange}
-        value={value}
+        // Note that unlike with a plain <input>, passing `onChange` does not
+        // also require passing in `value`, and in fact passing in `value`
+        // causes weird behavior. Our `onChange` is called in addition to the
+        // built-in one that allows it to manage its own state internally; our
+        // callback does not replace theirs. In short, do not pass `value` here.
       />
-      <ComboboxPopover>
-        <ComboboxList>
+      <ComboboxPopover className="mt-2 bg-default">
+        <ComboboxList persistSelection className="rounded border border-default">
           {matchSorter(items, value).map((item) => (
-            <ComboboxOption key={item} value={item} />
+            <ComboboxOption
+              // TODO: make border overlap container border like Justin's beautiful tables
+              className="cursor-pointer rounded border px-3 py-2 text-sans-sm border-default hover:bg-hover"
+              key={item}
+              value={item}
+            />
           ))}
         </ComboboxList>
       </ComboboxPopover>
