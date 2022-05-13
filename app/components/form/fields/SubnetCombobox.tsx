9d3075fc7c8f2ced05a00732903a1ba96e9e209b
David Crespo <david-crespo@users.noreply.github.com>
2022-05-12 19:10:59 -0500

    Combobox for disks and VPCs and subnets (#855)
    * very basic combobox and example usage attaching a disk
    
    * fix the weird behavior (don't pass value), spruce up styling
    
    * ugly no matches found message
    
    * fix ordering bug
    
    * fetch subnets list based on selected vpc
    
    * comments
    
    * remove unnecessary fallback

---
+++ app/components/form/fields/SubnetCombobox.tsx
@@ -0,0 +1,53 @@
+import type { Vpc } from '@oxide/api'
+import { useApiQuery } from '@oxide/api'
+import { useParams } from 'app/hooks'
+import { useField } from 'formik'
+import type { ComboboxFieldProps } from './ComboboxField'
+import { ComboboxField } from './ComboboxField'
+
+type SubnetComboboxProps = Omit<ComboboxFieldProps, 'items'> & {
+  /** `name` of the Formik field to read the `vpcName` from */
+  vpcNameField: string
+  /**
+   * We use this to check that the `vpcName` is real so we can avoid trying to
+   * fetch subnets for nonexistent VPCs. We could fetch the list in the
+   * component instead, but we'd have to make sure the params match exactly to
+   * ensure RQ dedupes the request
+   */
+  vpcs: Vpc[]
+}
+
+/**
+ * Read the selected VPC name from the Formik context and fetch the subnets for
+ * that VPC. Only fetch if the VPC name is that of an existing VPC.
+ *
+ * Needs to be its own component so it can go *inside* the `<Formik>` element in
+ * order to have access to the context.
+ */
+export function SubnetCombobox({ vpcNameField, vpcs, ...fieldProps }: SubnetComboboxProps) {
+  const pathParams = useParams('orgName', 'projectName')
+  const [, { value: vpcNameRaw }] = useField<string>({ name: vpcNameField })
+
+  const vpcName = vpcNameRaw.trim()
+
+  const vpcExists = vpcName.length > 0 && vpcs.map((x) => x.name).includes(vpcName)
+
+  // TODO: error handling other than fallback to empty list?
+  const subnets =
+    useApiQuery(
+      'vpcSubnetsGet',
+      { ...pathParams, vpcName, limit: 50 },
+      {
+        enabled: vpcExists,
+        useErrorBoundary: false,
+      }
+    ).data?.items || []
+
+  return (
+    <ComboboxField
+      {...fieldProps}
+      items={subnets.map((x) => x.name)}
+      disabled={!vpcExists}
+    />
+  )
+}
