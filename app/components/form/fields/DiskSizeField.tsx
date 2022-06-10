08dbcf8c91c1cccb4473b820ddd5e37aa23322e9
Justin Bennett <oxide@just-be.dev>
2022-06-10 17:04:10 -0400

    Unbreak the e2e tests (#931)

--- app/components/form/fields/DiskSizeField.tsx
+++ app/components/form/fields/DiskSizeField.tsx
@@ -1,6 +1,4 @@
 import { useFormikContext } from 'formik'
-import React from 'react'
-import invariant from 'tiny-invariant'
 
 import { GiB } from '@oxide/util'
 
@@ -21,7 +19,6 @@ export function DiskSizeField({
   ...props
 }: DiskSizeProps) {
   const { values } = useFormikContext<Record<string, number>>()
-  invariant(blockSizeField in values, `expected form values to contain ${blockSizeField}`)
   const blockSize = values[blockSizeField]
   return (
     <TextField
