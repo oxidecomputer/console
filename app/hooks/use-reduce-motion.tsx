c8f83390eb6997a05214ffa71b8329572306ca1d
Benjamin Leonard <hello@benleonard.co.uk>
2022-06-29 21:03:22 +0100

    Animated dialog test (#975)
    * Animated dialog test
    
    * Use base scrim rgb variable
    
    * Update SideModal.tsx
    
    * Switch `react-spring` to `@react-spring/web`
    
    * Tweak distance and speed
    
    * Add reduce motion for `react-spring`
    
    * Remove overlay animation to avoid struggle with rgba `react-spring` bug
    
    * tiny bit of cleanup
    
    Co-authored-by: Justin Bennett <oss@just-be.dev>
    Co-authored-by: David Crespo <david.crespo@oxidecomputer.com>

---
+++ app/hooks/use-reduce-motion.tsx
@@ -0,0 +1,31 @@
+import { Globals } from '@react-spring/web'
+import { useEffect, useState } from 'react'
+
+Globals.assign({ skipAnimation: true })
+
+const motionQuery = () => window.matchMedia('(prefers-reduced-motion: reduce)')
+
+/**
+ * Pulled from [react-reduce-motion](https://github.com/infiniteluke/react-reduce-motion).
+ */
+function useReducedMotion() {
+  const [reducedMotion, setReducedMotion] = useState(motionQuery().matches)
+  useEffect(() => {
+    const mq = motionQuery()
+    const handleChange = () => setReducedMotion(mq.matches)
+    handleChange()
+    mq.addEventListener('change', handleChange)
+    return () => mq.removeEventListener('change', handleChange)
+  }, [])
+  return reducedMotion
+}
+
+export function ReduceMotion() {
+  const prefersReducedMotion = useReducedMotion()
+
+  useEffect(() => {
+    Globals.assign({ skipAnimation: prefersReducedMotion })
+  }, [prefersReducedMotion])
+
+  return null
+}
