4f9118d0b5bf77c6ead72758891e94ed54b02a10
Justin Bennett <oxide@just-be.dev>
2022-06-30 01:41:20 -0400

    Fix distro selection (#1013)

--- app/components/form/fields/ImageSelectField.tsx
+++ app/components/form/fields/ImageSelectField.tsx
@@ -1,18 +1,23 @@
+import cn from 'classnames'
+import { useSelect } from 'downshift'
+import { useField } from 'formik'
 import type { ComponentType } from 'react'
 import { useState } from 'react'
+import { useCallback } from 'react'
+import { useEffect } from 'react'
 
 import type { GlobalImage } from '@oxide/api'
 import {
   CentosDistroIcon,
   DebianDistroIcon,
+  DirectionDownIcon,
   FedoraDistroIcon,
   Images24Icon,
-  Listbox,
   RadioCard,
   UbuntuDistroIcon,
   WindowsDistroIcon,
 } from '@oxide/ui'
-import { groupBy } from '@oxide/util'
+import { classed, groupBy } from '@oxide/util'
 
 import type { RadioFieldProps } from './RadioField'
 import { RadioField } from './RadioField'
@@ -24,7 +29,7 @@ const ArchDistroIcon = (props: { className?: string }) => {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M7.9985 0.996994C7.28621 2.74332 6.8566 3.88562 6.06357 5.58003C6.5498 6.09543 7.14662 6.69564 8.11585 7.37352C7.07383 6.94474 6.36304 6.51424 5.83186 6.06752C4.81693 8.18533 3.22683 11.202 0 17C2.53618 15.5358 4.50218 14.6331 6.3344 14.2887C6.25572 13.9503 6.21099 13.5843 6.21403 13.2024L6.21704 13.1211C6.25728 11.4963 7.10254 10.2467 8.10382 10.3316C9.1051 10.4164 9.88339 11.8032 9.84314 13.4281C9.83558 13.7338 9.80109 14.0279 9.74083 14.3007C11.5532 14.6553 13.4981 15.5556 16 17C15.5067 16.0918 15.0664 15.2731 14.6459 14.4933C13.9835 13.98 13.2926 13.3118 11.8834 12.5885C12.852 12.8402 13.5455 13.1306 14.0861 13.4551C9.81081 5.49523 9.4646 4.43753 7.9985 0.996994Z"
-        fill="#989A9B"
+        fill="currentColor"
       />
     </svg>
   )
@@ -85,42 +90,110 @@ function distroDisplay(image: GlobalImage): GlobalImage & {
 }
 
 interface ImageSelectFieldProps extends Omit<RadioFieldProps, 'children'> {
+  name: string
   images: GlobalImage[]
 }
 
-export function ImageSelectField({ images, ...props }: ImageSelectFieldProps) {
+export function ImageSelectField({ images, name, ...props }: ImageSelectFieldProps) {
   return (
-    <RadioField {...props}>
+    <RadioField name={name} {...props}>
       {Object.entries(groupBy(images, (i) => i.distribution)).map(
         ([distroName, distroValues]) => (
-          <ImageSelect key={distroName} images={distroValues} />
+          <ImageSelect key={distroName} images={distroValues} fieldName={name} />
         )
       )}
     </RadioField>
   )
 }
 
+const Outline = classed.div`absolute z-10 h-full w-full rounded border border-accent pointer-events-none`
+
 interface ImageSelectProps {
   images: GlobalImage[]
+  fieldName: string
 }
-function ImageSelect({ images, ...props }: ImageSelectProps) {
+function ImageSelect({ images, fieldName }: ImageSelectProps) {
   const distros = images.map(distroDisplay)
-  const { label, id, Icon } = distros[0]
-  const [value, setValue] = useState(id)
+  const { label, Icon, id } = distros[0]
+  const [, { value }, { setValue }] = useField<string>(fieldName)
+  const [currentDistro, setCurrentDistro] = useState<string | undefined>(id)
+
+  useEffect(() => {
+    if (distros.some((d) => d.id === value)) {
+      setCurrentDistro(value)
+    }
+  }, [distros, value])
+
+  const select = useSelect({
+    initialSelectedItem: distros[0],
+    items: distros,
+    itemToString: (distro) => distro?.version || '',
+    onSelectedItemChange(changes) {
+      if (changes.selectedItem) {
+        setValue(changes.selectedItem.id)
+      }
+    },
+  })
+  const onClick = useCallback(() => {
+    if (select.selectedItem) {
+      setValue(select.selectedItem.id)
+    }
+  }, [select.selectedItem, setValue])
+
+  const selected = currentDistro === value
   return (
-    <>
-      <RadioCard key={label} value={value} className="h-44 w-44 pb-2" {...props}>
-        <div className="relative flex h-full flex-col items-center justify-end space-y-4 children:border-secondary first:children:border-b peer-checked:children:border-accent-secondary">
-          <Listbox
-            items={distros.map((d) => ({ label: d.version, value: d.id }))}
-            onChange={(item) => item && setValue(item.value)}
-            defaultValue={id}
-            className="!absolute top-0 -mt-1 w-full text-mono-xs children:rounded-none children:border-0"
-          ></Listbox>
+    <div className="relative">
+      <RadioCard
+        name={fieldName}
+        value={currentDistro}
+        className="h-44 w-44 pb-2 relative"
+        onClick={onClick}
+      >
+        <div className="relative flex h-full flex-col items-center justify-end space-y-4 peer-checked:children:border-accent-secondary">
+          <button
+            type="button"
+            {...select.getToggleButtonProps()}
+            className={cn(
+              'absolute top-0 w-full flex items-center border-b border-secondary h-8 justify-between px-3 text-mono-md text-secondary',
+              selected ? 'text-accent' : 'text-secondary'
+            )}
+          >
+            <span>{select.selectedItem?.version}</span>
+            <DirectionDownIcon />
+          </button>
           <Icon className="h-12 w-12 text-tertiary" />
-          <span className="text-sans-xl text-secondary">{label}</span>
+          <span className={cn('text-sans-xl', selected ? 'text-accent' : 'text-secondary')}>
+            {label}
+          </span>
         </div>
       </RadioCard>
-    </>
+      <ul
+        className={cn(
+          '!children:border-b-secondary absolute top-8 left-0.5 right-0.5 overflow-y-auto rounded border-secondary bg-raise children:border-b children:border-secondary last:children:border-b-0 focus:outline-none max-h-[17.5rem] shadow-2xl',
+          select.isOpen && 'border'
+        )}
+        {...select.getMenuProps()}
+      >
+        {select.isOpen &&
+          distros.map((distro, index) => (
+            <li key={index} className="relative">
+              {selected && select.selectedItem?.id === distro.id && <Outline />}
+              <div
+                key={index}
+                className={cn(
+                  'p-3 text-sans-sm text-default hover:bg-raise-hover cursor-pointer',
+                  selected &&
+                    select.selectedItem?.id === distro.id &&
+                    'text-accent bg-accent-secondary hover:bg-accent-secondary-hover',
+                  select.highlightedIndex === index && 'bg-raise-hover'
+                )}
+                {...select.getItemProps({ item: distro, index })}
+              >
+                {distro.version}
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
   )
 }
