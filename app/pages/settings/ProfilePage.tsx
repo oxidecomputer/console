a4b7e4bcc88a9f131c0dd486add7ffd3f838bfd9
Justin Bennett <oxide@just-be.dev>
2022-06-30 02:27:08 -0400

    Show user details (#1014)
    * Show UUID in profile, bump types
    
    * Tweak sidebar styles for settings page

--- app/pages/settings/ProfilePage.tsx
+++ app/pages/settings/ProfilePage.tsx
@@ -1,20 +1,29 @@
+import { useApiQuery } from '@oxide/api'
 import { Settings24Icon } from '@oxide/ui'
 
 import { FullPageForm, TextField } from 'app/components/form'
 
 export function ProfilePage() {
+  const { data: user } = useApiQuery('sessionMe', {})
   return (
     <FullPageForm
       id="profile-form"
       title="Profile"
-      initialValues={{}}
+      initialValues={{
+        id: user?.id,
+      }}
       onSubmit={() => {}}
       error={new Error('This form is not yet implemented')}
       icon={<Settings24Icon />}
     >
-      <TextField id="profile-name" name="name" required disabled />
-      <TextField id="profile-username" name="username" required disabled />
-      <TextField id="profile-email" name="email" required disabled />
+      <TextField
+        id="profile-id"
+        name="id"
+        label="User ID"
+        required
+        disabled
+        fieldClassName="!cursor-default"
+      />
       <span className="inline-block text-secondary">
         <span>Your user information is managed by your organization. </span>
         <span className="md+:block">
