9a8f07009ff0c14e9119359dea2851abf8220ae6
Justin Bennett <oss@just-be.dev>
2022-06-29 21:52:13 -0400

    This should be a lint rule

--- app/forms/network-interface-create.tsx
+++ app/forms/network-interface-create.tsx
@@ -88,7 +88,7 @@ export default function CreateNetworkInterfaceSideModalForm({
         name="vpcName"
         label="VPC"
         items={vpcs.map(({ name }) => ({ label: name, value: name }))}
-        required={true}
+        required
       />
       <SubnetListbox
         id="nic-subnet"
@@ -96,7 +96,7 @@ export default function CreateNetworkInterfaceSideModalForm({
         label="Subnet"
         vpcNameField="vpcName"
         vpcs={vpcs}
-        required={true}
+        required
       />
       <TextField id="nic-ip" name="ip" label="IP Address" />
     </SideModalForm>
