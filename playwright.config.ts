72f5d22f3319bbc3d08e373da457530d41b58b69
David Crespo <david.crespo@oxidecomputer.com>
2022-07-26 14:35:09 -0500

    try 3 workers and put the timeout back to 10s

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 20000,
+    actionTimeout: 10000,
     baseURL: 'http://localhost:4009',
     trace: 'on-first-retry',
   },
