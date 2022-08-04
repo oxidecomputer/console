07d96be86696f8abcfac8a1f800ded6486f2a38f
David Crespo <david-crespo@users.noreply.github.com>
2022-08-04 11:53:15 -0500

    give all instances an ephemeral IP at create time (#1105)

--- app/forms/instance-create.tsx
+++ app/forms/instance-create.tsx
@@ -165,6 +165,7 @@ export default function CreateInstanceForm({
                 },
                 ...values.disks,
               ],
+              externalIps: [{ type: 'ephemeral' }],
             },
           })
         })
