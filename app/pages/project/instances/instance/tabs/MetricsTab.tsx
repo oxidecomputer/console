c810af883692ab5256bb5f8de8076e2cedaddf20
David Crespo <david.crespo@oxidecomputer.com>
2022-08-26 18:35:40 -0500

    bunch of todo comments

--- app/pages/project/instances/instance/tabs/MetricsTab.tsx
+++ app/pages/project/instances/instance/tabs/MetricsTab.tsx
@@ -17,10 +17,16 @@ type DiskMetricParams = {
   // TODO: specify bytes or count
 }
 
+// Recharts's built-in ticks behavior is useless and probably broken
+/**
+ * Split the data into n evenly spaced ticks, with one at the left end and one a
+ * little bit in from the right end, and the rest evenly spaced in between.
+ */
 function getTicks(data: { timestamp: number }[], n: number): number[] {
   if (data.length === 0) return []
   if (n < 2) throw Error('n must be at least 2 because of the start and end ticks')
-  const maxIdx = data.length - 1
+  // bring the last tick in a bit from the end
+  const maxIdx = data.length > 10 ? Math.floor((data.length - 1) * 0.9) : data.length - 1
   // if there are 4 ticks, their positions are 0/3, 1/3, 2/3, 3/3 (as fractions of maxIdx)
   const idxs = new Array(n).fill(0).map((_, i) => Math.floor((maxIdx * i) / (n - 1)))
   return idxs.map((i) => data[i].timestamp)
@@ -37,7 +43,6 @@ const DARK_GREEN = '#112725'
 function renderTooltip(props: TooltipProps<number, string>) {
   const { payload } = props
   if (!payload || payload.length < 1) return null
-  console.log(props)
   // TODO: there has to be a better way to get these values
   const {
     name,
@@ -47,10 +52,10 @@ function renderTooltip(props: TooltipProps<number, string>) {
   return (
     <div className="bg-raise text-secondary p-2 text-sans-md">
       <div>{longDateTime(timestamp)}</div>
-      {/* TODO: value needs a label (should be easy) */}
       <div>
         <span>{name}: </span>
         <span className="text-accent">{value}</span>
+        {/* TODO: unit on value if relevant */}
       </div>
     </div>
   )
@@ -112,6 +117,8 @@ function DiskMetric({
           dataKey="timestamp"
           interval="preserveStart"
           scale="time"
+          // we're doing the x axis as timestamp ms instead of Date primarily to make type=number work
+          // TODO: use Date directly as x-axis values
           type="number"
           name="Time"
           ticks={getTicks(data, 3)}
@@ -119,6 +126,7 @@ function DiskMetric({
           tickFormatter={shortDateTime}
         />
         <YAxis orientation="right" />
+        {/* TODO: stop tooltip being focused by default on pageload if nothing else has been clicked */}
         <Tooltip isAnimationActive={false} content={renderTooltip} />
       </ComposedChart>
     </div>
@@ -153,6 +161,10 @@ export function MetricsTab() {
         {/* TODO: need a nicer way of saying what the boot disk is */}
         Boot disk ( <code>{diskName}</code> )
       </h2>
+      {/* TODO: separate "Activations" from "(count)" so we can
+                a) style them differently in the title, and
+                b) show "Activations" but not "(count)" in the Tooltip?
+        */}
       <div className="flex flex-wrap gap-8 mt-8">
         <DiskMetric {...commonProps} title="Activations (count)" metricName="activated" />
         <DiskMetric {...commonProps} title="Reads (count)" metricName="read" />
