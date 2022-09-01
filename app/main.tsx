eefc736b82b72f63f951aab40a93e80bb4aec579
Justin Bennett <git@just-be.dev>
2022-09-01 12:43:56 -0400

    Be a little more specific around user-agent matching

--- app/main.tsx
+++ app/main.tsx
@@ -38,7 +38,7 @@ function render() {
 
 if (
   process.env.NODE_ENV !== 'production' &&
-  (process.env.MSW || window.navigator.userAgent.includes('MSW'))
+  (process.env.MSW || window.navigator.userAgent.endsWith('MSW'))
 ) {
   // MSW has NODE_ENV !== prod built into it, but let's be extra safe
   // need to defer requests until after the mock server starts up
