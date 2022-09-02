e4fddfebfed46806a5f5af9e6a7bf2bdf2a048f3
Justin Bennett <git@just-be.dev>
2022-09-01 23:01:06 -0400

    Back to 2 retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   globalSetup: 'app/test/e2e/global-setup.ts',
