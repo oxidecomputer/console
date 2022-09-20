466f777116d1f651f26bdb5dfa24e0743c297cee
Justin Bennett <oxide@just-be.dev>
2022-09-19 21:24:41 -0400

    Update instance test to use new names

--- app/test/instance-create.e2e.ts
+++ app/test/instance-create.e2e.ts
@@ -1,51 +1,43 @@
-import { expectVisible, genName, test } from 'app/test/e2e'
+import { expectVisible, test } from 'app/test/e2e'
 
-test.describe('Instance Create Form', () => {
-  const orgName = genName('instance-create-org')
-  const projectName = genName('instance-create-proj')
+test.beforeEach(async ({ createProject, orgName, projectName }) => {
+  await createProject(orgName, projectName)
+})
 
-  test.beforeEach(async ({ createOrg, createProject }) => {
-    await createOrg(orgName)
-    await createProject(orgName, projectName)
-  })
+test('can invoke instance create form from instances page', async ({
+  page,
+  orgName,
+  projectName,
+}) => {
+  await page.goto(`/orgs/${orgName}/projects/${projectName}/instances`)
+  await page.locator('text="New Instance"').click()
 
-  test('can invoke instance create form from instances page', async ({ page }) => {
-    await page.goto(`/orgs/${orgName}/projects/${projectName}/instances`)
-    await page.locator('text="New Instance"').click()
+  await expectVisible(page, [
+    'role=heading[name*="Create instance"]',
+    'role=heading[name="Hardware"]',
+    'role=heading[name="Boot disk"]',
+    'role=heading[name="Additional disks"]',
+    'role=heading[name="Networking"]',
+    'role=textbox[name="Name"]',
+    'role=textbox[name="Description"]',
+    'role=textbox[name="Disk name"]',
+    'role=spinbutton[name="Disk size (GiB)"]',
+    'role=radiogroup[name="Network interface"]',
+    'role=textbox[name="Hostname"]',
+    'role=button[name="Create instance"][disabled]',
+  ])
 
-    await expectVisible(page, [
-      'role=heading[name*="Create instance"]',
-      'role=heading[name="Hardware"]',
-      'role=heading[name="Boot disk"]',
-      'role=heading[name="Additional disks"]',
-      'role=heading[name="Networking"]',
-      'role=textbox[name="Name"]',
-      'role=textbox[name="Description"]',
-      'role=textbox[name="Disk name"]',
-      'role=spinbutton[name="Disk size (GiB)"]',
-      'role=radiogroup[name="Network interface"]',
-      'role=textbox[name="Hostname"]',
-      'role=button[name="Create instance"][disabled]',
-    ])
+  await page.fill('input[name=name]', 'mock-instance')
+  await page.locator('.ox-radio-card').nth(3).click()
 
-    await page.fill('input[name=name]', 'mock-instance')
-    await page.locator('.ox-radio-card').nth(3).click()
+  await page.fill('input[name=bootDiskName]', 'my-boot-disk')
+  await page.fill('input[name=bootDiskSize]', '20')
 
-    await page.fill('input[name=bootDiskName]', 'my-boot-disk')
-    await page.fill('input[name=bootDiskSize]', '20')
+  await page.locator('input[value=ubuntu-1] ~ .ox-radio-card').click()
 
-    await page.locator('input[value=ubuntu-1] ~ .ox-radio-card').click()
+  await page.locator('button:has-text("Create instance")').click()
 
-    await page.locator('button:has-text("Create instance")').click()
+  await page.waitForURL(`/orgs/${orgName}/projects/${projectName}/instances/mock-instance`)
 
-    await page.waitForURL(
-      `/orgs/${orgName}/projects/${projectName}/instances/mock-instance`
-    )
-
-    await expectVisible(page, [
-      'h1:has-text("mock-instance")',
-      'text=6 vCPUs',
-      'text=24 GiB',
-    ])
-  })
+  await expectVisible(page, ['h1:has-text("mock-instance")', 'text=6 vCPUs', 'text=24 GiB'])
 })
