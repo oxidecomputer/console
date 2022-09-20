87b10e80178ba9b4f6aa6e52ef0e70fc4a2b8f5e
David Crespo <david.crespo@oxidecomputer.com>
2022-09-20 16:56:10 -0500

    pull ContentPane back out for cuter <Layout>

--- app/layouts/helpers.tsx
+++ app/layouts/helpers.tsx
@@ -8,23 +8,27 @@ import { PageActionsTarget } from 'app/components/PageActions'
 import { Sidebar } from 'app/components/Sidebar'
 import { TopBar } from 'app/components/TopBar'
 
+const ContentPane = () => (
+  <div className="flex flex-col overflow-auto">
+    <div className="flex flex-grow flex-col pb-8">
+      <SkipLinkTarget />
+      <main className="[&>*]:gutter">
+        <Outlet />
+      </main>
+    </div>
+    <div className="sticky bottom-0 flex-shrink-0 justify-between overflow-hidden border-t bg-default border-secondary empty:border-t-0">
+      <Pagination.Target />
+      <PageActionsTarget />
+    </div>
+  </div>
+)
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid h-screen grid-cols-[13.75rem,1fr] grid-rows-[60px,1fr]">
       <TopBar />
       <Sidebar>{children}</Sidebar>
-      <div className="flex flex-col overflow-auto">
-        <div className="flex flex-grow flex-col pb-8">
-          <SkipLinkTarget />
-          <main className="[&>*]:gutter">
-            <Outlet />
-          </main>
-        </div>
-        <div className="sticky bottom-0 flex-shrink-0 justify-between overflow-hidden border-t bg-default border-secondary empty:border-t-0">
-          <Pagination.Target />
-          <PageActionsTarget />
-        </div>
-      </div>
+      <ContentPane />
     </div>
   )
 }
