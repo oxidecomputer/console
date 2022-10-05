51667fdfaa7ca7f8a3f7b4bb02e84cd7388f2130
David Crespo <david.crespo@oxidecomputer.com>
2022-10-04 21:26:54 -0500

    whoops, it's supposed to stay /spoof_login

--- app/routes.tsx
+++ app/routes.tsx
@@ -45,7 +45,7 @@ const vpcCrumb: CrumbFunc = (m) => m.params.vpcName!
 export const routes = createRoutesFromElements(
   <Route element={<RootLayout />}>
     <Route path="*" element={<NotFound />} />
-    <Route path="login_spoof_begin" element={<AuthLayout />}>
+    <Route path="spoof_login" element={<AuthLayout />}>
       <Route index element={<LoginPage />} />
     </Route>
 
