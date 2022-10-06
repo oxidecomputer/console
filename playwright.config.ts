073a303377a3f8184edb1e48ff553e099747ed0f
Justin Bennett <git@just-be.dev>
2022-10-06 00:43:06 -0400

    Centralize build step

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,7 +49,7 @@ const config: PlaywrightTestConfig = {
 
   // use different port so it doesn't conflict with local dev server
   webServer: {
-    command: `yarn start preview --port 4009`,
+    command: `yarn build --mode development && yarn start preview --port 4009`,
     port: 4009,
   },
 }
