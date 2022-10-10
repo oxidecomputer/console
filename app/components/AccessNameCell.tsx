043965058a007ce1f05cd6e462c9aff4a87cbc73
David Crespo <david.crespo@oxidecomputer.com>
2022-10-10 13:18:22 -0500

    rename TData -> RowData

--- app/components/AccessNameCell.tsx
+++ app/components/AccessNameCell.tsx
@@ -5,8 +5,8 @@ import { Badge } from '@oxide/ui'
 /**
  * Display the user or group name. If the row is for a group, add a GROUP badge.
  */
-export const AccessNameCell = <TData extends { name: string; type: 'user' | 'group' }>(
-  info: CellContext<TData, string>
+export const AccessNameCell = <RowData extends { name: string; type: 'user' | 'group' }>(
+  info: CellContext<RowData, string>
 ) => {
   const name = info.getValue()
   const identityType = info.row.original.type
