acf7c7d51999e0de05567f34d2a4f854229ff5a0
David Crespo <david-crespo@users.noreply.github.com>
2022-10-11 16:47:08 -0500

    Rewrite `bump-omicron.sh` in Python and Deno (#1223)
    * rewrite bump-omicron.sh in python
    
    * bump omicron script in deno
    
    * cut 11 lines from deno script, shut tsc up
    
    * cut 2 more lines. what am I doing with my life
    
    * print error output when command not found
    
    * inline checkCmdExists by using run instead
    
    * whoops
    
    * inline parsing old version file
    
    * save 2 lines in python
    
    * delete python and bash
    
    * need to push the branch before creating the PR
    
    * be more cute with the dry run flag, delete local branch after PR
    
    * we are always going to choose to save 3 lines

---
+++ tools/bump-omicron.ts
@@ -0,0 +1,77 @@
+#! /usr/bin/env -S deno run --allow-run --allow-net --allow-read --allow-write
+import * as flags from 'https://deno.land/std@0.159.0/flags/mod.ts'
+import * as path from 'https://deno.land/std@0.159.0/path/mod.ts'
+
+// Automatically update tools/console_version in ../omicron with current console
+// commit hash and tarball hash. Then create PR in Omicron with that change.
+//
+// Requirements/assumptions:
+//
+// - GitHub CLI
+// - Omicron is a sibling dir to console
+
+const OMICRON_DIR = '../omicron'
+const VERSION_FILE = path.join(OMICRON_DIR, 'tools/console_version')
+
+const GH_MISSING = 'GitHub CLI not found. Please install it and try again.'
+const VERSION_FILE_MISSING = `Omicron console version file at '${VERSION_FILE}' not found. This script assumes Omicron is cloned in a sibling directory next to Console.`
+
+/** Wrap Deno.run, get output as string */
+async function run(cmd: string[]): Promise<string> {
+  const proc = Deno.run({ cmd, stdout: 'piped' })
+  const { code } = await proc.status()
+
+  // stderr is not piped so it'll show up automatically
+  if (code !== 0) throw Error(`Shell command '${cmd.join(' ')}' failed`)
+
+  const output = await proc.output()
+  return new TextDecoder().decode(output).trim()
+}
+
+// script starts here
+
+const { dryRun } = flags.parse(Deno.args, {
+  alias: { dryRun: ['d', 'dry-run'] },
+  boolean: ['dryRun'],
+})
+
+const newCommit = await run(['git', 'rev-parse', 'HEAD'])
+const shaUrl = `https://dl.oxide.computer/releases/console/${newCommit}.sha256.txt`
+const newSha2 = (await fetch(shaUrl).then((resp) => resp.text())).trim()
+
+const newVersionFile = `COMMIT="${newCommit}"\nSHA2="${newSha2}"\n`
+
+if (dryRun) {
+  console.log(newVersionFile)
+  Deno.exit()
+}
+
+await run(['which', 'gh']).catch(() => {
+  throw Error(GH_MISSING)
+})
+
+const oldVersionFile = await Deno.readTextFile(VERSION_FILE).catch(() => {
+  throw Error(VERSION_FILE_MISSING)
+})
+
+const oldCommit = /COMMIT="?([a-f0-9]+)"?/.exec(oldVersionFile)?.[1]
+if (!oldCommit) throw Error('Could not parse existing version file')
+
+await Deno.writeTextFile(VERSION_FILE, newVersionFile)
+
+const branchName = `bump-console-${newCommit.slice(0, 8)}`
+const prTitle = 'Bump console to latest main'
+const prBody = `Changes: https://github.com/oxidecomputer/console/compare/${oldCommit}...${newCommit}`
+
+// cd to omicron, pull main, create new branch, commit changes, push, PR it, go back to
+// main, delete branch
+Deno.chdir(OMICRON_DIR)
+await run(['git', 'checkout', 'main'])
+await run(['git', 'pull'])
+await run(['git', 'checkout', '-b', branchName])
+await run(['git', 'add', '--all'])
+await run(['git', 'commit', '-m', prTitle, '-m', prBody])
+await run(['git', 'push'])
+await run(['gh', 'pr', 'create', '--title', prTitle, '--body', prBody])
+await run(['git', 'checkout', 'main'])
+await run(['git', 'branch', '-D', branchName])
