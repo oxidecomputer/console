a7088be9285983f817cde6ce4ea01d943b2823e6
Justin Bennett <git@just-be.dev>
2022-10-26 15:09:05 -0500

    Fix export icons script

--- tools/export-icons.sh
+++ tools/export-icons.sh
@@ -13,14 +13,14 @@ export $(egrep -v '^#' .env | xargs)
 rm -rf $ICONS_DIR
 
 # This command requires a FIGMA_TOKEN env var with read access to Oxide's DS to be set
-npm run figma-export use-config
+npx figma-export use-config
 
 for file in $ICONS_DIR/*.js $ICONS_DIR/**/*.js; do
   mv -- "$file" "${file%.js}.ts"
 done
 
 for file in $CODEMOD_DIR/*.icons.js; do
-    npx jscodeshift -- -t $file --extensions=ts,tsx --parser=tsx $ICONS_DIR
+    npx jscodeshift -t $file --extensions=ts,tsx --parser=tsx $ICONS_DIR
 done
 
 npm run fmt .
