a72e733c0efc4094a6e2022a7ff9a0bf28307504
David Crespo <david.crespo@oxidecomputer.com>
2022-10-26 13:33:55 -0500

    move useForm call inside SideModalForm, use render prop to pass control

--- app/forms/org-create.tsx
+++ app/forms/org-create.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
 import type { Organization, OrganizationCreate } from '@oxide/api'
@@ -45,38 +43,23 @@ export function CreateOrgSideModalForm({
     onError,
   })
 
-  // TODO: we don't want to have manually specify this every time we make a
-  // form. might have to make a wrapper hook with different defaults
-  //
-  // TODO: the docs warn against the performance implications of validating on
-  // every change
-  const form = useForm({ defaultValues, mode: 'all' })
-  const canSubmit = form.formState.isDirty && form.formState.isValid
-
-  // TODO: calling useForm all the way up here means it's always mounted whether
-  // the side modal is open or not, which means form state hangs around even
-  // when the modal is closed. Using useEffect like this is a code smell, (and
-  // error-prone, and would have to be wrapped up in a custom useForm in order
-  // to get consistent correct behavior everywhere) so I would like this to work
-  // differently. Ideally useForm would be called one level lower, e.g., in a
-  // component that wraps name and description in the children of SideModalForm
-  // so it only gets rendered when the form is open.
-  useEffect(() => {
-    if (!isOpen) form.reset()
-  }, [form, isOpen])
-
   return (
     <SideModalForm
       id="create-org-form"
+      defaultValues={defaultValues}
       title={title}
       onDismiss={onDismiss}
-      onSubmit={form.handleSubmit((values) => createOrg.mutate({ body: values }))}
-      submitDisabled={createOrg.isLoading || !canSubmit}
+      onSubmit={(values) => createOrg.mutate({ body: values })}
+      submitDisabled={createOrg.isLoading}
       error={createOrg.error?.error as Error | undefined}
       isOpen={isOpen}
     >
-      <NameField name="name" control={form.control} />
-      <DescriptionField name="description" control={form.control} />
+      {(control) => (
+        <>
+          <NameField name="name" control={control} />
+          <DescriptionField name="description" control={control} />
+        </>
+      )}
     </SideModalForm>
   )
 }
