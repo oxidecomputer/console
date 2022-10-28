aace8cd7af44d16e9b7bf1e4ca07a413aa49148b
David Crespo <david-crespo@users.noreply.github.com>
2022-10-28 00:26:41 -0500

    RHF IAM forms (#1266)
    * convert silo access page
    
    * extract common code and convert org access forms
    
    * convert project access
    
    * whoops

--- app/forms/silo-access.tsx
+++ app/forms/silo-access.tsx
@@ -1,47 +1,23 @@
-import * as Yup from 'yup'
+import {
+  setUserRole,
+  useApiMutation,
+  useApiQueryClient,
+  useUsersNotInPolicy,
+} from '@oxide/api'
 
-import type { Policy, RoleKey } from '@oxide/api'
-import { allRoles, useUsersNotInPolicy } from '@oxide/api'
-import { setUserRole } from '@oxide/api'
-import { useApiQueryClient } from '@oxide/api'
-import { useApiMutation } from '@oxide/api'
-import { capitalize } from '@oxide/util'
+import { ListboxField, SideModalForm } from 'app/components/hook-form'
 
-import { Form, ListboxField, SideModalForm } from 'app/components/form'
+import { defaultValues, roleItems } from './access-util'
+import type { AddRoleModalProps, EditRoleModalProps } from './access-util'
 
-import type { CreateSideModalFormProps, EditSideModalFormProps } from '.'
-
-type AddUserValues = {
-  userId: string
-  roleName: RoleKey | ''
-}
-
-const initialValues: AddUserValues = {
-  userId: '',
-  roleName: '',
-}
-
-const roleItems = allRoles.map((role) => ({ value: role, label: capitalize(role) }))
-
-type AddRoleModalProps = CreateSideModalFormProps<AddUserValues, Policy> & {
-  policy: Policy
-}
-
-export function SiloAccessAddUserSideModal({
-  onSubmit,
-  onSuccess,
-  onDismiss,
-  policy,
-  ...props
-}: AddRoleModalProps) {
+export function SiloAccessAddUserSideModal({ onDismiss, policy }: AddRoleModalProps) {
   const users = useUsersNotInPolicy(policy)
   const userItems = users.map((u) => ({ value: u.id, label: u.displayName }))
 
   const queryClient = useApiQueryClient()
   const updatePolicy = useApiMutation('policyUpdate', {
-    onSuccess: (data) => {
+    onSuccess: () => {
       queryClient.invalidateQueries('policyView', {})
-      onSuccess?.(data)
       onDismiss()
     },
   })
@@ -51,80 +27,81 @@ export function SiloAccessAddUserSideModal({
       onDismiss={onDismiss}
       title="Add user or group"
       id="silo-access-add-user"
-      initialValues={initialValues}
-      onSubmit={
-        onSubmit ||
-        (({ userId, roleName }) => {
-          // can't happen because roleName is validated not to be '', but TS
-          // wants to be sure
-          if (roleName === '') return
+      formOptions={{ defaultValues }}
+      onSubmit={({ userId, roleName }) => {
+        // can't happen because roleName is validated not to be '', but TS
+        // wants to be sure
+        if (roleName === '') return
 
-          updatePolicy.mutate({
-            body: setUserRole(userId, roleName, policy),
-          })
+        updatePolicy.mutate({
+          body: setUserRole(userId, roleName, policy),
         })
-      }
-      validationSchema={Yup.object({
-        userId: Yup.string().required(),
-        roleName: Yup.string().required(),
-      })}
+      }}
       submitDisabled={updatePolicy.isLoading}
-      error={updatePolicy.error?.error as Error | undefined}
-      {...props}
+      submitError={updatePolicy.error}
+      submitLabel="Add user"
     >
-      <ListboxField id="userId" name="userId" items={userItems} label="User" required />
-      <ListboxField id="roleName" name="roleName" label="Role" items={roleItems} required />
-      <Form.Submit>Add user</Form.Submit>
+      {(control) => (
+        <>
+          <ListboxField
+            name="userId"
+            items={userItems}
+            label="User"
+            required
+            control={control}
+          />
+          <ListboxField
+            name="roleName"
+            label="Role"
+            items={roleItems}
+            required
+            control={control}
+          />
+        </>
+      )}
     </SideModalForm>
   )
 }
 
-type EditUserValues = {
-  roleName: RoleKey
-}
-
-type EditRoleModalProps = EditSideModalFormProps<EditUserValues, Policy> & {
-  userId: string
-  policy: Policy
-}
-
 export function SiloAccessEditUserSideModal({
-  onSubmit,
-  onSuccess,
   onDismiss,
   userId,
   policy,
-  ...props
+  defaultValues,
 }: EditRoleModalProps) {
   const queryClient = useApiQueryClient()
   const updatePolicy = useApiMutation('policyUpdate', {
-    onSuccess: (data) => {
+    onSuccess: () => {
       queryClient.invalidateQueries('policyView', {})
-      onSuccess?.(data)
       onDismiss()
     },
   })
 
   return (
     <SideModalForm
-      onDismiss={onDismiss}
       // TODO: show user name in header or SOMEWHERE
       title="Change user role"
       id="silo-access-edit-user"
-      onSubmit={
-        onSubmit ||
-        (({ roleName }) => {
-          updatePolicy.mutate({
-            body: setUserRole(userId, roleName, policy),
-          })
+      formOptions={{ defaultValues }}
+      onSubmit={({ roleName }) => {
+        updatePolicy.mutate({
+          body: setUserRole(userId, roleName, policy),
         })
-      }
+      }}
       submitDisabled={updatePolicy.isLoading || !policy}
-      error={updatePolicy.error?.error as Error | undefined}
-      {...props}
+      submitError={updatePolicy.error}
+      submitLabel="Update role"
+      onDismiss={onDismiss}
     >
-      <ListboxField id="roleName" name="roleName" label="Role" items={roleItems} required />
-      <Form.Submit>Update role</Form.Submit>
+      {(control) => (
+        <ListboxField
+          name="roleName"
+          label="Role"
+          items={roleItems}
+          required
+          control={control}
+        />
+      )}
     </SideModalForm>
   )
 }
