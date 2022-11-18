79480289c3aa056603aee7c461ecc46cc17072d9
Benjamin Leonard <hello@benleonard.co.uk>
2022-11-18 17:43:53 +0000

    Updated design for 404 page

--- app/pages/NotFound.tsx
+++ app/pages/NotFound.tsx
@@ -1,13 +1,28 @@
+import { Error12Icon } from '@oxide/ui'
+
 export default function NotFound() {
   return (
     <div className="flex w-full justify-center">
-      <div className="my-48 w-96 space-y-4">
-        <div>
-          <h1 className="text-center text-sans-light-2xl text-accent">
-            Page not found.
-            <br />
-            🤷 Sorry.
+      <div
+        className="fixed top-0 left-0 right-0 bottom-0 z-0 opacity-20"
+        style={{
+          backgroundImage:
+            'linear-gradient(to bottom, var(--surface-error), var(--surface-default))',
+        }}
+      />
+      <div className="absolute top-1/2 left-1/2 flex w-96 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center space-y-4 rounded border p-8 !bg-error-secondary border-error-tertiary elevation-3">
+        <div className="my-2 flex flex inline-flex h-12 w-12 items-center justify-center">
+          <div className="absolute h-12 w-12 animate-[ping_2s_cubic-bezier(0,0,0.2,1)_infinite] rounded-full opacity-20 bg-destructive" />
+          <Error12Icon className="relative h-8 w-8 text-error" />
+        </div>
+
+        <div className="space-y-1">
+          <h1 className="text-center text-[32px] text-sans-light-2xl text-error">
+            Page not found
           </h1>
+          <p className="text-center text-error-tertiary">
+            The page you are looking for doesn’t exist or you may not have access to it.
+          </p>
         </div>
       </div>
     </div>
