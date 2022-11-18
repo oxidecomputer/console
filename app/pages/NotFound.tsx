514978a8bc8bfa494d2159badf2c8cb342f8e697
David Crespo <david.crespo@oxidecomputer.com>
2022-11-18 12:53:54 -0600

    respect reduced motion

--- app/pages/NotFound.tsx
+++ app/pages/NotFound.tsx
@@ -1,6 +1,10 @@
+import { useReducedMotion } from '@react-spring/web'
+import cn from 'classnames'
+
 import { Error12Icon } from '@oxide/ui'
 
 export default function NotFound() {
+  const reducedMotion = useReducedMotion()
   return (
     <div className="flex w-full justify-center">
       <div
@@ -10,16 +14,21 @@ export default function NotFound() {
             'radial-gradient(200% 100% at 50% 100%, var(--surface-default) 0%, #161B1D 100%)',
         }}
       />
-      <div className="absolute top-1/2 left-1/2 flex w-96 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center space-y-4 rounded border p-8 !bg-raise border-secondary elevation-3">
-        <div className="my-2 flex inline-flex h-12 w-12 items-center justify-center">
-          <div className="absolute h-12 w-12 animate-[ping_2s_cubic-bezier(0,0,0.2,1)_infinite] rounded-full opacity-20 bg-destructive" />
+      <div className="flex absolute top-1/2 left-1/2 w-96 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center space-y-4 rounded border p-8 !bg-raise border-secondary elevation-3">
+        <div className="flex my-2 h-12 w-12 items-center justify-center">
+          <div
+            className={cn('absolute h-12 w-12 rounded-full opacity-20 bg-destructive', {
+              'animate-[ping_2s_cubic-bezier(0,0,0.2,1)_infinite]': !reducedMotion,
+            })}
+          />
           <Error12Icon className="relative h-8 w-8 text-error" />
         </div>
 
         <div className="space-y-2">
           <h1 className="text-center text-sans-light-3xl">Page not found</h1>
           <p className="text-center text-tertiary">
-            The page you are looking for doesn’t exist or you may not have access to it.
+            The page you are looking for doesn&apos;t exist or you may not have access to
+            it.
           </p>
         </div>
       </div>
