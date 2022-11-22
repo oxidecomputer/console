87f9f092e6725473b0036305e05905c4b98c7360
Benjamin Leonard <hello@benleonard.co.uk>
2022-11-22 16:40:58 +0000

    Add back to console button

--- app/pages/NotFound.tsx
+++ app/pages/NotFound.tsx
@@ -1,15 +1,26 @@
-import { Error12Icon } from '@oxide/ui'
+import { Link } from 'react-router-dom'
+
+import { DirectionLeftIcon, Error12Icon } from '@oxide/ui'
 
 export default function NotFound() {
   return (
     <div className="flex w-full justify-center">
       <div
-        className="fixed top-0 left-0 right-0 bottom-0 z-0"
+        className="fixed top-0 left-0 right-0 bottom-0 -z-10"
         style={{
           background:
             'radial-gradient(200% 100% at 50% 100%, var(--surface-default) 0%, #161B1D 100%)',
         }}
       />
+      <div className="w-full">
+        <Link
+          to="/"
+          className="flex inline-flex items-center p-6 text-mono-sm text-secondary hover:text-default"
+        >
+          <DirectionLeftIcon title="Select" className="mr-2 w-2 text-tertiary" />
+          Back to console
+        </Link>
+      </div>
       <div className="absolute top-1/2 left-1/2 flex w-96 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center space-y-4 rounded-lg border p-8 !bg-raise border-secondary elevation-3">
         <div className="my-2 flex inline-flex h-12 w-12 items-center justify-center">
           <div className="absolute h-12 w-12 rounded-full opacity-20 bg-destructive motion-safe:animate-[ping_2s_cubic-bezier(0,0,0.2,1)_infinite]" />
