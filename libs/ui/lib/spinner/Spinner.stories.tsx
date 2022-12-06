8be394f0348c515a92585bfb2cf791afaceb37cf
Benjamin Leonard <hello@benleonard.co.uk>
2022-12-06 12:14:01 +0000

    New spinner in two sizes with eased animation

--- libs/ui/lib/spinner/Spinner.stories.tsx
+++ libs/ui/lib/spinner/Spinner.stories.tsx
@@ -1,3 +1,13 @@
-import { Spinner } from './Spinner'
+import { Spinner, sizes, variants } from './Spinner'
 
-export const Default = () => <Spinner />
+export const Default = () => (
+  <div className="space-y-2">
+    {sizes.map((size) => (
+      <div key={size} className="flex flex-row flex-wrap space-x-2">
+        {variants.map((variant) => (
+          <Spinner key={size + variant} size={size} variant={variant} />
+        ))}
+      </div>
+    ))}
+  </div>
+)
