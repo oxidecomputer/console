4f3f364d53a944175a21b00fed213de887d44311
David Crespo <david.crespo@oxidecomputer.com>
2023-01-23 13:14:55 -0600

    hard coded properties table on update page for system state

--- app/pages/system/UpdatePage.tsx
+++ app/pages/system/UpdatePage.tsx
@@ -2,7 +2,14 @@ import { Outlet } from 'react-router-dom'
 
 import { apiQueryClient } from '@oxide/api'
 import { DateCell, linkCell, useQueryTable } from '@oxide/table'
-import { EmptyMessage, PageHeader, PageTitle, SoftwareUpdate16Icon } from '@oxide/ui'
+import {
+  Badge,
+  EmptyMessage,
+  PageHeader,
+  PageTitle,
+  PropertiesTable,
+  SoftwareUpdate16Icon,
+} from '@oxide/ui'
 
 import { pb } from 'app/util/path-builder'
 
@@ -28,6 +35,18 @@ export function UpdatePage() {
           New provider
         </Link>
       </TableActions> */}
+
+      <PropertiesTable className="mb-8">
+        <PropertiesTable.Row label="status">
+          <Badge color="neutral">Steady</Badge>
+        </PropertiesTable.Row>
+        <PropertiesTable.Row label="version">
+          <span className="text-secondary">v1.0.2–v1.2.4</span>
+        </PropertiesTable.Row>
+        <PropertiesTable.Row label="last updated">
+          <span className="text-secondary">{new Date().toISOString()}</span>
+        </PropertiesTable.Row>
+      </PropertiesTable>
       <Table emptyState={<EmptyState />}>
         <Column
           accessor="version"
