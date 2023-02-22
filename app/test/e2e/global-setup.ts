1742b3fecdbb0a22348532279ad18a9ff2521a62
David Crespo <david.crespo@oxidecomputer.com>
2023-02-22 10:57:26 -0600

    bump esbuild

--- app/test/e2e/global-setup.ts
+++ app/test/e2e/global-setup.ts
@@ -6,8 +6,5 @@ import { createServer } from '@mswjs/http-middleware'
 import { handlers } from '@oxide/api-mocks'
 
 export default async function globalSetup() {
-  // For tests not relying on mocked data and not pointing to a real server, start a local mock server
-  if (!process.env.API_URL) {
-    createServer(...handlers).listen(12220)
-  }
+  createServer(...handlers).listen(12220)
 }
