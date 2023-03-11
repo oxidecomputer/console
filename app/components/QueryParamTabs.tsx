211a7d8d54282e18dbfaf2770963261c83bd1a1c
David Crespo <david-crespo@users.noreply.github.com>
2023-03-11 11:04:35 -0600

    Switch to Radix tabs (#1394)
    * basic radix tabs with our styling
    
    * fancy schmancy classed.inject utility
    
    * convert query-synced tabs to new one
    
    * remove reach tabs for real
    
    * remove unused helper
    
    * bring back the infamous div, fix instance create default values
    
    * consolidate ox-tab-panel and ox-tabs-panel
    
    * missing flex was making everything look like shit
    
    * fix sneaky left gutter on instance create
    
    * on second thought, nix the helper, and require defaultValue on Root
    
    * QueryTabs -> QueryParamTabs

---
+++ app/components/QueryParamTabs.tsx
@@ -0,0 +1,25 @@
+import { useSearchParams } from 'react-router-dom'
+
+import { Tabs, type TabsRootProps } from '@oxide/ui'
+
+/**
+ * Use instead of `Tabs.Root` to sync current tab with arg in URL query string.
+ *
+ * If you don't want the query arg functionality, e.g., if you have multiple
+ * sets of tabs on the same page, use `Tabs.Root` directly.
+ */
+export function QueryParamTabs(props: TabsRootProps) {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const value = searchParams.get('tab') || props.defaultValue
+
+  function onValueChange(newValue: string) {
+    if (newValue === props.defaultValue) {
+      searchParams.delete('tab')
+    } else {
+      searchParams.set('tab', newValue)
+    }
+    setSearchParams(searchParams, { replace: true })
+  }
+
+  return <Tabs.Root {...props} value={value} onValueChange={onValueChange} />
+}
