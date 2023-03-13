d211c8fd5ccb44f938e63d453c4ea60ac1be59b5
Benjamin Leonard <hello@benleonard.co.uk>
2023-03-13 11:43:33 +0000

    No longer needed now that truncate isn't in `util`

--- libs/ui/lib/tooltip/Tooltip.stories.tsx
+++ libs/ui/lib/tooltip/Tooltip.stories.tsx
@@ -1,4 +1,3 @@
-import { Truncate } from '../../util/truncate'
 import { Filter12Icon } from '../icons'
 import { Tooltip } from './Tooltip'
 
@@ -9,26 +8,5 @@ export const Default = () => (
         <Filter12Icon />
       </button>
     </Tooltip>
-    <div>
-      <Truncate text="document.getElementById(foo).innerHTML" maxLength={30} />
-    </div>
-    <div>
-      <Truncate
-        text="document.getElementById(foo).innerHTML"
-        maxLength={30}
-        position="middle"
-      />
-    </div>
-    <div className="text-secondary">
-      <Truncate
-        text="document.getElementById(foo).innerHTML"
-        maxLength={30}
-        position="middle"
-        hasCopyButton
-      />
-    </div>
-    <div>
-      <Truncate text="document.getElementById(foo).innerHTML" maxLength={100} />
-    </div>
   </>
 )
