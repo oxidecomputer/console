13c9865c231ff20982649dce80e9077d51ab0688
David Crespo <david.crespo@oxidecomputer.com>
2023-03-14 14:46:05 -0500

    also forward ref on DropdownMenu.Content

--- libs/ui/lib/dropdown-menu/DropdownMenu.tsx
+++ libs/ui/lib/dropdown-menu/DropdownMenu.tsx
@@ -6,26 +6,25 @@ import { Content, Item, Portal, Root, Trigger } from '@radix-ui/react-dropdown-m
 import cn from 'classnames'
 import { type ForwardedRef, forwardRef } from 'react'
 
+type DivRef = ForwardedRef<HTMLDivElement>
+
 export const DropdownMenu = {
   Root,
   Trigger,
   Portal,
-  Content: ({ className, ...props }: DropdownMenuContentProps) => (
+  // don't need to forward ref here for a particular reason but Radix gives a
+  // big angry warning if we don't
+  Content: forwardRef(({ className, ...props }: DropdownMenuContentProps, ref: DivRef) => (
     <Content
+      {...props}
       // prevents focus ring showing up on trigger when you close the menu
       onCloseAutoFocus={(e) => e.preventDefault()}
       className={cn('DropdownMenuContent', className)}
-      {...props}
+      ref={ref}
     />
-  ),
+  )),
   // need to forward ref because of tooltips on disabled menu buttons
-  Item: forwardRef(
-    ({ className, ...props }: DropdownMenuItemProps, ref: ForwardedRef<HTMLDivElement>) => (
-      <Item
-        {...props}
-        className={cn('DropdownMenuItem ox-menu-item', className)}
-        ref={ref}
-      />
-    )
-  ),
+  Item: forwardRef(({ className, ...props }: DropdownMenuItemProps, ref: DivRef) => (
+    <Item {...props} className={cn('DropdownMenuItem ox-menu-item', className)} ref={ref} />
+  )),
 }
