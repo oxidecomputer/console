8c9e1aeab34c35475db2be7eea520b5f61914b99
David Crespo <david.crespo@oxidecomputer.com>
2023-03-13 21:37:30 -0500

    fix e2e tests

--- app/pages/__tests__/silo-access.e2e.ts
+++ app/pages/__tests__/silo-access.e2e.ts
@@ -85,11 +85,11 @@ test('Click through silo access page', async ({ page }) => {
   await expectRowVisible(table, { Name: user3.display_name, 'Silo role': 'viewer' })
 
   // now delete user 3
+  await expectVisible(page, [`role=cell[name="${user3.display_name}"]`])
   await page
     .locator('role=row', { hasText: user3.display_name })
     .locator('role=button[name="Row actions"]')
     .click()
-  await expectVisible(page, [`role=cell[name="${user3.display_name}"]`])
   await page.click('role=menuitem[name="Delete"]')
   await expectNotVisible(page, [`role=cell[name="${user3.display_name}"]`])
 })
