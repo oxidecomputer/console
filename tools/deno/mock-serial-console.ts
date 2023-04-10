fb8e2c3d5976de311171eb0627c9f9f4e557bd40
David Crespo <david.crespo@oxidecomputer.com>
2023-04-10 12:34:53 -0500

    print a progress bar as part of the serial console

--- tools/deno/mock-serial-console.ts
+++ tools/deno/mock-serial-console.ts
@@ -1,4 +1,4 @@
-#! /usr/bin/env -S deno run --allow-run --allow-net
+#! /usr/bin/env -S deno run --watch --allow-run --allow-net
 import { delay } from 'https://deno.land/std@0.181.0/async/delay.ts'
 import { serve } from 'https://deno.land/std@0.181.0/http/server.ts'
 
@@ -7,10 +7,39 @@ import { serve } from 'https://deno.land/std@0.181.0/http/server.ts'
  * run this little server and configure Vite to proxy WS requests to it.
  */
 
+const enc = new TextEncoder()
+
 async function streamString(socket: WebSocket, s: string, delayMs = 50) {
   for (const c of s) {
-    socket.send(new TextEncoder().encode(c))
+    socket.send(enc.encode(c))
     await delay(delayMs)
+  }
+}
+
+// ANSI escape codes
+const RESET_FMT = '\x1b[0m' // reset all formatting
+const SET_GREEN = '\x1b[32m' // setting the foreground color to green
+const CURSOR_HOME = '\x1b[1G' // move cursor to the beginning of the line
+
+function generateProgressBar(progress: number) {
+  const width = 30
+  const filledWidth = Math.floor(progress * width)
+  const emptyWidth = width - filledWidth
+
+  const filledPart = SET_GREEN + '#'.repeat(filledWidth) + RESET_FMT
+  const emptyPart = '-'.repeat(emptyWidth)
+  const percentage = Math.floor(progress * 100)
+
+  return `[${filledPart}${emptyPart}] ${percentage}%`
+}
+
+async function displayProgressBar(socket: WebSocket) {
+  for (let progress = 0; progress <= 1; progress += 0.02) {
+    if (socket.readyState !== WebSocket.OPEN) return
+
+    const progressBar = generateProgressBar(progress)
+    socket.send(enc.encode(CURSOR_HOME + progressBar))
+    await delay(100)
   }
 }
 
@@ -22,10 +51,11 @@ async function serialConsole(req: Request) {
   console.log(`New client connected`)
 
   // send hello as a binary frame for xterm to display
-  socket.onopen = () => {
-    setTimeout(() => {
-      streamString(socket, 'Wake up Neo...')
-    }, 200)
+  socket.onopen = async () => {
+    await delay(200)
+    await streamString(socket, 'Wake up Neo...\n')
+    await displayProgressBar(socket)
+    await streamString(socket, '\n' + CURSOR_HOME + 'Done!\n' + CURSOR_HOME)
   }
 
   // echo back binary data
