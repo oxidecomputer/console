bbd97c5f9b0375e00772db0b857e2af68bfbbbf2
Benjamin Leonard <benji@oxide.computer>
2023-05-12 17:19:13 +0100

    Create disk from image or snapshot (#1482)
    * Block out disk from image or snapshot
    
    * Only need block size for blank disks
    
    * Add empty state for listbox
    
    * Fix focus edgecase
    
    This is a manual focus so we need to do the `ring-error-secondary` manually
    
    * Update app/forms/disk-create.tsx
    
    Co-authored-by: Justin Bennett <oxide@just-be.dev>
    
    * nest the thing in the thing
    
    * Clarify snapshot meta
    
    * dead simple e2e tests
    
    ---------
    
    Co-authored-by: Justin Bennett <oxide@just-be.dev>
    Co-authored-by: David Crespo <david.crespo@oxidecomputer.com>

--- app/forms/disk-create.tsx
+++ app/forms/disk-create.tsx
@@ -1,14 +1,19 @@
+import { format } from 'date-fns'
+import type { Control } from 'react-hook-form'
+import { useController } from 'react-hook-form'
 import type { NavigateFunction } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
 import type { BlockSize, Disk, DiskCreate } from '@oxide/api'
-import { useApiMutation, useApiQueryClient } from '@oxide/api'
+import { useApiMutation, useApiQuery, useApiQueryClient } from '@oxide/api'
 import { Divider } from '@oxide/ui'
+import { FieldLabel, Radio, RadioGroup } from '@oxide/ui'
 import { GiB } from '@oxide/util'
 
 import {
   DescriptionField,
   DiskSizeField,
+  ListboxField,
   NameField,
   RadioField,
   SideModalForm,
@@ -79,6 +84,40 @@ export function CreateDiskSideModalForm({
           <NameField name="name" control={control} />
           <DescriptionField name="description" control={control} />
           <Divider />
+          <DiskSourceField control={control} />
+          <DiskSizeField name="size" control={control} />
+        </>
+      )}
+    </SideModalForm>
+  )
+}
+
+const DiskSourceField = ({ control }: { control: Control<DiskCreate> }) => {
+  const {
+    field: { value, onChange },
+  } = useController({ control, name: 'diskSource' })
+
+  return (
+    <>
+      <div className="max-w-lg space-y-2">
+        <FieldLabel id="disk-source-label">Source</FieldLabel>
+        <RadioGroup
+          aria-labelledby="disk-source-label"
+          name="diskSource"
+          column
+          defaultChecked={value.type}
+          onChange={(event) => {
+            const newType = event.target.value as DiskCreate['diskSource']['type']
+            onChange({ type: newType })
+          }}
+        >
+          <Radio value="blank">Blank</Radio>
+          <Radio value="snapshot">Snapshot</Radio>
+          <Radio value="image">Image</Radio>
+        </RadioGroup>
+      </div>
+      <div className="max-w-lg">
+        {value.type === 'blank' && (
           <RadioField
             column
             name="diskSource.blockSize"
@@ -91,9 +130,89 @@ export function CreateDiskSideModalForm({
               { label: '512', value: 512 },
             ]}
           />
-          <DiskSizeField name="size" control={control} />
-        </>
-      )}
-    </SideModalForm>
+        )}
+        {value.type === 'image' && <ImageSelectField control={control} />}
+
+        {value.type === 'snapshot' && <SnapshotSelectField control={control} />}
+      </div>
+    </>
+  )
+}
+
+const ImageSelectField = ({ control }: { control: Control<DiskCreate> }) => {
+  const projectSelector = useProjectSelector()
+
+  const images =
+    useApiQuery('imageList', { query: { includeSiloImages: true, ...projectSelector } })
+      .data?.items || []
+
+  return (
+    <ListboxField
+      control={control}
+      name="diskSource.imageId"
+      label="Source image"
+      placeholder="Select an image"
+      items={images.map((i) => {
+        return {
+          value: i.id,
+          labelString: `${i.name} (${i.os}, ${i.version})`,
+          label: (
+            <>
+              <div>{i.name}</div>
+              <div className="text-secondary">
+                {i.os} <span className="text-quinary">/</span> {i.version}{' '}
+                <span className="text-quinary">/</span>{' '}
+                {i.projectId ? 'Project image' : 'Silo image'}
+              </div>
+            </>
+          ),
+        }
+      })}
+      required
+    />
+  )
+}
+
+const SnapshotSelectField = ({ control }: { control: Control<DiskCreate> }) => {
+  const projectSelector = useProjectSelector()
+
+  const snapshots =
+    useApiQuery('snapshotList', { query: projectSelector }).data?.items || []
+
+  const DiskNameFromId = ({ value }: { value: string }) => {
+    const { data, isLoading, isError } = useApiQuery(
+      'diskView',
+      { path: { disk: value } },
+      // this can 404 if the source disk has been deleted, and that's fine
+      { useErrorBoundary: false }
+    )
+
+    if (isLoading || isError) return null
+    return <> from {data.name}</>
+  }
+
+  return (
+    <ListboxField
+      control={control}
+      name="diskSource.snapshotId"
+      label="Source snapshot"
+      placeholder="Select a snapshot"
+      items={snapshots.map((i) => {
+        return {
+          value: i.id,
+          labelString: `${i.name}`,
+          label: (
+            <>
+              <div>{i.name}</div>
+              <div className="text-secondary">
+                Created on {format(i.timeCreated, 'MMM d, yyyy')}
+                <DiskNameFromId value={i.diskId} />
+              </div>
+            </>
+          ),
+        }
+      })}
+      required
+    />
   )
 }
---
+++ app/pages/__tests__/disk-create.e2e.ts
@@ -0,0 +1,33 @@
+import { expectVisible, test } from 'app/test/e2e'
+
+test.describe('Disk create', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/projects/mock-project/disks-new')
+    await page.getByRole('textbox', { name: 'Name' }).fill('a-new-disk')
+  })
+
+  test.afterEach(async ({ page }) => {
+    await page.getByRole('button', { name: 'Create disk' }).click()
+
+    await expectVisible(page, ['text="Your disk has been created"'])
+
+    await page.getByRole('button', { name: 'Next' }).click()
+    await expectVisible(page, ['role=cell[name="a-new-disk"]'])
+  })
+
+  test('from blank', async ({ page }) => {
+    await page.getByRole('radio', { name: '512' }).click()
+  })
+
+  test('from snapshot', async ({ page }) => {
+    await page.getByRole('radio', { name: 'Snapshot' }).click()
+    await page.getByRole('button', { name: 'Source' }).click()
+    await page.getByRole('option', { name: 'snapshot-3' }).click()
+  })
+
+  test('from image', async ({ page }) => {
+    await page.getByRole('radio', { name: 'Image' }).click()
+    await page.getByRole('button', { name: 'Source' }).click()
+    await page.getByRole('option', { name: 'image-3' }).click()
+  })
+})
