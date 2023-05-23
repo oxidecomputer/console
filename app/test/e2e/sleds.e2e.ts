6841a4f554fa743f10f6174b68d57286de155170
Justin Bennett <oxide@just-be.dev>
2023-05-22 23:46:16 -0400

    Use correct expect, await expects

--- app/test/e2e/sleds.e2e.ts
+++ app/test/e2e/sleds.e2e.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 import { expectRowVisible, expectVisible } from './utils'
 
@@ -8,8 +8,8 @@ test('Sled inventory page', async ({ page }) => {
   await expectVisible(page, ['role=heading[name*="Inventory"]'])
 
   const sledsTab = page.getByRole('tab', { name: 'Sleds' })
-  expect(sledsTab).toBeVisible()
-  expect(sledsTab).toHaveClass('.is-selected')
+  await expect(sledsTab).toBeVisible()
+  await expect(sledsTab).toHaveClass('.is-selected')
 
   const sledsTable = page.getByRole('table')
   await expectRowVisible(sledsTable, {
@@ -24,8 +24,8 @@ test('Sled inventory page', async ({ page }) => {
   await expectVisible(page, ['role=heading[name*="Sled 0"'])
 
   const instancesTab = page.getByRole('tab', { name: 'Instances' })
-  expect(instancesTab).toBeVisible()
-  expect(instancesTab).toHaveClass('.is-selected')
+  await expect(instancesTab).toBeVisible()
+  await expect(instancesTab).toHaveClass('.is-selected')
 
   const instancesTable = page.getByRole('table')
   await expectRowVisible(instancesTable, {
