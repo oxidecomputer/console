9b3787352f6065af36edbae414bee6500ce1c292
Justin Bennett <oxide@just-be.dev>
2023-05-22 22:05:34 -0400

    Update role methods, fix selector

--- app/test/e2e/sleds.e2e.ts
+++ app/test/e2e/sleds.e2e.ts
@@ -5,29 +5,29 @@ import { expectRowVisible, expectVisible } from './utils'
 test('Sled inventory page', async ({ page }) => {
   await page.goto('/system/inventory/sleds')
 
-  await expectVisible(page, ['role=heading[name*="Inventory"'])
+  await expectVisible(page, ['role=heading[name*="Inventory"]'])
 
-  const sledsTab = page.locator('role=tab[name="Sleds"]')
+  const sledsTab = page.getByRole('tab', { name: 'Sleds' })
   expect(sledsTab).toBeVisible()
   expect(sledsTab).toHaveClass('.is-selected')
 
-  const sledsTable = page.locator('role=table')
+  const sledsTable = page.getByRole('table')
   await expectRowVisible(sledsTable, {
     // TODO: Once sled location is piped through this'll need to be dynamic
     location: 'SLD0',
   })
 
   // Visit the sled detail page of the first sled
-  sledsTable.locator('role=link').first().click()
+  sledsTable.getByRole('link').first().click()
 
   // TODO: Once sled location is piped through this'll need to be dynamic
   await expectVisible(page, ['role=heading[name*="Sled 0"'])
 
-  const instancesTab = page.locator('role=tab[name="Instances"]')
+  const instancesTab = page.getByRole('tab', { name: 'Instances' })
   expect(instancesTab).toBeVisible()
   expect(instancesTab).toHaveClass('.is-selected')
 
-  const instancesTable = page.locator('role=table')
+  const instancesTable = page.getByRole('table')
   await expectRowVisible(instancesTable, {
     name: 'default-silo / mock-project\ndb1',
   })
