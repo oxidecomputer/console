b73499fb69ef1022562ef4e23ee88ccbab254e04
David Crespo <david.crespo@oxidecomputer.com>
2023-06-23 13:35:08 -0500

    lil e2es

---
+++ app/test/e2e/utilization.e2e.ts
@@ -0,0 +1,24 @@
+import { expectNotVisible, expectVisible, test } from './utils'
+
+// not trying to get elaborate here. just make sure the pages load, which
+// exercises the loader prefetches and invariant checks
+
+test('System utilization', async ({ page }) => {
+  await page.goto('/system/utilization')
+  await expectVisible(page, [
+    page.getByRole('heading', { name: 'Capacity & Utilization' }),
+    page.getByText('Disk capacity'),
+    page.getByText('CPU capacity'),
+    page.getByText('Memory capacity'),
+  ])
+})
+
+test('Silo utilization', async ({ page }) => {
+  await page.goto('/utilization')
+  await expectVisible(page, [page.getByRole('heading', { name: 'Capacity & Utilization' })])
+  await expectNotVisible(page, [
+    page.getByText('Disk capacity'),
+    page.getByText('CPU capacity'),
+    page.getByText('Memory capacity'),
+  ])
+})
