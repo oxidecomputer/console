c1fc9bc85f842179f038c2e1ae01c35139e63047
David Crespo <david.crespo@oxidecomputer.com>
2023-07-18 12:15:50 -0500

    don't show msw banner in local dev

--- vite.config.ts
+++ vite.config.ts
@@ -51,6 +51,8 @@ export default defineConfig(({ mode }) => ({
   },
   define: {
     'process.env.MSW': JSON.stringify(apiMode === 'msw'),
+    // we don't want to have to look at this banner all day
+    'process.env.MSW_BANNER': JSON.stringify(apiMode === 'msw' && mode === 'production'),
     // used in production build to console.log the SHA at page load
     'process.env.SHA': JSON.stringify(process.env.SHA),
     // used by MSW — number for % likelihood of API request failure (decimals allowed)
