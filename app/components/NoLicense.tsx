b01b0776d8dc65c2f08d773439b89814f719f563
David Crespo <david-crespo@users.noreply.github.com>
2023-07-18 16:45:49 -0500

    Update app/components/NoLicense.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/components/NoLicense.tsx
+++ app/components/NoLicense.tsx
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *  
+ * Copyright Oxide Computer Company
+ */
+
 export function NoLicenseHere() {
   return null
 }
