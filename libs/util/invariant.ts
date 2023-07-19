48c7a5e522581d4f8c262e408a58deb15add4a48
David Crespo <david.crespo@oxidecomputer.com>
2023-07-19 18:39:24 -0500

    unknown is usually better than any

--- libs/util/invariant.ts
+++ libs/util/invariant.ts
@@ -10,8 +10,6 @@
  * Throw error if `condition` is falsy. Not using `tiny-invariant` because we
  * want the full error in production.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function invariant(condition: any, message: string): asserts condition {
-  if (condition) return
-  throw new Error(`Invariant failed: ${message}`)
+export function invariant(condition: unknown, message: string): asserts condition {
+  if (!condition) throw new Error(`Invariant failed: ${message}`)
 }
