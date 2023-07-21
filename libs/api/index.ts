e998c8d56d140969019e97da4f8d5a5b5896e2bd
David Crespo <david.crespo@oxidecomputer.com>
2023-07-21 16:47:15 -0500

    regen API with token auth change and cleanup

--- libs/api/index.ts
+++ libs/api/index.ts
@@ -5,9 +5,9 @@
  *
  * Copyright Oxide Computer Company
  */
-// for convenience so we can do `import type { ApiTypes } from '@oxide/api'`
 import { QueryClient } from '@tanstack/react-query'
 
+// for convenience so we can do `import type { ApiTypes } from '@oxide/api'`
 import type * as ApiTypes from './__generated__/Api'
 import { Api } from './__generated__/Api'
 import {
@@ -19,9 +19,10 @@ import {
   wrapQueryClient,
 } from './hooks'
 
-// unit tests run in Node, whose fetch implementation requires a full URL
-const baseUrl = process.env.NODE_ENV === 'test' ? 'http://testhost' : ''
-export const api = new Api({ baseUrl })
+export const api = new Api({
+  // unit tests run in Node, whose fetch implementation requires a full URL
+  host: process.env.NODE_ENV === 'test' ? 'http://testhost' : '',
+})
 
 export type ApiMethods = typeof api.methods
 
