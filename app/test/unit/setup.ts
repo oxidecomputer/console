b2d5c5afbeb07434add7d0a4edb296dbe9c95d4e
David Crespo <david.crespo@oxidecomputer.com>
2023-10-13 15:30:47 -0500

    bump almost all the deps

--- app/test/unit/setup.ts
+++ app/test/unit/setup.ts
@@ -10,24 +10,13 @@
  * This file is ran by vitest before any tests are ran. Configuration
  * in this file does _not_ impact end-to-end tests.
  */
-import matchers, { type TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
-import { afterAll, afterEach, beforeAll, expect } from 'vitest'
+import { afterAll, afterEach, beforeAll } from 'vitest'
 
 import { resetDb } from '@oxide/api-mocks'
 
 import { server } from './server'
-
-// fancy asserts
-expect.extend(matchers)
-
-// make TS not be confused by the jest matchers being on the vitest expect
-declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  interface JestAssertion<T = any>
-    extends jest.Matchers<void, T>,
-      TestingLibraryMatchers<T, void> {}
-}
 
 beforeAll(() => server.listen())
 afterEach(() => {
