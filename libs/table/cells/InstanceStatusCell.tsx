69927db1273b07ac8fb92c0771542ee01cb1ddb9
Charlie Park <charlie@oxidecomputer.com>
2024-01-16 12:33:42 -0800

    Add a tooltip with absolute datetime to relative time element (#1879)
    * Add a tooltip with absolute datetime to relative time element
    
    * Improve tooltip centering

--- libs/table/cells/InstanceStatusCell.tsx
+++ libs/table/cells/InstanceStatusCell.tsx
@@ -8,21 +8,17 @@
 import type { Instance } from '@oxide/api'
 
 import { InstanceStatusBadge } from 'app/components/StatusBadge'
-import { timeAgoAbbr } from 'app/util/date'
+import { TimeAgo } from 'app/components/TimeAgo'
 
 import type { Cell } from './Cell'
-import { TwoLineCell } from './TwoLineCell'
 
 export const InstanceStatusCell = ({
   value,
 }: Cell<Pick<Instance, 'runState' | 'timeRunStateUpdated'>>) => {
   return (
-    <TwoLineCell
-      detailsClass="text-sans-sm"
-      value={[
-        <InstanceStatusBadge key="run-state" status={value.runState} />,
-        timeAgoAbbr(value.timeRunStateUpdated),
-      ]}
-    ></TwoLineCell>
+    <div className="flex flex-col">
+      <InstanceStatusBadge key="run-state" status={value.runState} />
+      <TimeAgo description="Run state updated" datetime={value.timeRunStateUpdated} />
+    </div>
   )
 }
