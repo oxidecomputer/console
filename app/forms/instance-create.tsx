18fbf1f0adee018eee089f3d40efdc5ab9157676
Charlie Park <charlie@oxidecomputer.com>
2024-01-18 16:19:45 -0800

    Target createInstance.error in useEffect

--- app/forms/instance-create.tsx
+++ app/forms/instance-create.tsx
@@ -144,7 +144,7 @@ export function CreateInstanceForm() {
     if (createInstance.error) {
       setIsSubmitting(false)
     }
-  }, [createInstance])
+  }, [createInstance.error])
 
   return (
     <FullPageForm
