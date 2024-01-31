0669919f26cb037dd85edefb7c3b88a59c52f42b
David Crespo <david.crespo@oxidecomputer.com>
2024-01-31 17:43:05 -0600

    mock IP range add/remove

--- libs/api-mocks/msw/handlers.ts
+++ libs/api-mocks/msw/handlers.ts
@@ -625,13 +625,45 @@ export const handlers = makeHandlers({
 
     return ipPoolSilo
   },
-  ipPoolRangeAdd: NotImplemented,
   ipPoolRangeList({ path, query }) {
     const pool = lookup.ipPool(path)
     const ranges = db.ipPoolRanges.filter((r) => r.ip_pool_id === pool.id)
     return paginated(query, ranges)
   },
-  ipPoolRangeRemove: NotImplemented,
+  ipPoolRangeAdd({ path, body }) {
+    const pool = lookup.ipPool(path)
+
+    const newRange: Json<Api.IpPoolRange> = {
+      id: uuid(),
+      ip_pool_id: pool.id,
+      range: body,
+      time_created: new Date().toISOString(),
+    }
+
+    // TODO: validate that it doesn't overlap with existing ranges
+    db.ipPoolRanges.push(newRange)
+
+    return json(newRange, { status: 201 })
+  },
+  ipPoolRangeRemove({ path, body }) {
+    const pool = lookup.ipPool(path)
+
+    const idsToDelete = db.ipPoolRanges
+      .filter(
+        (r) =>
+          r.ip_pool_id === pool.id &&
+          r.range.first === body.first &&
+          r.range.last === body.last
+      )
+      .map((r) => r.id)
+
+    // if nothing in the DB matches, 404
+    if (idsToDelete.length === 0) throw notFoundErr()
+
+    db.ipPoolRanges = db.ipPoolRanges.filter((r) => !idsToDelete.includes(r.id))
+
+    return 204
+  },
   ipPoolCreate({ body }) {
     errIfExists(db.ipPools, { name: body.name }, 'IP pool')
 
