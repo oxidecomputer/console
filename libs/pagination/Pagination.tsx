951473495601e22a39b5b670c61519a63312eb15
David Crespo <david.crespo@oxidecomputer.com>
2024-02-23 10:29:11 -0600

    shuffle @oxide/pagination

--- libs/pagination/Pagination.tsx
+++ libs/pagination/Pagination.tsx
@@ -5,6 +5,7 @@
  *
  * Copyright Oxide Computer Company
  */
+import { useState } from 'react'
 import tunnel from 'tunnel-rat'
 
 import {
@@ -29,3 +30,28 @@ export function Pagination({ inline = false, ...props }: PaginationProps) {
 }
 
 Pagination.Target = Tunnel.Out
+
+type PageToken = string | undefined
+
+export function usePagination() {
+  const [prevPages, setPrevPages] = useState<PageToken[]>([])
+  const [currentPage, setCurrentPage] = useState<PageToken>()
+
+  const goToPrevPage = () => {
+    const prevPage = prevPages.pop()
+    setCurrentPage(prevPage)
+    setPrevPages(prevPages)
+  }
+
+  const goToNextPage = (nextPageToken: string) => {
+    setPrevPages([...prevPages, currentPage])
+    setCurrentPage(nextPageToken)
+  }
+
+  return {
+    currentPage,
+    goToNextPage,
+    goToPrevPage,
+    hasPrev: prevPages.length > 0,
+  }
+}
--- libs/pagination/index.tsx
+++ libs/pagination/index.tsx
@@ -6,6 +6,30 @@
  * Copyright Oxide Computer Company
  */
 import { useState } from 'react'
+import tunnel from 'tunnel-rat'
+
+import {
+  Pagination as UIPagination,
+  type PaginationProps as UIPaginationProps,
+} from '@oxide/ui'
+
+const Tunnel = tunnel('pagination')
+
+interface PaginationProps extends UIPaginationProps {
+  /** If true pagination will be rendered wherever `Pagination.Target` is included */
+  inline?: boolean
+}
+export function Pagination({ inline = false, ...props }: PaginationProps) {
+  if (inline) return <UIPagination {...props} />
+
+  return (
+    <Tunnel.In>
+      <UIPagination className="gutter h-14 py-5" {...props} />
+    </Tunnel.In>
+  )
+}
+
+Pagination.Target = Tunnel.Out
 
 type PageToken = string | undefined
 
