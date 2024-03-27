9182d1c46c84b66be86ce7b3801a79e443c353b8
David Crespo <david.crespo@oxidecomputer.com>
2024-03-27 16:09:24 -0500

    add commit arg

--- tools/debug-ci-e2e-fail.sh
+++ tools/debug-ci-e2e-fail.sh
@@ -6,32 +6,50 @@
 #  
 # Copyright Oxide Computer Company
 
+# Run this script on a branch to download the E2E failure traces from the
+# latest run on that branch. Pass a commit (full hash, not abbreviated)
+# to pull the traces for that commit.
+
 set -e
 set -o pipefail
 
 DIR="ci-e2e-traces"
 
-# Get the ID of the last github actions run if there was one
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-RUN_ID=$(gh run list -b "$BRANCH" -w CI -L 1 --json databaseId --jq '.[0].databaseId')
+COMMIT=$1
 
-if [ -z "$RUN_ID" ]; then
-  echo "No action runs found for this branch"
-  exit 0
+# Get the ID of the last github actions run if there was one. If a commit
+# is specified, use that, otherwise
+if [ -n "$COMMIT" ]; then
+    echo -e "Looking at latest run on commit $COMMIT"
+    RUN_ID=$(gh run list --commit "$COMMIT" -w CI -L 1 --json databaseId --jq '.[0].databaseId')
+    if [ -z "$RUN_ID" ]; then
+      echo "No action runs found. Make sure to use full commit SHA."
+      exit 0
+    fi
+else 
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    echo -e "Looking at latest run on branch $BRANCH"
+    RUN_ID=$(gh run list -b "$BRANCH" -w CI -L 1 --json databaseId --jq '.[0].databaseId')
+    if [ -z "$RUN_ID" ]; then
+      echo "No action runs found"
+      exit 0
+    fi
 fi
+
 
 if [ -e "$DIR/.run" ] && [ "$RUN_ID" == "$(cat $DIR/.run)" ]; then
     : # Do nothing, the test results are already up to date
 else
     rm -rf $DIR
-    echo "Attempting to download test failure traces for current branch..."
+    echo "Downloading traces..."
     gh run download $RUN_ID --dir $DIR
     echo $RUN_ID > $DIR/.run
 fi
 
-
-echo "Choose a test trace to view"
-select trace in $(find $DIR -name "trace.zip"); do
-    npx playwright show-trace $trace
+# gnarly select and re-find is to take filepath noise out of the select list
+echo "Choose a trace to view"
+select trace in $(find $DIR -name "trace.zip" | sed 's/.*\/\(.*\)\/trace.zip/\1/'); do
+    selected_trace=$(find $DIR -path "*/$trace"/trace.zip)
+    npx playwright show-trace "$selected_trace"
     exit 0
 done
