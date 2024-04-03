8a7280780fdeef0a8ef9a6cf91268d66d267fbec
David Crespo <david.crespo@oxidecomputer.com>
2024-04-03 10:49:42 -0500

    delete some cell files. now we're talkin

--- app/table/columns/common.tsx
+++ app/table/columns/common.tsx
@@ -6,14 +6,30 @@
  * Copyright Oxide Computer Company
  */
 
+import { format } from 'date-fns/format'
+import { filesize } from 'filesize'
+
 import { Truncate } from '~/ui/lib/Truncate'
 
-import { DateCell } from '../cells/DateCell'
-import { SizeCell } from '../cells/SizeCell'
+import { TwoLineCell } from '../cells/TwoLineCell'
 
 // the full type of the info arg is CellContext<Row, Item> from RT, but in these
 // cells we only care about the return value of getValue
 type Info<T> = { getValue: () => T }
+
+function dateCell(info: Info<Date>) {
+  const date = info.getValue()
+  return <TwoLineCell value={[format(date, 'MMM d, yyyy'), format(date, 'p')]} />
+}
+
+function sizeCell(info: Info<number>) {
+  const size = filesize(info.getValue(), { base: 2, output: 'object' })
+  return (
+    <span className="text-secondary">
+      {size.value} <span className="text-quaternary">{size.unit}</span>
+    </span>
+  )
+}
 
 /** Columns used in a bunch of tables */
 export const Columns = {
@@ -21,15 +37,7 @@ export const Columns = {
   description: {
     cell: (info: Info<string>) => <Truncate text={info.getValue()} maxLength={48} />,
   },
-  size: {
-    cell: (info: Info<number>) => <SizeCell value={info.getValue()} />,
-  },
-  timeCreated: {
-    header: 'created',
-    cell: (info: Info<Date>) => <DateCell value={info.getValue()} />,
-  },
-  timeModified: {
-    header: 'modified',
-    cell: (info: Info<Date>) => <DateCell value={info.getValue()} />,
-  },
+  size: { cell: sizeCell },
+  timeCreated: { header: 'created', cell: dateCell },
+  timeModified: { header: 'modified', cell: dateCell },
 }
