112671595ca9aed1be1020a6401abea730a4ea1e
David Crespo <david.crespo@oxidecomputer.com>
2024-04-12 16:54:22 -0500

    clean it up

--- app/ui/lib/Identicon.tsx
+++ app/ui/lib/Identicon.tsx
@@ -8,42 +8,31 @@
 import md5 from 'md5'
 import { useMemo } from 'react'
 
-type Rectangle = {
-  x: number
-  y: number
-  isPixel: boolean
-}
+type Rectangle = { x: number; y: number; isOn: boolean }
 
-const renderPixels = (hash: string) => {
+const getPixels = (s: string) => {
+  const hash = md5(s)
   const buffer: Rectangle[] = []
 
   for (let i = 0; i < 18; i++) {
-    const isPixel = hash.charCodeAt(i) % 2 === 0
+    const isOn = hash.charCodeAt(i) % 2 === 0
 
     if (i < 3) {
       // Start with the two central columns
-      buffer.push({ x: 2, y: i, isPixel: isPixel })
-      buffer.push({ x: 3, y: i, isPixel: isPixel })
+      buffer.push({ x: 2, y: i, isOn })
+      buffer.push({ x: 3, y: i, isOn })
     } else if (i < 6) {
       // Move out to the columns one from the edge
-      buffer.push({ x: 1, y: i - 3, isPixel: isPixel })
-      buffer.push({ x: 4, y: i - 3, isPixel: isPixel })
+      buffer.push({ x: 1, y: i - 3, isOn })
+      buffer.push({ x: 4, y: i - 3, isOn })
     } else if (i < 9) {
       // Fill the outside columns
-      buffer.push({ x: 0, y: i - 6, isPixel: isPixel })
-      buffer.push({ x: 5, y: i - 6, isPixel: isPixel })
+      buffer.push({ x: 0, y: i - 6, isOn })
+      buffer.push({ x: 5, y: i - 6, isOn })
     }
   }
 
   return buffer
-}
-
-function Pixel(pixel: Rectangle) {
-  if (!pixel.isPixel) return null
-
-  const x = pixel.x * 3 + 2 * pixel.x
-  const y = pixel.y * 8 + 2 * pixel.y
-  return <rect key={`${pixel.x}|${pixel.y}`} x={x} y={y} width="3" height="8" />
 }
 
 type IdenticonProps = {
@@ -53,14 +42,17 @@ type IdenticonProps = {
 }
 
 export function Identicon({ name, className }: IdenticonProps) {
-  const pixels = useMemo(() => renderPixels(md5(name)), [name])
+  const pixels = useMemo(() => getPixels(name), [name])
   return (
     <div className={className}>
       <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28">
         <g fill="currentColor">
-          {pixels.map((pixel) => (
-            <Pixel key={`${pixel.x}|${pixel.y}`} {...pixel} />
-          ))}
+          {pixels.map((pixel) => {
+            if (!pixel.isOn) return null
+            const x = pixel.x * 3 + 2 * pixel.x
+            const y = pixel.y * 8 + 2 * pixel.y
+            return <rect key={`${x}|${y}`} x={x} y={y} width="3" height="8" />
+          })}
         </g>
       </svg>
     </div>
