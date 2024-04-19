56549e391208be7413bc9e89d66e33c580dcf4f6
Charlie Park <charlie@oxidecomputer.com>
2024-04-19 12:48:37 -0700

    slightly smaller textbox max-width, to keep measure's character count to ~80

--- app/ui/lib/Message.tsx
+++ app/ui/lib/Message.tsx
@@ -98,7 +98,7 @@ export const Message = ({
         {title && <div className="text-sans-semi-md">{title}</div>}
         <div
           className={cn(
-            'max-w-2xl text-sans-md [&>a]:underline',
+            'max-w-xl text-sans-md [&>a]:underline',
             title ? secondaryTextColor[variant] : textColor[variant]
           )}
         >
