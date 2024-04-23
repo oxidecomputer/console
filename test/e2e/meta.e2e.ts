d2a9345f2a87a08bb05a3668c82f5a8884a1afe3
iliana etaoin <iliana@oxide.computer>
2024-04-23 13:16:49 -0700

    use a content-security-policy in development (#2142)
    * use a content-security-policy in development
    
    * cram things in a bit
    
    * playwright test for dev mode headers
    
    * fix license, add draft docs/csp-headers.md
    
    * add preview script to package.json
    
    * comment explaining why we use the nonce thing, add justification to doc
    
    * remove style-src 'unsafe-inline' :)
    
    * fix the docs and test
    
    * apply patch for react-focus-guards
    
    * patch global styles out of react-remove-scroll
    
    * `patch-package --reverse` before vercel caches it
    
    * patch out the other react-remove-scroll-bar reference
    
    * save another 31 bytes, why not
    
    * Revert "remove style-src 'unsafe-inline' :)"
    
    This reverts commit 22f40bbfc2aabb39646f5f40f8b79b5e817f47ce.
    
    * Revert "fix the docs and test"
    
    This reverts commit 06e13cd5b60f4f950c861e1f4544ea6fb69a228c.
    
    * update docs
    
    ---------
    
    Co-authored-by: David Crespo <david.crespo@oxidecomputer.com>

---
+++ test/e2e/meta.e2e.ts
@@ -0,0 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright Oxide Computer Company
+ */
+import { expect, test } from '@playwright/test'
+
+test('CSP headers', async ({ page }) => {
+  // doesn't matter what page we go to
+  const response = await page.goto('/')
+  expect(response?.headers()).toMatchObject({
+    // note nonce is represented as [0-9a-f]+
+    'content-security-policy': expect.stringMatching(
+      /^default-src 'self'; style-src 'unsafe-inline' 'self'; frame-src 'none'; object-src 'none'; form-action 'none'; frame-ancestors 'none'; script-src 'nonce-[0-9a-f]+' 'self'$/
+    ),
+    'x-content-type-options': 'nosniff',
+    'x-frame-options': 'DENY',
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
  *
  * Copyright Oxide Computer Company
  */
+import { randomBytes } from 'crypto'
 import { resolve } from 'path'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react-swc'
@@ -12,6 +13,8 @@ import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { z } from 'zod'
+
+import vercelConfig from './vercel.json'
 
 const ApiMode = z.enum(['msw', 'dogfood', 'nexus'])
 
@@ -67,6 +70,21 @@ const previewMetaTag = [
   },
 ]
 
+// vercel config is source of truth for headers
+const vercelHeaders = vercelConfig.headers[0].headers
+const headers = Object.fromEntries(vercelHeaders.map((h) => [h.key, h.value]))
+
+// This is only needed for local dev to avoid breaking Vite's script injection.
+// Rather than use unsafe-inline all the time, the nonce approach is much more
+// narrowly scoped and lets us make sure everything *else* works fine without
+// unsafe-inline.
+const cspNonce = randomBytes(8).toString('hex')
+const csp = headers['content-security-policy']
+const devHeaders = {
+  ...headers,
+  'content-security-policy': `${csp}; script-src 'nonce-${cspNonce}' 'self'`,
+}
+
 // see https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
@@ -79,6 +97,8 @@ export default defineConfig(({ mode }) => ({
         app: 'index.html',
       },
     },
+    // prevent inlining assets as `data:`, which is not permitted by our Content-Security-Policy
+    assetsInlineLimit: 0,
   },
   define: {
     'process.env.MSW': JSON.stringify(apiMode === 'msw'),
@@ -99,8 +119,14 @@ export default defineConfig(({ mode }) => ({
     react(),
     apiMode === 'dogfood' && basicSsl(),
   ],
+  html: {
+    // don't include a placeholder nonce in production.
+    // use a CSP nonce in dev to avoid needing to permit 'unsafe-inline'
+    cspNonce: mode === 'production' ? undefined : cspNonce,
+  },
   server: {
     port: 4000,
+    headers: devHeaders,
     // these only get hit when MSW doesn't intercept the request
     proxy: {
       '/v1': {
@@ -119,6 +145,7 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
+  preview: { headers },
   test: {
     environment: 'jsdom',
     setupFiles: ['test/unit/setup.ts'],
