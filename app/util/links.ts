c0bb82fd3662656bedb68f7e3978c317b5428eab
Charlie Park <charlie@oxidecomputer.com>
2024-04-25 10:14:13 -0700

    Add links

--- app/util/links.ts
+++ app/util/links.ts
@@ -6,8 +6,19 @@
  * Copyright Oxide Computer Company
  */
 export const links: Record<string, string> = {
+  accessDocs: 'https://docs.oxide.computer/guides/configuring-access',
   cloudInitFormat: 'https://cloudinit.readthedocs.io/en/latest/explanation/format.html',
   cloudInitExamples: 'https://cloudinit.readthedocs.io/en/latest/reference/examples.html',
+  disksDocs: 'https://docs.oxide.computer/guides/managing-disks-and-snapshots#disks',
   floatingIpsDocs: 'https://docs.oxide.computer/guides/managing-floating-ips',
+  imagesDocs: 'https://docs.oxide.computer/guides/creating-and-sharing-images',
+  instancesDocs: 'https://docs.oxide.computer/guides/managing-instances',
   ipPoolsDocs: 'https://docs.oxide.computer/guides/operator/ip-pool-management',
+  projectsDocs: 'https://docs.oxide.computer/guides/onboarding-projects',
+  siloDocs: 'https://docs.oxide.computer/guides/operator/silo-management',
+  snapshotsDocs:
+    'https://docs.oxide.computer/guides/managing-disks-and-snapshots#snapshots',
+  vmsDocs:
+    'https://docs.oxide.computer/guides/deploying-workloads#_creating_virtual_machines',
+  vpcsDocs: 'https://docs.oxide.computer/guides/configuring-guest-networking',
 }
