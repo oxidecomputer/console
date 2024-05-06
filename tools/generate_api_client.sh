38726931ef2a93d9eb3fba5211acefdcb11cfdbf
David Crespo <david.crespo@oxidecomputer.com>
2024-05-06 10:51:29 -0500

    use TS client generator from npm

--- tools/generate_api_client.sh
+++ tools/generate_api_client.sh
@@ -9,19 +9,13 @@ set -o errexit # exit if anything fails
 set -o pipefail
 set -o xtrace
 
-# script assumes oxide.ts is cloned under the same parent dir as console and
-# you've run `npm install` over there
-
 OMICRON_SHA=$(head -n 1 OMICRON_VERSION)
 GEN_DIR="$PWD/app/api/__generated__"
 
 SPEC_URL="https://raw.githubusercontent.com/oxidecomputer/omicron/$OMICRON_SHA/openapi/nexus.json"
 
-npx tsx ../oxide.ts/generator/index.ts $SPEC_URL $GEN_DIR
+npx @oxide/openapi-gen-ts@0.1.14 $SPEC_URL $GEN_DIR --features msw
 npx prettier --write --log-level error "$GEN_DIR"
-
-# hack until we start pulling this from npm. we don't want this file
-rm "$GEN_DIR/type-test.ts"
 
 cat > $GEN_DIR/OMICRON_VERSION <<EOF
 # generated file. do not update manually. see docs/update-pinned-api.md
