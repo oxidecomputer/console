af7262b425e0088eec05e452fa41a7618b37ef9e
Charlie Park <charlie@oxidecomputer.com>
2024-06-03 14:47:11 -0400

    CSS mostly working, though some redundancies

--- app/ui/lib/Combobox.tsx
+++ app/ui/lib/Combobox.tsx
@@ -20,7 +20,6 @@ import { useState, type ReactNode } from 'react'
 import { SelectArrows6Icon } from '@oxide/design-system/icons/react'
 
 import { FieldLabel } from './FieldLabel'
-import { SpinnerLoader } from './Spinner'
 import { TextInputHint } from './TextInput'
 
 export type ComboboxItem<Value extends string = string> = {
@@ -82,54 +81,54 @@ export const Combobox = ({
                 >
                   <Label>{label}</Label>
                 </FieldLabel>
-                {description && <TextInputHint id={``}>{description}</TextInputHint>}
+                {description && (
+                  <TextInputHint id={description.toLocaleString()}>
+                    {description}
+                  </TextInputHint>
+                )}
               </div>
             )}
 
-            <div className="flex rounded">
+            <ComboboxButton
+              className={cn(
+                `flex h-10 w-full grow items-center justify-between
+                                      rounded border text-sans-md`,
+                hasError
+                  ? 'focus-error border-error-secondary hover:border-error'
+                  : 'border-default hover:border-hover',
+                open && 'ring-2 ring-accent-secondary',
+                open && hasError && 'ring-error-secondary',
+                isDisabled
+                  ? 'cursor-not-allowed text-disabled bg-disabled !border-default'
+                  : 'bg-default',
+                isDisabled && hasError && '!border-error-secondary'
+              )}
+            >
               <ComboboxInput
                 aria-label="Assignee"
                 displayValue={() => (selected ? selected : query)}
                 onChange={(event) => setQuery(event.target.value)}
-                className={`w-full rounded border-none
-          px-3 py-[0.6875rem] !outline-offset-1
-          text-sans-md text-default bg-default
-          placeholder:text-quaternary focus:outline-none disabled:cursor-not-allowed disabled:text-tertiary disabled:bg-disabled`}
+                className={cn(
+                  `w-full rounded !border-none px-3 py-[0.5rem] !outline-none text-sans-md text-default placeholder:text-quaternary disabled:cursor-not-allowed disabled:text-tertiary disabled:bg-disabled`,
+                  isDisabled
+                    ? 'cursor-not-allowed text-disabled bg-disabled !border-default'
+                    : 'bg-default',
+                  hasError && 'focus-error'
+                )}
               />
-              {!isDisabled && <SpinnerLoader isLoading={isLoading} />}
-              <ComboboxButton>
-                <div
-                  //   className="flex h-[calc(100%-12px)] items-center border-l px-3 bg-default border-secondary"
-                  className={cn(
-                    `flex h-10 w-full items-center justify-between
-                    rounded border text-sans-md`,
-                    hasError
-                      ? 'focus-error border-error-secondary hover:border-error'
-                      : 'border-default hover:border-hover',
-                    open && 'ring-2 ring-accent-secondary',
-                    open && hasError && 'ring-error-secondary',
-                    isDisabled
-                      ? 'cursor-not-allowed text-disabled bg-disabled !border-default'
-                      : 'bg-default',
-                    isDisabled && hasError && '!border-error-secondary'
-                  )}
-                  aria-hidden
-                >
-                  <SelectArrows6Icon title="Select" className="w-2 text-tertiary" />
-                </div>
-              </ComboboxButton>
-            </div>
+              <div className="flex items-center border-l px-3 border-secondary" aria-hidden>
+                <SelectArrows6Icon title="Select" className="w-2 text-tertiary" />
+              </div>
+            </ComboboxButton>
             <ComboboxOptions
-              anchor="bottom"
-              className="DropdownMenuContent ox-menu pointer-events-auto relative z-sideModalDropdown overflow-y-auto border-b !outline-none border-secondary last:border-0 empty:hidden"
+              anchor="bottom start"
+              className="DropdownMenuContent ox-menu pointer-events-auto relative z-sideModalDropdown w-[var(--button-width)] overflow-y-auto border border-b !outline-none border-secondary [--anchor-gap:8px] last:border-0 empty:hidden"
             >
               {filteredItems.map((item) => (
                 <ComboboxOption
                   key={item.label}
                   value={item.label}
-                  className={cn(
-                    'DropdownMenuItem ox-menu-item relative border-b text-tertiary border-secondary last:border-0 active:text-accent-secondary selected:text-accent-secondary'
-                  )}
+                  className="relative border text-tertiary border-secondary active:text-accent-secondary selected:text-accent-secondary"
                   onSelect={() => {
                     onChange(item.label)
                     setQuery(item.label)
