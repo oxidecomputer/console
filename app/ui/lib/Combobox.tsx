64a259822562dd62e26e798c3641660c207c31c9
David Crespo <david.crespo@oxidecomputer.com>
2024-06-06 15:40:07 -0500

    use data attr instead of render prop for open

--- app/ui/lib/Combobox.tsx
+++ app/ui/lib/Combobox.tsx
@@ -74,87 +74,83 @@ export const Combobox = ({
         defaultValue={selected}
         disabled={isDisabled || isLoading}
       >
-        {({ open }) => (
-          <>
-            {label && (
-              <div className="mb-2">
-                <FieldLabel id="FieldLabel" as="div" tip={tooltipText} optional={!required}>
-                  <Label>{label}</Label>
-                </FieldLabel>
-                {description && (
-                  <TextInputHint id="TextInputHint">{description}</TextInputHint>
-                )}
-              </div>
-            )}
-            <ComboboxButton
-              className={cn(
-                `flex h-10 w-full grow items-center justify-between rounded border text-sans-md`,
-                hasError
-                  ? 'focus-error border-error-secondary hover:border-error'
-                  : 'border-default hover:border-hover',
-                open && 'ring-2 ring-accent-secondary',
-                open && hasError && 'ring-error-secondary',
-                isDisabled
-                  ? 'cursor-not-allowed text-disabled bg-disabled !border-default'
-                  : 'bg-default',
-                isDisabled && hasError && '!border-error-secondary'
-              )}
-            >
-              <ComboboxInput
-                aria-label="Select a disk"
-                displayValue={() => (selected ? selected : query)}
-                onChange={(event) => setQuery(event.target.value)}
-                placeholder={placeholder}
-                disabled={isDisabled || isLoading}
-                className={cn(
-                  `w-full rounded !border-none px-3 py-[0.5rem] !outline-none text-sans-md text-default placeholder:text-quaternary`,
-                  isDisabled
-                    ? 'cursor-not-allowed text-disabled bg-disabled !border-default'
-                    : 'bg-default',
-                  hasError && 'focus-error'
-                )}
-              />
-              <div className="flex items-center border-l px-3 border-secondary" aria-hidden>
-                <SelectArrows6Icon title="Select" className="w-2 text-tertiary" />
-              </div>
-            </ComboboxButton>
-            <ComboboxOptions
-              anchor="bottom start"
-              // 14px gap is presumably because it's measured from inside the outline or something
-              className="DropdownMenuContent ox-menu pointer-events-auto relative z-sideModalDropdown w-[var(--button-width)] overflow-y-auto border !outline-none border-secondary [--anchor-gap:14px] empty:hidden"
-            >
-              {filteredItems.length === 0 && (
-                <ComboboxOption disabled value="no-matches" className="relative ">
-                  <div className="ox-menu-item !text-disabled">No items match</div>
-                </ComboboxOption>
-              )}
-              {filteredItems.map((item) => (
-                <ComboboxOption
-                  key={item.label}
-                  value={item.label}
-                  className="relative border-b text-tertiary border-secondary last:border-0 active:text-accent-secondary selected:text-accent-secondary"
-                  onSelect={() => {
-                    onChange(item.label)
-                    setQuery(item.label)
-                  }}
-                >
-                  {({ active, selected }) => (
-                    // TODO: redo active styling with `data-active` somehow
-                    <div
-                      className={cn(
-                        'ox-menu-item text-secondary',
-                        selected && 'is-selected',
-                        active && 'is-highlighted'
-                      )}
-                    >
-                      {item.label}
-                    </div>
-                  )}
-                </ComboboxOption>
-              ))}
-            </ComboboxOptions>
-          </>
+        {label && (
+          // TODO: FieldLabel needs a real ID
+          <div className="mb-2">
+            <FieldLabel id="FieldLabel" as="div" tip={tooltipText} optional={!required}>
+              <Label>{label}</Label>
+            </FieldLabel>
+            {description && <TextInputHint id="TextInputHint">{description}</TextInputHint>}
+          </div>
         )}
+        <ComboboxButton
+          className={cn(
+            `flex h-10 w-full grow items-center justify-between rounded border text-sans-md ring-accent-secondary data-[open]:ring-2`,
+            hasError
+              ? 'focus-error border-error-secondary hover:border-error'
+              : 'border-default hover:border-hover',
+            hasError && 'data-[open]:ring-error-secondary',
+            isDisabled
+              ? 'cursor-not-allowed text-disabled bg-disabled !border-default'
+              : 'bg-default',
+            isDisabled && hasError && '!border-error-secondary'
+          )}
+        >
+          <ComboboxInput
+            aria-label="Select a disk"
+            displayValue={() => (selected ? selected : query)}
+            onChange={(event) => setQuery(event.target.value)}
+            placeholder={placeholder}
+            disabled={isDisabled || isLoading}
+            className={cn(
+              `w-full rounded !border-none px-3 py-[0.5rem] !outline-none text-sans-md text-default placeholder:text-quaternary`,
+              isDisabled
+                ? 'cursor-not-allowed text-disabled bg-disabled !border-default'
+                : 'bg-default',
+              hasError && 'focus-error'
+            )}
+          />
+          <div className="flex items-center border-l px-3 border-secondary" aria-hidden>
+            <SelectArrows6Icon title="Select" className="w-2 text-tertiary" />
+          </div>
+        </ComboboxButton>
+        <ComboboxOptions
+          anchor="bottom start"
+          // 14px gap is presumably because it's measured from inside the outline or something
+          className="DropdownMenuContent ox-menu pointer-events-auto relative z-sideModalDropdown w-[var(--button-width)] overflow-y-auto border !outline-none border-secondary [--anchor-gap:14px] empty:hidden"
+        >
+          {filteredItems.length === 0 && (
+            <ComboboxOption disabled value="no-matches" className="relative ">
+              <div className="ox-menu-item !text-disabled">No items match</div>
+            </ComboboxOption>
+          )}
+          {filteredItems.map((item) => (
+            <ComboboxOption
+              key={item.label}
+              value={item.label}
+              className="relative border-b text-tertiary border-secondary last:border-0"
+              onSelect={() => {
+                onChange(item.label)
+                setQuery(item.label)
+              }}
+            >
+              {({ focus, selected }) => (
+                // This *could* be done with data-[focus] and data-[selected] instead, but
+                // it would be a lot more verbose. those can only be used with TW classes,
+                // not our .is-selected and .is-highlighted, so we'd have to copy the pieces
+                // of those rules one by one. Better to rely on the shared classes.
+                <div
+                  className={cn('ox-menu-item', {
+                    'is-selected': selected,
+                    'is-highlighted': focus,
+                  })}
+                >
+                  {item.label}
+                </div>
+              )}
+            </ComboboxOption>
+          ))}
+        </ComboboxOptions>
       </HCombobox>
     </>
   )
