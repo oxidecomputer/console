21c3b2d67f2d0d4b8ea94f2a456f94a23bb28c3a
David Crespo <david.crespo@oxidecomputer.com>
2024-07-16 11:44:06 -0500

    fix e2e test and assert more stuff about the form

--- test/e2e/firewall-rules.e2e.ts
+++ test/e2e/firewall-rules.e2e.ts
@@ -317,15 +317,28 @@ test('create from existing rule', async ({ page }) => {
   const modal = page.getByRole('dialog', { name: 'Add firewall rule' })
   await expect(modal).toBeHidden()
 
-  await clickRowAction(page, 'allow-rdp', 'New similar rule')
+  await clickRowAction(page, 'allow-rdp', 'Clone')
 
   await expect(page).toHaveURL(url + '-new/allow-rdp')
   await expect(modal).toBeVisible()
-  await expect(page.getByRole('textbox', { name: 'Name', exact: true })).toHaveValue(
+  await expect(modal.getByRole('textbox', { name: 'Name', exact: true })).toHaveValue(
     'allow-rdp-1'
   )
 
-  // TODO: should assert all the values, really
+  await expect(modal.getByRole('checkbox', { name: 'TCP' })).toBeChecked()
+  await expect(modal.getByRole('checkbox', { name: 'UDP' })).not.toBeChecked()
+  await expect(modal.getByRole('checkbox', { name: 'ICMP' })).not.toBeChecked()
+
+  await expect(
+    modal
+      .getByRole('table', { name: 'Port filters' })
+      .getByRole('cell', { name: '3389', exact: true })
+  ).toBeVisible()
+  await expect(
+    modal
+      .getByRole('table', { name: 'Targets' })
+      .getByRole('row', { name: 'Name: default, Type: vpc' })
+  ).toBeVisible()
 })
 
 const rulePath = '/projects/mock-project/vpcs/mock-vpc/firewall-rules/allow-icmp/edit'
