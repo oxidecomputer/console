1bb927065dd869493ac1bade1439d93ad5c2a0da
Charlie Park <charlie@oxidecomputer.com>
2024-08-22 12:48:31 -0700

    Add SSH info to Instance Connect tab (#2339)
    * Add box to Connect page with SSH info
    
    * Remove CopyToClipboard; refactor
    
    * Copy tweak
    
    * Update size; remove unnecessary copy
    
    * tweak for more cardiness
    
    * fix ladle type error
    
    * Placeholder copy update; still in flux
    
    * add correct link to new remote access guide
    
    * extract InlineCode, tweak styling, marginally gooder copy
    
    ---------
    
    Co-authored-by: David Crespo <david.crespo@oxidecomputer.com>

---
+++ app/ui/lib/InlineCode.tsx
@@ -0,0 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright Oxide Computer Company
+ */
+
+import { classed } from '~/util/classed'
+
+export const InlineCode = classed.code`whitespace-nowrap rounded-sm px-[3px] py-[1px] text-mono-sm !normal-case bg-raise border border-secondary mx-px`
