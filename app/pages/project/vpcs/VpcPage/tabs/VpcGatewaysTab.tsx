0a62befe1dc8cbdc7f108f0e4f26d6e64a119803
Charlie Park <charlie@oxidecomputer.com>
2024-10-07 00:27:40 -0400

    remove commented-out code

--- app/pages/project/vpcs/VpcPage/tabs/VpcGatewaysTab.tsx
+++ app/pages/project/vpcs/VpcPage/tabs/VpcGatewaysTab.tsx
@@ -10,17 +10,11 @@ import { createColumnHelper } from '@tanstack/react-table'
 import { useMemo } from 'react'
 import { Outlet, type LoaderFunctionArgs } from 'react-router-dom'
 
-import {
-  apiQueryClient,
-  type InternetGateway,
-  // type InternetGatewayIpAddress,
-  // type InternetGatewayIpPool,
-} from '~/api'
+import { apiQueryClient, type InternetGateway } from '~/api'
 import { getVpcSelector, useVpcSelector } from '~/hooks/use-params'
 import { makeLinkCell } from '~/table/cells/LinkCell'
 import { Columns } from '~/table/columns/common'
 import { useQueryTable } from '~/table/QueryTable'
-// import { CreateLink } from '~/ui/lib/CreateButton'
 import { EmptyMessage } from '~/ui/lib/EmptyMessage'
 import { ALL_ISH } from '~/util/consts'
 import { pb } from '~/util/path-builder'
@@ -41,37 +35,9 @@ VpcInternetGatewaysTab.loader = async ({ params }: LoaderFunctionArgs) => {
 export function VpcInternetGatewaysTab() {
   const vpcSelector = useVpcSelector()
   const { project, vpc } = vpcSelector
-  // const { data: internetGatewayIpAddresses } = usePrefetchedApiQuery(
-  //   'internetGatewayIpAddressList',
-  //   { query }
-  // )
-  // const { data: internetGatewayIpPools } = usePrefetchedApiQuery(
-  //   'internetGatewayIpPoolList',
-  //   { query }
-  // )
   const { Table } = useQueryTable('internetGatewayList', {
     query: { project, vpc, limit: ALL_ISH },
   })
-
-  // type PartitionedIpAddresses = Record<string, InternetGatewayIpAddress[]>
-  // const partitionedIpAddresses: PartitionedIpAddresses = useMemo(() => {
-  //   if (!internetGatewayIpAddresses) return {}
-  //   return internetGatewayIpAddresses.items.reduce((acc, ip: InternetGatewayIpAddress) => {
-  //     acc[ip.internetGatewayId] = acc[ip.internetGatewayId] || []
-  //     acc[ip.internetGatewayId].push(ip)
-  //     return acc
-  //   }, {} as PartitionedIpAddresses)
-  // }, [internetGatewayIpAddresses])
-
-  // type PartitionedIpPools = Record<string, InternetGatewayIpPool[]>
-  // const partitionedIpPools: PartitionedIpPools = useMemo(() => {
-  //   if (!internetGatewayIpPools) return {}
-  //   return internetGatewayIpPools.items.reduce((acc, ip: InternetGatewayIpPool) => {
-  //     acc[ip.internetGatewayId] = acc[ip.internetGatewayId] || []
-  //     acc[ip.internetGatewayId].push(ip)
-  //     return acc
-  //   }, {} as PartitionedIpPools)
-  // }, [internetGatewayIpPools])
 
   const emptyState = (
     <EmptyMessage
@@ -95,12 +61,6 @@ export function VpcInternetGatewaysTab() {
 
   return (
     <>
-      <div className="mb-3 flex justify-end space-x-2">
-        {/* Add this back in when moving out of read-only mode */}
-        {/* <CreateLink to={pb.vpcInternetGatewaysNew({ project, vpc })}>
-          New internet gateway
-        </CreateLink> */}
-      </div>
       <Table columns={staticColumns} emptyState={emptyState} />
       <Outlet />
     </>
