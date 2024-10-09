7a609c84f0e898fdc7710219929497992b47f89c
Benjamin Leonard <hello@benleonard.co.uk>
2024-10-09 13:49:19 +0100

    Switch to regular modal

--- app/forms/instance-resize.tsx
+++ app/forms/instance-resize.tsx
@@ -7,20 +7,24 @@
  */
 import { useForm } from 'react-hook-form'
 import { useNavigate, type LoaderFunctionArgs } from 'react-router-dom'
-import * as R from 'remeda'
 
 import {
   apiQueryClient,
+  INSTANCE_MAX_CPU,
+  INSTANCE_MAX_RAM_GiB,
+  instanceCan,
   useApiMutation,
   useApiQueryClient,
   usePrefetchedApiQuery,
 } from '@oxide/api'
 
 import { NumberField } from '~/components/form/fields/NumberField'
-import { SideModalForm } from '~/components/form/SideModalForm'
 import { getInstanceSelector, useInstanceSelector } from '~/hooks/use-params'
 import { addToast } from '~/stores/toast'
+import { Message } from '~/ui/lib/Message'
+import { Modal } from '~/ui/lib/Modal'
 import { pb } from '~/util/path-builder'
+import { GiB } from '~/util/units'
 
 InstanceResizeForm.loader = async ({ params }: LoaderFunctionArgs) => {
   const { project, instance } = getInstanceSelector(params)
@@ -41,35 +45,109 @@ export function InstanceResizeForm() {
     query: { project },
   })
 
+  const onDismiss = () => navigate(pb.instance({ project, instance: instanceName }))
+
   const instanceUpdate = useApiMutation('instanceUpdate', {
     onSuccess(_updatedInstance) {
       queryClient.invalidateQueries('instanceView')
       navigate(pb.instance({ project, instance: instanceName }))
-      addToast({ title: 'Instance updated' })
+      addToast({ title: 'Instance resized' })
     },
+    onError: (err) => {
+      addToast({ title: 'Error', content: err.message, variant: 'error' })
+    },
+    onSettled: onDismiss,
   })
 
-  const form = useForm({ defaultValues: R.pick(instance, ['ncpus', 'memory']) })
+  const form = useForm({
+    defaultValues: {
+      ncpus: instance.ncpus,
+      memory: instance.memory / GiB, // memory is stored as bytes
+    },
+    mode: 'onChange',
+  })
+
+  const canResize = instanceCan.update(instance)
+  const isDisabled = !form.formState.isValid || !canResize
+
+  const onAction = form.handleSubmit(({ ncpus, memory }) => {
+    instanceUpdate.mutate({
+      path: { instance: instanceName },
+      query: { project },
+      body: { ncpus, memory: memory * GiB, bootDisk: instance.bootDiskId },
+    })
+  })
 
   return (
-    <SideModalForm
-      form={form}
-      formType="edit"
-      resourceName="Instance"
+    <Modal
+      title="Resize instance"
+      isOpen
       onDismiss={() => navigate(pb.instance({ project, instance: instanceName }))}
-      onSubmit={({ ncpus, memory }) => {
-        instanceUpdate.mutate({
-          path: { instance: instanceName },
-          query: { project },
-          // very important to include the boot disk or it will be unset
-          body: { ncpus, memory, bootDisk: instance.bootDiskId },
-        })
-      }}
-      loading={instanceUpdate.isPending}
-      submitError={instanceUpdate.error}
     >
-      <NumberField name="ncpus" control={form.control} />
-      <NumberField name="memory" control={form.control} />
-    </SideModalForm>
+      <Modal.Body>
+        <Modal.Section>
+          {!canResize ? (
+            <Message variant="error" content="An instance must be stopped to be resized" />
+          ) : (
+            <Message
+              variant="info"
+              content={
+                <>
+                  <span>Currently using:</span> {instance.ncpus} vCPUs /{' '}
+                  {instance.memory / GiB} GiB
+                </>
+              }
+            />
+          )}
+          <form autoComplete="off" className="space-y-4">
+            <NumberField
+              required
+              label="CPUs"
+              name="ncpus"
+              min={1}
+              control={form.control}
+              validate={(cpus) => {
+                if (cpus < 1) {
+                  return `Must be at least 1 vCPU`
+                }
+                if (cpus > INSTANCE_MAX_CPU) {
+                  return `CPUs capped to ${INSTANCE_MAX_CPU}`
+                }
+                // We can show this error and therefore inform the user
+                // of the limit rather than preventing it completely
+              }}
+              disabled={!canResize}
+            />
+            <NumberField
+              units="GiB"
+              required
+              label="Memory"
+              name="memory"
+              min={1}
+              control={form.control}
+              validate={(memory) => {
+                if (memory < 1) {
+                  return `Must be at least 1 GiB`
+                }
+                if (memory > INSTANCE_MAX_RAM_GiB) {
+                  return `Can be at most ${INSTANCE_MAX_RAM_GiB} GiB`
+                }
+              }}
+              disabled={!canResize}
+            />
+          </form>
+          {instanceUpdate.error && (
+            <p className="mt-4 text-error">{instanceUpdate.error.message}</p>
+          )}
+        </Modal.Section>
+      </Modal.Body>
+      <Modal.Footer
+        onDismiss={onDismiss}
+        onAction={onAction}
+        actionText="Resize"
+        actionLoading={instanceUpdate.isPending}
+        disabled={isDisabled}
+      />
+    </Modal>
   )
 }
