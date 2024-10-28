c95a78e2614fbf6db7a0f6400769df61a1af5abb
Charlie Park <charlie@oxidecomputer.com>
2024-10-28 14:39:57 -0700

    Update zIndex to handle bottomBar and action menu stacking

--- app/components/TopBarPicker.tsx
+++ app/components/TopBarPicker.tsx
@@ -112,7 +112,7 @@ const TopBarPicker = (props: TopBarPickerProps) => {
       {/* TODO: popover position should be further right */}
       {props.items && (
         <DropdownMenu.Content
-          className="!z-topBarPopover mt-2 max-h-80 min-w-[12.8125rem] overflow-y-auto"
+          className="!z-topBarDropdown mt-2 max-h-80 min-w-[12.8125rem] overflow-y-auto"
           anchor="bottom start"
         >
           {props.items.length > 0 ? (
