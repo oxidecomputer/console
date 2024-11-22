091a071dbe9f8689ac6704b61e7937fdd28725ee
Charlie Park <charlie@oxidecomputer.com>
2024-11-22 14:40:12 -0800

    Add overflow logic

--- app/components/ExternalIps.tsx
+++ app/components/ExternalIps.tsx
@@ -6,12 +6,16 @@
  * Copyright Oxide Computer Company
  */
 
+import { Link } from 'react-router-dom'
+
 import { useApiQuery } from '@oxide/api'
 
 import { EmptyCell, SkeletonCell } from '~/table/cells/EmptyCell'
+import { Badge } from '~/ui/lib/Badge'
 import { CopyableIp } from '~/ui/lib/CopyableIp'
 import { Slash } from '~/ui/lib/Slash'
 import { intersperse } from '~/util/array'
+import { pb } from '~/util/path-builder'
 
 type InstanceSelector = { project: string; instance: string }
 
@@ -26,11 +30,18 @@ export function ExternalIps({ project, instance }: InstanceSelector) {
   if (!ips || ips.length === 0) return <EmptyCell />
   // create a copy of ips so we don't mutate the original; move ephemeral ip to the end
   const orderedIps = [...ips].sort((a) => (a.kind === 'ephemeral' ? 1 : -1))
+  const toShow = orderedIps.slice(0, 2)
+  const overflowCount = orderedIps.length - toShow.length
   return (
-    <div className="flex max-w-full items-center gap-1 overflow-x-scroll">
+    <div className="flex max-w-full items-center gap-1">
       {intersperse(
-        orderedIps.map((eip) => <CopyableIp ip={eip.ip} key={eip.ip} />),
+        toShow.map((eip) => <CopyableIp ip={eip.ip} key={eip.ip} />),
         <Slash />
+      )}
+      {overflowCount > 0 && (
+        <Link to={pb.instanceNetworking({ project, instance })}>
+          <Badge>+{overflowCount}</Badge>
+        </Link>
       )}
     </div>
   )
