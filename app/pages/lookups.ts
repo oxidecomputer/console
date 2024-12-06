604ecf52c70071159d6df6b4ae580935073f2ea2
David Crespo <david.crespo@oxidecomputer.com>
2024-12-06 12:50:45 -0600

    convert some more, actually cut some lines!

--- app/pages/lookups.ts
+++ app/pages/lookups.ts
@@ -7,19 +7,19 @@
  */
 import { redirect, type LoaderFunctionArgs } from 'react-router-dom'
 
-import { apiQueryClient } from '@oxide/api'
+import { apiq, queryClient } from '@oxide/api'
 
 import { trigger404 } from '~/components/ErrorBoundary'
 import { pb } from '~/util/path-builder'
 
 export async function instanceLookupLoader({ params }: LoaderFunctionArgs) {
   try {
-    const instance = await apiQueryClient.fetchQuery('instanceView', {
-      path: { instance: params.instance! },
-    })
-    const project = await apiQueryClient.fetchQuery('projectView', {
-      path: { project: instance.projectId },
-    })
+    const instance = await queryClient.fetchQuery(
+      apiq('instanceView', { path: { instance: params.instance! } })
+    )
+    const project = await queryClient.fetchQuery(
+      apiq('projectView', { path: { project: instance.projectId } })
+    )
     return redirect(pb.instance({ project: project.name, instance: instance.name }))
   } catch (_e) {
     throw trigger404
