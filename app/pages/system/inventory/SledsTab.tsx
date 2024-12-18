9758a9371a19880a9498c24c8d04cf138fcf534c
David Crespo <david.crespo@oxidecomputer.com>
2024-12-18 12:20:17 -0600

    use beautiful column grouping

--- app/pages/system/inventory/SledsTab.tsx
+++ app/pages/system/inventory/SledsTab.tsx
@@ -11,21 +11,20 @@ import {
   getListQFn,
   queryClient,
   type Sled,
-  type SledProvisionPolicy,
+  type SledPolicy,
   type SledState,
 } from '@oxide/api'
-import { Servers24Icon } from '@oxide/design-system/icons/react'
+import {
+  Checkmark12Icon,
+  Close12Icon,
+  Servers24Icon,
+} from '@oxide/design-system/icons/react'
 
 import { makeLinkCell } from '~/table/cells/LinkCell'
 import { useQueryTable } from '~/table/QueryTable'
 import { Badge, type BadgeColor } from '~/ui/lib/Badge'
 import { EmptyMessage } from '~/ui/lib/EmptyMessage'
 import { pb } from '~/util/path-builder'
-
-const PROV_POLICY_DISP: Record<SledProvisionPolicy, [string, BadgeColor]> = {
-  provisionable: ['Provisionable', 'default'],
-  non_provisionable: ['Not provisionable', 'neutral'],
-}
 
 const STATE_BADGE_COLORS: Record<SledState, BadgeColor> = {
   active: 'default',
@@ -45,24 +44,45 @@ const staticCols = [
     cell: makeLinkCell((sledId) => pb.sled({ sledId })),
   }),
   // TODO: colHelper.accessor('baseboard.serviceAddress', { header: 'service address' }),
-  colHelper.accessor('baseboard.part', { header: 'part number' }),
-  colHelper.accessor('baseboard.serial', { header: 'serial number' }),
-  colHelper.accessor('baseboard.revision', { header: 'revision' }),
-  colHelper.accessor('policy', {
-    header: 'policy',
-    cell: (info) => {
-      const policy = info.getValue()
-      if (policy.kind === 'expunged') return <Badge color="neutral">Expunged</Badge>
-      const [label, color] = PROV_POLICY_DISP[policy.provisionPolicy]
-      return (
-        <div className="space-x-0.5">
-          <Badge>In service</Badge>
-          <Badge variant="solid" color={color}>
-            {label}
-          </Badge>
-        </div>
-      )
-    },
+  colHelper.group({
+    id: 'baseboard',
+    header: 'Baseboard',
+    columns: [
+      colHelper.accessor('baseboard.part', { header: 'part number' }),
+      colHelper.accessor('baseboard.serial', { header: 'serial number' }),
+      colHelper.accessor('baseboard.revision', { header: 'revision' }),
+    ],
+  }),
+  colHelper.group({
+    id: 'policy',
+    header: 'Policy',
+    columns: [
+      colHelper.accessor('policy', {
+        header: 'Kind',
+        cell: (info) => {
+          // need to cast because inference is broken inside groups
+          // https://github.com/TanStack/table/issues/5065
+          const policy: SledPolicy = info.getValue()
+          return policy.kind === 'expunged' ? (
+            <Badge color="neutral">Expunged</Badge>
+          ) : (
+            <Badge>In service</Badge>
+          )
+        },
+      }),
+      colHelper.accessor('policy', {
+        header: 'Provisionable',
+        cell: (info) => {
+          const policy: SledPolicy = info.getValue()
+          if (policy.kind === 'expunged') return <Close12Icon />
+          return policy.provisionPolicy === 'provisionable' ? (
+            <Checkmark12Icon className="text-accent" />
+          ) : (
+            <Close12Icon />
+          )
+        },
+      }),
+    ],
   }),
   colHelper.accessor('state', {
     cell: (info) => (
