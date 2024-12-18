9483e3e5e68bab3d518393ddb94be1c5831291f6
Charlie Park <charlie@oxidecomputer.com>
2024-12-18 15:45:06 -0800

    Use EmptyCell when we'd have an empty cell

--- test/e2e/disks.e2e.ts
+++ test/e2e/disks.e2e.ts
@@ -29,7 +29,7 @@ test('List disks and snapshot', async ({ page }) => {
     state: 'attached',
   })
   await expectRowVisible(table, {
-    'Attached to': '',
+    'Attached to': '—',
     name: 'disk-3',
     size: '6 GiB',
     state: 'detached',
