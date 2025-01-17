2c3d3bffeb86a246af8917549323ef05212e2374
Charlie Park <charlie@oxidecomputer.com>
2025-01-17 15:45:59 -0800

    Add stub of Disk Metrics using OxQL

--- app/util/date.ts
+++ app/util/date.ts
@@ -53,3 +53,7 @@ export const toLocaleTimeString = (d: Date, locale?: string) =>
 
 export const toLocaleDateTimeString = (d: Date, locale?: string) =>
   new Intl.DateTimeFormat(locale, { dateStyle: 'medium', timeStyle: 'short' }).format(d)
+
+/** get the number of minutes between two date objects */
+export const getDurationMinutes = ({ start, end }: { start: Date; end: Date }) =>
+  (end.getTime() - start.getTime()) / (1000 * 60)
