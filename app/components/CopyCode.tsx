c14b4aface46bf9a102994cde66000f19c62bf1a
David Crespo <david-crespo@users.noreply.github.com>
2025-02-13 15:39:17 -0600

    minor: Abstract equivalent command modal into `CopyCode` (#2693)
    abstract equivalent command modal into CopyCode

--- app/components/CopyCode.tsx
+++ app/components/CopyCode.tsx
@@ -5,7 +5,7 @@
  *
  * Copyright Oxide Computer Company
  */
-import { useState } from 'react'
+import { useState, type ReactNode } from 'react'
 
 import { Success12Icon } from '@oxide/design-system/icons/react'
 
@@ -13,7 +13,22 @@ import { Button } from '~/ui/lib/Button'
 import { Modal } from '~/ui/lib/Modal'
 import { useTimeout } from '~/ui/lib/use-timeout'
 
-export function EquivalentCliCommand({ command }: { command: string }) {
+type CopyCodeProps = {
+  code: string
+  modalButtonText: string
+  copyButtonText: string
+  modalTitle: string
+  /** rendered code */
+  children?: ReactNode
+}
+
+export function CopyCode({
+  code,
+  modalButtonText,
+  copyButtonText,
+  modalTitle,
+  children,
+}: CopyCodeProps) {
   const [isOpen, setIsOpen] = useState(false)
   const [hasCopied, setHasCopied] = useState(false)
 
@@ -24,7 +39,7 @@ export function EquivalentCliCommand({ command }: { command: string }) {
   useTimeout(() => setHasCopied(false), hasCopied ? 2000 : null)
 
   const handleCopy = () => {
-    window.navigator.clipboard.writeText(command).then(() => {
+    window.navigator.clipboard.writeText(code).then(() => {
       setHasCopied(true)
     })
   }
@@ -32,13 +47,12 @@ export function EquivalentCliCommand({ command }: { command: string }) {
   return (
     <>
       <Button variant="ghost" size="sm" className="ml-2" onClick={() => setIsOpen(true)}>
-        Equivalent CLI Command
+        {modalButtonText}
       </Button>
-      <Modal isOpen={isOpen} onDismiss={handleDismiss} title="CLI command">
+      <Modal isOpen={isOpen} onDismiss={handleDismiss} title={modalTitle} width="free">
         <Modal.Section>
           <pre className="flex w-full rounded border px-4 py-3 !normal-case !tracking-normal text-mono-md bg-default border-secondary">
-            <div className="mr-2 select-none text-tertiary">$</div>
-            {command}
+            {children}
           </pre>
         </Modal.Section>
         <Modal.Footer
@@ -46,8 +60,7 @@ export function EquivalentCliCommand({ command }: { command: string }) {
           onAction={handleCopy}
           actionText={
             <>
-              {/* use of invisible keeps button the same size in both states */}
-              <span className={hasCopied ? 'invisible' : ''}>Copy command</span>
+              <span className={hasCopied ? 'invisible' : ''}>{copyButtonText}</span>
               <span
                 className={`absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center ${
                   hasCopied ? '' : 'invisible'
@@ -61,5 +74,27 @@ export function EquivalentCliCommand({ command }: { command: string }) {
         />
       </Modal>
     </>
+  )
+}
+
+type EquivProps = { project: string; instance: string }
+
+export function EquivalentCliCommand({ project, instance }: EquivProps) {
+  const cmdParts = [
+    'oxide instance serial console',
+    `--project ${project}`,
+    `--instance ${instance}`,
+  ]
+
+  return (
+    <CopyCode
+      code={cmdParts.join(' ')}
+      modalButtonText="Equivalent CLI Command"
+      copyButtonText="Copy command"
+      modalTitle="CLI command"
+    >
+      <div className="mr-2 select-none text-tertiary">$</div>
+      {cmdParts.join(' \\\n')}
+    </CopyCode>
   )
 }
