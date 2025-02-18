6a6b28f776009bf2aecbe1a90697e77a1167cdfd
Benjamin Leonard <benji@oxide.computer>
2025-02-18 11:22:41 +0000

    OxQL metrics more actions

--- app/components/CopyCode.tsx
+++ app/components/CopyCode.tsx
@@ -5,6 +5,7 @@
  *
  * Copyright Oxide Computer Company
  */
+import { motion as m } from 'framer-motion'
 import { useState, type ReactNode } from 'react'
 
 import { Success12Icon } from '@oxide/design-system/icons/react'
@@ -15,7 +16,6 @@ import { useTimeout } from '~/ui/lib/use-timeout'
 
 type CopyCodeProps = {
   code: string
-  modalButtonText: string
   copyButtonText: string
   modalTitle: string
   footer?: ReactNode
@@ -23,21 +23,33 @@ type CopyCodeProps = {
   children?: ReactNode
 }
 
-export function CopyCode({
-  code,
-  modalButtonText,
-  copyButtonText,
-  modalTitle,
-  children,
-  footer,
-}: CopyCodeProps) {
+export function CopyCode(props: CopyCodeProps & { modalButtonText: string }) {
   const [isOpen, setIsOpen] = useState(false)
-  const [hasCopied, setHasCopied] = useState(false)
 
   function handleDismiss() {
     setIsOpen(false)
   }
 
+  return (
+    <>
+      <Button variant="ghost" size="sm" className="ml-2" onClick={() => setIsOpen(true)}>
+        {props.modalButtonText}
+      </Button>
+      <CopyCodeModal {...props} isOpen={isOpen} onDismiss={handleDismiss} />
+    </>
+  )
+}
+
+export function CopyCodeModal({
+  isOpen,
+  onDismiss,
+  code,
+  copyButtonText,
+  modalTitle,
+  children,
+  footer,
+}: CopyCodeProps & { isOpen: boolean; onDismiss: () => void }) {
+  const [hasCopied, setHasCopied] = useState(false)
   useTimeout(() => setHasCopied(false), hasCopied ? 2000 : null)
 
   const handleCopy = () => {
@@ -47,37 +59,44 @@ export function CopyCode({
   }
 
   return (
-    <>
-      <Button variant="ghost" size="sm" className="ml-2" onClick={() => setIsOpen(true)}>
-        {modalButtonText}
-      </Button>
-      <Modal isOpen={isOpen} onDismiss={handleDismiss} title={modalTitle} width="free">
-        <Modal.Section>
-          <pre className="w-full rounded border px-4 py-3 !normal-case !tracking-normal text-mono-md bg-default border-secondary">
-            {children}
-          </pre>
-        </Modal.Section>
-        <Modal.Footer
-          onDismiss={handleDismiss}
-          onAction={handleCopy}
-          actionText={
-            <>
-              <span className={hasCopied ? 'invisible' : ''}>{copyButtonText}</span>
-              <span
-                className={`absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center ${
-                  hasCopied ? '' : 'invisible'
-                }`}
+    <Modal isOpen={isOpen} onDismiss={onDismiss} title={modalTitle} width="free">
+      <Modal.Section>
+        <pre className="w-full rounded border px-4 py-3 !normal-case !tracking-normal text-mono-md bg-default border-secondary">
+          {children}
+        </pre>
+      </Modal.Section>
+      <Modal.Footer
+        onDismiss={onDismiss}
+        onAction={handleCopy}
+        actionText={
+          <>
+            <m.span
+              className="flex items-center"
+              animate={{
+                opacity: hasCopied ? 0 : 1,
+                y: hasCopied ? 25 : 0,
+              }}
+              transition={{ type: 'spring', duration: 0.3, bounce: 0 }}
+            >
+              {copyButtonText}
+            </m.span>
+
+            {hasCopied && (
+              <m.span
+                animate={{ opacity: 1, y: '-50%', x: '-50%' }}
+                initial={{ opacity: 0, y: 'calc(-50% - 25px)', x: '-50%' }}
+                transition={{ type: 'spring', duration: 0.3, bounce: 0 }}
+                className="absolute left-1/2 top-1/2 flex items-center"
               >
-                <Success12Icon className="mr-2 text-accent" />
-                Copied
-              </span>
-            </>
-          }
-        >
-          {footer}
-        </Modal.Footer>
-      </Modal>
-    </>
+                <Success12Icon className="text-accent" />
+              </m.span>
+            )}
+          </>
+        }
+      >
+        {footer}
+      </Modal.Footer>
+    </Modal>
   )
 }
 
