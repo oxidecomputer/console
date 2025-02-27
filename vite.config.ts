3dd0629468440514e6a99ecf1e8335bb4d1423ec
David Crespo <david.crespo@oxidecomputer.com>
2025-02-27 11:16:17 -0600

    add npm run start:colo

--- vite.config.ts
+++ vite.config.ts
@@ -18,13 +18,17 @@ import vercelConfig from './vercel.json'
 
 const KiB = 1024
 
-const ApiMode = z.enum(['msw', 'dogfood', 'nexus'])
+const ApiMode = z.enum(['msw', 'remote', 'nexus'])
+
+function bail(msg: string): never {
+  console.error(msg)
+  process.exit(1)
+}
 
 const apiModeResult = ApiMode.default('nexus').safeParse(process.env.API_MODE)
 if (!apiModeResult.success) {
   const options = ApiMode.options.join(', ')
-  console.error(`Error: API_MODE must be one of: [${options}]. If unset, default is "msw".`)
-  process.exit(1)
+  bail(`Error: API_MODE must be one of: [${options}]. If unset, default is "msw".`)
 }
 /**
  * What API are we talking to? Only relevant in development mode.
@@ -35,8 +39,11 @@ if (!apiModeResult.success) {
  */
 const apiMode = apiModeResult.data
 
-// if you want a different host you can override it with EXT_HOST
-const DOGFOOD_HOST = process.env.EXT_HOST || 'oxide.sys.rack2.eng.oxide.computer'
+if (apiMode === 'remote' && !process.env.EXT_HOST) {
+  bail(`Error: EXT_HOST is required when API_MODE=remote. See package.json for examples.`)
+}
+
+const EXT_HOST = process.env.EXT_HOST
 
 const previewAnalyticsTag = {
   injectTo: 'head' as const,
@@ -126,7 +133,7 @@ export default defineConfig(({ mode }) => ({
       },
     }),
     react(),
-    apiMode === 'dogfood' && basicSsl(),
+    apiMode === 'remote' && basicSsl(),
   ],
   html: {
     // don't include a placeholder nonce in production.
@@ -139,8 +146,7 @@ export default defineConfig(({ mode }) => ({
     // these only get hit when MSW doesn't intercept the request
     proxy: {
       '/v1': {
-        target:
-          apiMode === 'dogfood' ? `https://${DOGFOOD_HOST}` : 'http://localhost:12220',
+        target: apiMode === 'remote' ? `https://${EXT_HOST}` : 'http://localhost:12220',
         changeOrigin: true,
       },
     },
