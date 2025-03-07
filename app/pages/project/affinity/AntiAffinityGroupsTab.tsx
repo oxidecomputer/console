00172846b7df8fd59f86b4883328568a592bad1a
Charlie Park <charlie@oxidecomputer.com>
2025-03-07 09:22:06 -0800

    stub of AntiAffinityGroupTab

--- app/pages/project/affinity/AntiAffinityGroupsTab.tsx
+++ app/pages/project/affinity/AntiAffinityGroupsTab.tsx
@@ -6,26 +6,61 @@
  * Copyright Oxide Computer Company
  */
 
+import { createColumnHelper } from '@tanstack/react-table'
 import type { LoaderFunctionArgs } from 'react-router'
 
-import { apiQueryClient } from '@oxide/api'
+import { getListQFn, queryClient, type AntiAffinityGroup } from '@oxide/api'
+import { Access24Icon } from '@oxide/design-system/icons/react'
 
-import { getProjectSelector } from '~/hooks/use-params'
-
-export async function clientLoader({ params }: LoaderFunctionArgs) {
-  const { project } = getProjectSelector(params)
-  await Promise.all([
-    apiQueryClient.prefetchQuery('policyView', {}),
-    apiQueryClient.prefetchQuery('projectPolicyView', { path: { project } }),
-    // used to resolve user names
-    apiQueryClient.prefetchQuery('userList', {}),
-    apiQueryClient.prefetchQuery('groupList', {}),
-  ])
-  return null
-}
+import { getProjectSelector, useProjectSelector } from '~/hooks/use-params'
+import { makeLinkCell } from '~/table/cells/LinkCell'
+import { Columns } from '~/table/columns/common'
+import { useQueryTable } from '~/table/QueryTable'
+import { EmptyMessage } from '~/ui/lib/EmptyMessage'
+import { pb } from '~/util/path-builder'
+import type * as PP from '~/util/path-params'
 
 export const handle = { crumb: 'Anti-Affinity' }
 
-export default function ProjectAccessPage() {
-  return <>Table goes here</>
+const EmptyState = () => (
+  <EmptyMessage
+    icon={<Access24Icon />}
+    title="No Anti-Affinity Groups"
+    body="Create an anti-affinity group to see it here"
+    buttonText="New anti-affinity group"
+    buttonTo={pb.antiAffinityGroupNew(useProjectSelector())}
+  />
+)
+
+const antiAffinityGroupList = (query: PP.Project) =>
+  getListQFn('antiAffinityGroupList', { query })
+
+export async function clientLoader({ params }: LoaderFunctionArgs) {
+  const { project } = getProjectSelector(params)
+  await queryClient.prefetchQuery(antiAffinityGroupList({ project }).optionsFn())
+  return null
+}
+
+const colHelper = createColumnHelper<AntiAffinityGroup>()
+
+export default function AffinityGroupsTab() {
+  const { project } = useProjectSelector()
+
+  const staticCols = [
+    colHelper.accessor('name', {
+      cell: makeLinkCell((antiAffinityGroup) =>
+        pb.antiAffinityGroup({ project, antiAffinityGroup })
+      ),
+    }),
+    colHelper.accessor('description', Columns.description),
+    colHelper.accessor('timeCreated', Columns.timeCreated),
+  ]
+
+  const { table } = useQueryTable({
+    query: antiAffinityGroupList({ project }),
+    columns: staticCols,
+    emptyState: <EmptyState />,
+  })
+
+  return table
 }
