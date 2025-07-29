ef73c18e89a7b1f2bbe488a60fdc30b387e917fd
David Crespo <david-crespo@users.noreply.github.com>
2025-07-29 16:15:36 -0500

    Upgrade to client generator 0.8.1 to test changes (#2863)
    upgrade to client generator 0.8.1 to test changes

--- app/api/__generated__/Api.ts
+++ app/api/__generated__/Api.ts
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 /**
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,6 +5,8 @@
  *
  * Copyright Oxide Computer Company
  */
+
+/* eslint-disable */
 
 import { HttpClient, toQueryString, type FetchParams } from './http-client'
 
