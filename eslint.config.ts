367ed26f68b8d3dacd502b09a9b90c1b14206a16
David Crespo <david.crespo@oxidecomputer.com>
2025-10-23 21:29:04 -0500

    license

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright Oxide Computer Company
+ */
 import { fixupConfigRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
