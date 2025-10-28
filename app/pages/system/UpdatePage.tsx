3f5e7e602ef3df0dd72dcd995a11cce29e4e565b
David Crespo <david-crespo@users.noreply.github.com>
2025-10-28 14:14:21 -0500

    Poll update status + disable set target on older releases (#2947)
    * poll system update status
    
    * disable set target for lower versions

--- app/pages/system/UpdatePage.tsx
+++ app/pages/system/UpdatePage.tsx
@@ -6,8 +6,10 @@
  * Copyright Oxide Computer Company
  */
 
+import { differenceInMinutes } from 'date-fns'
 import { useMemo } from 'react'
 import * as R from 'remeda'
+import { lt as semverLt } from 'semver'
 
 import {
   Images24Icon,
@@ -46,7 +48,29 @@ import { percentage, round } from '~/util/math'
 
 export const handle = makeCrumb('System Update')
 
-const statusQuery = apiq('systemUpdateStatus', {})
+const SEC = 1000 // ms, obviously
+const POLL_FAST = 20 * SEC
+const POLL_SLOW = 120 * SEC
+
+const statusQuery = apiq(
+  'systemUpdateStatus',
+  {},
+  {
+    refetchInterval({ state: { data: status } }) {
+      if (!status) return false // should be impossible due to prefetch
+
+      const now = new Date()
+      const minSinceTargetSet = status.targetRelease
+        ? differenceInMinutes(now, status.targetRelease.timeRequested)
+        : null
+      const minSinceLastStepPlanned = differenceInMinutes(now, status.timeLastStepPlanned)
+      return minSinceLastStepPlanned < 30 ||
+        (minSinceTargetSet !== null && minSinceTargetSet < 30)
+        ? POLL_FAST
+        : POLL_SLOW
+    },
+  }
+)
 const reposQuery = apiq('systemUpdateRepositoryList', { query: { limit: ALL_ISH } })
 
 const refreshData = () =>
@@ -175,7 +199,17 @@ export default function UpdatePage() {
         <CardBlock.Body>
           <ul className="space-y-3">
             {repos.items.map((repo) => {
-              const isTarget = repo.systemVersion === status.targetRelease?.version
+              const targetVersion = status.targetRelease?.version
+              const isTarget = repo.systemVersion === targetVersion
+              // semverLt looks at prerelease meta but not build meta. In prod
+              // it doesn't matter either way because there will be neither. On
+              // dogfood it shouldn't matter because the versions will usually
+              // be the same and with the same prelease meta and only differing
+              // build meta, so semverLt will return false and we don't disable
+              // any. Very important this is
+              const olderThanTarget = targetVersion
+                ? semverLt(repo.systemVersion, targetVersion)
+                : false
               return (
                 <li
                   key={repo.hash}
@@ -226,10 +260,13 @@ export default function UpdatePage() {
                             errorTitle: `Error setting target release to ${repo.systemVersion}`,
                           })
                         }}
-                        // TODO: follow API logic, disabling for older releases.
-                        // Or maybe just have the API tell us by adding a field to
-                        // the TufRepo response type.
-                        disabled={isTarget && 'Already set as target'}
+                        disabled={
+                          isTarget
+                            ? 'Already set as target'
+                            : olderThanTarget
+                              ? 'Cannot set older release as target'
+                              : false
+                        }
                       />
                     </MoreActionsMenu>
                   </div>
