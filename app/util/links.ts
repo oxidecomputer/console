966c31ab1256363ff4693509c3ba16fb23f997bc
David Crespo <david.crespo@oxidecomputer.com>
2026-02-06 11:48:35 -0600

    reduce redundant link definitions

--- app/util/links.ts
+++ app/util/links.ts
@@ -10,62 +10,31 @@ const remoteAccess = 'https://docs.oxide.computer/guides/remote-access'
 
 export const links = {
   accessDocs: 'https://docs.oxide.computer/guides/configuring-access',
-  affinityDocs:
-    'https://docs.oxide.computer/guides/deploying-workloads#_affinity_and_anti_affinity',
   cloudInitFormat: 'https://cloudinit.readthedocs.io/en/latest/explanation/format.html',
   cloudInitExamples: 'https://cloudinit.readthedocs.io/en/latest/reference/examples.html',
-  deviceTokenSetup:
-    'https://docs.oxide.computer/guides/working-with-api-and-sdk#_device_token_setup',
-  disksDocs: 'https://docs.oxide.computer/guides/managing-disks-and-snapshots',
   firewallRulesDocs:
     'https://docs.oxide.computer/guides/configuring-guest-networking#_firewall_rules',
-  floatingIpsDocs: 'https://docs.oxide.computer/guides/managing-floating-ips',
-  gatewaysDocs:
-    'https://docs.oxide.computer/guides/configuring-guest-networking#internet-gateway',
   imagesDocs: 'https://docs.oxide.computer/guides/creating-and-sharing-images',
   preparingImagesDocs:
     'https://docs.oxide.computer/guides/creating-and-sharing-images#_preparing_images_for_import',
   identityProvidersDocs: 'https://docs.oxide.computer/guides/operator/identity-providers',
-  instanceActionsDocs: 'https://docs.oxide.computer/guides/managing-instances',
   // TODO: link to section
   instanceBootDiskDocs: 'https://docs.oxide.computer/guides/deploying-workloads',
   instanceUpdateDocs:
     'https://docs.oxide.computer/guides/managing-instances#_update_instances',
-  keyConceptsIamPolicyDocs:
-    'https://docs.oxide.computer/guides/key-entities-and-concepts#iam-policy',
-  keyConceptsProjectsDocs:
-    'https://docs.oxide.computer/guides/key-entities-and-concepts#_projects',
   oxqlDocs: 'https://docs.oxide.computer/guides/operator/system-metrics#_oxql_quickstart',
   oxqlSchemaDocs: (metric: string) =>
     `https://docs.oxide.computer/guides/metrics/timeseries-schemas#_${metric.replace(':', '')}`,
-  projectsDocs: 'https://docs.oxide.computer/guides/onboarding-projects',
-  quickStart: 'https://docs.oxide.computer/guides/quickstart',
-  routesDocs: 'https://docs.oxide.computer/guides/configuring-guest-networking#vpc-subnet',
-  customRoutersDocs:
-    'https://docs.oxide.computer/guides/configuring-guest-networking#_custom_routers',
   scimDocs:
     'https://docs.oxide.computer/guides/operator/identity-providers#_saml_authentication_scim_user_provisioning',
   siloQuotasDocs:
     'https://docs.oxide.computer/guides/operator/silo-management#_silo_resource_quota_management',
-  sledDocs:
-    'https://docs.oxide.computer/guides/architecture/service-processors#_server_sled',
-  snapshotsDocs:
-    'https://docs.oxide.computer/guides/managing-disks-and-snapshots#_snapshots',
   serialConsoleDocs: remoteAccess + '#serial-console',
   sshDocs: remoteAccess + '#ssh',
-  sshKeysDocs: 'https://docs.oxide.computer/guides/user-settings#_ssh_keys',
-  storageDocs:
-    'https://docs.oxide.computer/guides/architecture/os-hypervisor-storage#_storage',
-  systemIpPoolsDocs: 'https://docs.oxide.computer/guides/operator/ip-pool-management',
-  systemMetricsDocs: 'https://docs.oxide.computer/guides/operator/system-metrics',
-  systemSiloDocs: 'https://docs.oxide.computer/guides/operator/silo-management',
-  systemUpdateDocs: 'https://docs.oxide.computer/guides/operator/system-update',
   transitIpsDocs:
     'https://docs.oxide.computer/guides/configuring-guest-networking#_example_4_software_routing_tunnels',
   troubleshootingAccess:
     'https://docs.oxide.computer/guides/operator/faq#_how_do_i_fix_the_something_went_wrong_error',
-  instancesDocs: 'https://docs.oxide.computer/guides/deploying-workloads',
-  vpcsDocs: 'https://docs.oxide.computer/guides/configuring-guest-networking',
 }
 
 // These are links (and associated titles) to help documentation
@@ -75,15 +44,15 @@ export const docLinks = {
     linkText: 'Access Control',
   },
   affinity: {
-    href: links.affinityDocs,
+    href: 'https://docs.oxide.computer/guides/deploying-workloads#_affinity_and_anti_affinity',
     linkText: 'Anti-Affinity Groups',
   },
   deviceTokens: {
-    href: links.deviceTokenSetup,
+    href: 'https://docs.oxide.computer/guides/working-with-api-and-sdk#_device_token_setup',
     linkText: 'Access Tokens',
   },
   disks: {
-    href: links.disksDocs,
+    href: 'https://docs.oxide.computer/guides/managing-disks-and-snapshots',
     linkText: 'Disks and Snapshots',
   },
   firewallRules: {
@@ -91,15 +60,15 @@ export const docLinks = {
     linkText: 'Firewall Rules',
   },
   floatingIps: {
-    href: links.floatingIpsDocs,
+    href: 'https://docs.oxide.computer/guides/managing-floating-ips',
     linkText: 'Floating IPs',
   },
   gateways: {
-    href: links.gatewaysDocs,
+    href: 'https://docs.oxide.computer/guides/configuring-guest-networking#internet-gateway',
     linkText: 'Internet Gateways',
   },
   keyConceptsIam: {
-    href: links.keyConceptsIamPolicyDocs,
+    href: 'https://docs.oxide.computer/guides/key-entities-and-concepts#iam-policy',
     linkText: 'Key Concepts',
   },
   identityProviders: {
@@ -111,19 +80,19 @@ export const docLinks = {
     linkText: 'Images',
   },
   instanceActions: {
-    href: links.instanceActionsDocs,
+    href: 'https://docs.oxide.computer/guides/managing-instances',
     linkText: 'Instance Actions',
   },
   keyConceptsProjects: {
-    href: links.keyConceptsProjectsDocs,
+    href: 'https://docs.oxide.computer/guides/key-entities-and-concepts#_projects',
     linkText: 'Key Concepts',
   },
   projects: {
-    href: links.projectsDocs,
+    href: 'https://docs.oxide.computer/guides/onboarding-projects',
     linkText: 'Projects',
   },
   quickStart: {
-    href: links.quickStart,
+    href: 'https://docs.oxide.computer/guides/quickstart',
     linkText: 'Quick Start',
   },
   remoteAccess: {
@@ -131,51 +100,51 @@ export const docLinks = {
     linkText: 'Remote Access',
   },
   routers: {
-    href: links.customRoutersDocs,
+    href: 'https://docs.oxide.computer/guides/configuring-guest-networking#_custom_routers',
     linkText: 'Custom Routers',
   },
   routes: {
-    href: links.routesDocs,
+    href: 'https://docs.oxide.computer/guides/configuring-guest-networking#vpc-subnet',
     linkText: 'VPC Subnet Routing',
   },
   sleds: {
-    href: links.sledDocs,
+    href: 'https://docs.oxide.computer/guides/architecture/service-processors#_server_sled',
     linkText: 'Server Sleds',
   },
   snapshots: {
-    href: links.snapshotsDocs,
+    href: 'https://docs.oxide.computer/guides/managing-disks-and-snapshots#_snapshots',
     linkText: 'Disks and Snapshots',
   },
   sshKeys: {
-    href: links.sshKeysDocs,
+    href: 'https://docs.oxide.computer/guides/user-settings#_ssh_keys',
     linkText: 'SSH Keys',
   },
   storage: {
-    href: links.storageDocs,
+    href: 'https://docs.oxide.computer/guides/architecture/os-hypervisor-storage#_storage',
     linkText: 'Storage',
   },
   systemIpPools: {
-    href: links.systemIpPoolsDocs,
+    href: 'https://docs.oxide.computer/guides/operator/ip-pool-management',
     linkText: 'IP Pools',
   },
   systemMetrics: {
-    href: links.systemMetricsDocs,
+    href: 'https://docs.oxide.computer/guides/operator/system-metrics',
     linkText: 'Metrics',
   },
   systemSilo: {
-    href: links.systemSiloDocs,
+    href: 'https://docs.oxide.computer/guides/operator/silo-management',
     linkText: 'Silos',
   },
   systemUpdate: {
-    href: links.systemUpdateDocs,
+    href: 'https://docs.oxide.computer/guides/operator/system-update',
     linkText: 'System Update',
   },
   instances: {
-    href: links.instancesDocs,
+    href: 'https://docs.oxide.computer/guides/deploying-workloads',
     linkText: 'Instances',
   },
   vpcs: {
-    href: links.vpcsDocs,
+    href: 'https://docs.oxide.computer/guides/configuring-guest-networking',
     linkText: 'Networking',
   },
 }
