oxidecomputer/console

commit b9013a33eaa3f5efdcd5c7d244e36a54e7222295

Add `user_data` field to instance create (#1898)
* Block out `user_data` field

* Update `instance-create` e2e test

* Add help text and description

* Switch description to tooltipText and allow nodes for helpText

* Switch helpText to description

* Add user data description with examples

* Remove async from `FullPageForm`

* simplify links object, don't bother falling back to GET if HEAD fails

* remove fragment from url

---------

Co-authored-by: David Crespo <david.crespo@oxidecomputer.com>

app/forms/instance-create.tsx
@@ -5,6 +5,7 @@
  *
  * Copyright Oxide Computer Company
  */
+import * as Accordion from '@radix-ui/react-accordion'
 import { useEffect, useState } from 'react'
 import { useWatch } from 'react-hook-form'
 import { useNavigate, type LoaderFunctionArgs } from 'react-router-dom'
@@ -21,6 +22,7 @@ import {
   type InstanceCreate,
 } from '@oxide/api'
 import {
+  DirectionRightIcon,
   EmptyMessage,
   FieldLabel,
   FormDivider,
@@ -41,6 +43,7 @@ import {
   DescriptionField,
   DiskSizeField,
   DisksTableField,
+  FileField,
   Form,
   FullPageForm,
   ImageSelectField,
@@ -51,6 +54,8 @@ import {
   type DiskTableItem,
 } from 'app/components/form'
 import { getProjectSelector, useForm, useProjectSelector, useToast } from 'app/hooks'
+import { readBlobAsBase64 } from 'app/util/file'
+import { links } from 'app/util/links'
 import { pb } from 'app/util/path-builder'
 
 export type InstanceCreateInput = Assign<
@@ -62,6 +67,7 @@ export type InstanceCreateInput = Assign<
     bootDiskName: string
     bootDiskSize: number
     image: string
+    userData: File | null
   }
 >
 
@@ -85,6 +91,8 @@ const baseDefaultValues: InstanceCreateInput = {
   networkInterfaces: { type: 'default' },
 
   start: true,
+
+  userData: null,
 }
 
 CreateInstanceForm.loader = async ({ params }: LoaderFunctionArgs) => {
@@ -153,7 +161,7 @@ export function CreateInstanceForm() {
       form={form}
       title="Create instance"
       icon={<Instances24Icon />}
-      onSubmit={(values) => {
+      onSubmit={async (values) => {
         setIsSubmitting(true)
         // we should never have a presetId that's not in the list
         const preset = PRESETS.find((option) => option.id === values.presetId)!
@@ -168,6 +176,10 @@ export function CreateInstanceForm() {
         invariant(image, 'Expected image to be defined')
 
         const bootDiskName = values.bootDiskName || genName(values.name, image.name)
+
+        const userData = values.userData
+          ? await readBlobAsBase64(values.userData)
+          : undefined
 
         createInstance.mutate({
           query: projectSelector,
@@ -197,6 +209,7 @@ export function CreateInstanceForm() {
             externalIps: [{ type: 'ephemeral' }],
             start: values.start,
             networkInterfaces: values.networkInterfaces,
+            userData,
           },
         })
       }}
@@ -390,7 +403,7 @@ export function CreateInstanceForm() {
       <NameField
         name="bootDiskName"
         label="Disk name"
-        description="Will be autogenerated if name not provided"
+        tooltipText="Will be autogenerated if name not provided"
         required={false}
         control={control}
         disabled={isSubmitting}
@@ -406,16 +419,46 @@ export function CreateInstanceForm() {
       <SshKeysTable />
 
       <FormDivider />
-      <Form.Heading id="networking">Networking</Form.Heading>
+      <Form.Heading id="advanced">Advanced</Form.Heading>
 
-      <NetworkInterfaceField control={control} disabled={isSubmitting} />
+      <Accordion.Root type="multiple" className="mt-12">
+        <Accordion.Item value="networking">
+          <AccordionHeader id="networking">Networking</AccordionHeader>
+          <AccordionContent>
+            <NetworkInterfaceField control={control} disabled={isSubmitting} />
 
-      <TextField
-        name="hostname"
-        description="Will be generated if not provided"
-        control={control}
-        disabled={isSubmitting}
-      />
+            <TextField
+              name="hostname"
+              tooltipText="Will be generated if not provided"
+              control={control}
+              disabled={isSubmitting}
+            />
+          </AccordionContent>
+        </Accordion.Item>
+        <Accordion.Item value="configuration">
+          <AccordionHeader id="configuration">Configuration</AccordionHeader>
+          <AccordionContent>
+            <FileField
+              id="user-data-input"
+              description={
+                <>
+                  Data or scripts to be passed to cloud-init as{' '}
+                  <a href={links.cloudInitFormat} target="_blank" rel="noreferrer">
+                    user data
+                  </a>{' '}
+                  <a href={links.cloudInitExamples} target="_blank" rel="noreferrer">
+                    (examples)
+                  </a>{' '}
+                  if the selected boot image supports it. Maximum size 32 KiB.
+                </>
+              }
+              name="userData"
+              label="User Data"
+              control={control}
+            />
+          </AccordionContent>
+        </Accordion.Item>
+      </Accordion.Root>
 
       <Form.Actions>
         <Form.Submit loading={createInstance.isPending}>Create instance</Form.Submit>
@@ -424,6 +467,21 @@ export function CreateInstanceForm() {
     </FullPageForm>
   )
 }
+
+const AccordionHeader = ({ id, children }: { id: string; children: React.ReactNode }) => (
+  <Accordion.Header id={id} className="max-w-lg">
+    <Accordion.Trigger className="group flex w-full items-center justify-between border-t py-2 text-sans-xl border-secondary [&>svg]:data-[state=open]:rotate-90">
+      <div className="text-secondary">{children}</div>
+      <DirectionRightIcon className="transition-all text-secondary" />
+    </Accordion.Trigger>
+  </Accordion.Header>
+)
+
+const AccordionContent = ({ children }: { children: React.ReactNode }) => (
+  <Accordion.Content className="AccordionContent max-w-lg overflow-hidden">
+    <div className="ox-accordion-content py-8">{children}</div>
+  </Accordion.Content>
+)
 
 const SshKeysTable = () => {
   const keys = usePrefetchedApiQuery('currentUserSshKeyList', {}).data?.items || []

libs/ui/lib/text-input/TextInput.tsx
@@ -101,7 +101,13 @@ type HintProps = {
  * Pass id here and include that ID in aria-describedby on the TextField
  */
 export const TextInputHint = ({ id, children, className }: HintProps) => (
-  <div id={id} className={cn('mt-1 text-sans-sm text-tertiary', className)}>
+  <div
+    id={id}
+    className={cn(
+      'mt-1 text-sans-sm text-tertiary [&_>_a]:underline hover:[&_>_a]:text-default',
+      className
+    )}
+  >
     {children}
   </div>
 )

app/components/TimeAgo.tsx
@@ -14,16 +14,16 @@ import { timeAgoAbbr } from 'app/util/date'
 
 export const TimeAgo = ({
   datetime,
-  description,
+  tooltipText,
   placement = 'top',
 }: {
   datetime: Date
-  description?: string
+  tooltipText?: string
   placement?: Placement
 }): JSX.Element => {
   const content = (
     <div className="flex flex-col">
-      <span className="text-tertiary">{description}</span>
+      <span className="text-tertiary">{tooltipText}</span>
       <span>{format(datetime, 'MMM d, yyyy p')}</span>
     </div>
   )

app/components/form/fields/FileField.tsx
@@ -18,20 +18,20 @@ export function FileField<
   id,
   name,
   label,
-  description,
+  tooltipText,
   control,
   required = false,
   accept,
-  helpText,
+  description,
 }: {
   id: string
   name: TName
   label: string
-  description?: string
+  tooltipText?: string
   control: Control<TFieldValues>
   required?: boolean
   accept?: string
-  helpText?: string
+  description?: string | React.ReactNode
 }) {
   return (
     <Controller
@@ -44,12 +44,14 @@ export function FileField<
             <FieldLabel
               id={`${id}-label`}
               htmlFor={id}
-              tip={description}
+              tip={tooltipText}
               optional={!required}
             >
               {label}
             </FieldLabel>
-            {helpText && <TextInputHint id={`${id}-help-text`}>{helpText}</TextInputHint>}
+            {description && (
+              <TextInputHint id={`${id}-help-text`}>{description}</TextInputHint>
+            )}
           </div>
           <FileInput id={id} className="mt-2" accept={accept} {...rest} error={!!error} />
           <ErrorMessage error={error} label={label} />

app/components/form/fields/ListboxField.tsx
@@ -22,8 +22,8 @@ export type ListboxFieldProps<
   className?: string
   label?: string
   required?: boolean
-  helpText?: string
-  description?: string
+  description?: string | React.ReactNode | React.ReactNode
+  tooltipText?: string
   control: Control<TFieldValues>
   disabled?: boolean
   items: ListboxItem[]
@@ -41,8 +41,8 @@ export function ListboxField<
   label = capitalize(name),
   disabled,
   required,
+  tooltipText,
   description,
-  helpText,
   className,
   control,
   onChange,
@@ -59,9 +59,9 @@ export function ListboxField<
         render={({ field, fieldState: { error } }) => (
           <>
             <Listbox
-              helpText={helpText}
-              label={label}
               description={description}
+              label={label}
+              tooltipText={tooltipText}
               required={required}
               placeholder={placeholder}
               selected={field.value || null}

app/components/form/fields/NetworkInterfaceField.tsx
@@ -43,88 +43,90 @@ export function NetworkInterfaceField({
   return (
     <div className="max-w-lg space-y-2">
       <FieldLabel id="network-interface-type-label">Network interface</FieldLabel>
-      <RadioGroup
-        aria-labelledby="network-interface-type-label"
-        name="networkInterfaceType"
-        column
-        className="pt-1"
-        defaultChecked={value.type}
-        onChange={(event) => {
-          const newType = event.target.value as InstanceNetworkInterfaceAttachment['type']
+      <div className="space-y-4">
+        <RadioGroup
+          aria-labelledby="network-interface-type-label"
+          name="networkInterfaceType"
+          column
+          className="pt-1"
+          defaultChecked={value.type}
+          onChange={(event) => {
+            const newType = event.target.value as InstanceNetworkInterfaceAttachment['type']
 
-          if (value.type === 'create') {
-            setOldParams(value.params)
-          }
+            if (value.type === 'create') {
+              setOldParams(value.params)
+            }
 
-          newType === 'create'
-            ? onChange({ type: newType, params: oldParams })
-            : onChange({ type: newType })
-        }}
-        disabled={disabled}
-      >
-        <Radio value="none">None</Radio>
-        <Radio value="default">Default</Radio>
-        <Radio value="create">Custom</Radio>
-      </RadioGroup>
-      {value.type === 'create' && (
-        <>
-          {value.params.length > 0 && (
-            <MiniTable.Table className="mb-4">
-              <MiniTable.Header>
-                <MiniTable.HeadCell>Name</MiniTable.HeadCell>
-                <MiniTable.HeadCell>VPC</MiniTable.HeadCell>
-                <MiniTable.HeadCell>Subnet</MiniTable.HeadCell>
-                {/* For remove button */}
-                <MiniTable.HeadCell className="w-12" />
-              </MiniTable.Header>
-              <MiniTable.Body>
-                {value.params.map((item, index) => (
-                  <MiniTable.Row
-                    tabIndex={0}
-                    aria-rowindex={index + 1}
-                    aria-label={`Name: ${item.name}, Vpc: ${item.vpcName}, Subnet: ${item.subnetName}`}
-                    key={item.name}
-                  >
-                    <MiniTable.Cell>{item.name}</MiniTable.Cell>
-                    <MiniTable.Cell>{item.vpcName}</MiniTable.Cell>
-                    <MiniTable.Cell>{item.subnetName}</MiniTable.Cell>
-                    <MiniTable.Cell>
-                      <button
-                        onClick={() =>
-                          onChange({
-                            type: 'create',
-                            params: value.params.filter((i) => i.name !== item.name),
-                          })
-                        }
-                      >
-                        <Error16Icon title={`remove ${item.name}`} />
-                      </button>
-                    </MiniTable.Cell>
-                  </MiniTable.Row>
-                ))}
-              </MiniTable.Body>
-            </MiniTable.Table>
-          )}
+            newType === 'create'
+              ? onChange({ type: newType, params: oldParams })
+              : onChange({ type: newType })
+          }}
+          disabled={disabled}
+        >
+          <Radio value="none">None</Radio>
+          <Radio value="default">Default</Radio>
+          <Radio value="create">Custom</Radio>
+        </RadioGroup>
+        {value.type === 'create' && (
+          <>
+            {value.params.length > 0 && (
+              <MiniTable.Table className="pt-2">
+                <MiniTable.Header>
+                  <MiniTable.HeadCell>Name</MiniTable.HeadCell>
+                  <MiniTable.HeadCell>VPC</MiniTable.HeadCell>
+                  <MiniTable.HeadCell>Subnet</MiniTable.HeadCell>
+                  {/* For remove button */}
+                  <MiniTable.HeadCell className="w-12" />
+                </MiniTable.Header>
+                <MiniTable.Body>
+                  {value.params.map((item, index) => (
+                    <MiniTable.Row
+                      tabIndex={0}
+                      aria-rowindex={index + 1}
+                      aria-label={`Name: ${item.name}, Vpc: ${item.vpcName}, Subnet: ${item.subnetName}`}
+                      key={item.name}
+                    >
+                      <MiniTable.Cell>{item.name}</MiniTable.Cell>
+                      <MiniTable.Cell>{item.vpcName}</MiniTable.Cell>
+                      <MiniTable.Cell>{item.subnetName}</MiniTable.Cell>
+                      <MiniTable.Cell>
+                        <button
+                          onClick={() =>
+                            onChange({
+                              type: 'create',
+                              params: value.params.filter((i) => i.name !== item.name),
+                            })
+                          }
+                        >
+                          <Error16Icon title={`remove ${item.name}`} />
+                        </button>
+                      </MiniTable.Cell>
+                    </MiniTable.Row>
+                  ))}
+                </MiniTable.Body>
+              </MiniTable.Table>
+            )}
 
-          {showForm && (
-            <CreateNetworkInterfaceForm
-              onSubmit={(networkInterface) => {
-                onChange({
-                  type: 'create',
-                  params: [...value.params, networkInterface],
-                })
-                setShowForm(false)
-              }}
-              onDismiss={() => setShowForm(false)}
-            />
-          )}
-          <div className="space-x-3">
-            <Button size="sm" onClick={() => setShowForm(true)}>
-              Add network interface
-            </Button>
-          </div>
-        </>
-      )}
+            {showForm && (
+              <CreateNetworkInterfaceForm
+                onSubmit={(networkInterface) => {
+                  onChange({
+                    type: 'create',
+                    params: [...value.params, networkInterface],
+                  })
+                  setShowForm(false)
+                }}
+                onDismiss={() => setShowForm(false)}
+              />
+            )}
+            <div className="space-x-3">
+              <Button size="sm" onClick={() => setShowForm(true)}>
+                Add network interface
+              </Button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   )
 }

app/components/form/fields/NumberField.tsx
@@ -23,8 +23,8 @@ export function NumberField<
   name,
   label = capitalize(name),
   units,
+  tooltipText,
   description,
-  helpText,
   required,
   ...props
 }: Omit<TextFieldProps<TFieldValues, TName>, 'id'>) {
@@ -33,12 +33,12 @@ export function NumberField<
   return (
     <div className="max-w-lg">
       <div className="mb-2">
-        <FieldLabel htmlFor={id} id={`${id}-label`} tip={description} optional={!required}>
+        <FieldLabel htmlFor={id} id={`${id}-label`} tip={tooltipText} optional={!required}>
           {label} {units && <span className="ml-1 text-secondary">({units})</span>}
         </FieldLabel>
-        {helpText && (
+        {description && (
           <TextInputHint id={`${id}-help-text`} className="mb-2">
-            {helpText}
+            {description}
           </TextInputHint>
         )}
       </div>
@@ -65,7 +65,7 @@ export const NumberFieldInner = <
   label = capitalize(name),
   validate,
   control,
-  description,
+  tooltipText,
   required,
   id: idProp,
   disabled,
@@ -85,9 +85,9 @@ export const NumberFieldInner = <
               id={id}
               error={!!error}
               aria-labelledby={cn(`${id}-label`, {
-                [`${id}-help-text`]: !!description,
+                [`${id}-help-text`]: !!tooltipText,
               })}
-              aria-describedby={description ? `${id}-label-tip` : undefined}
+              aria-describedby={tooltipText ? `${id}-label-tip` : undefined}
               isDisabled={disabled}
               {...field}
             />

app/components/form/fields/RadioField.tsx
@@ -37,7 +37,7 @@ export type RadioFieldProps<
    * complete the input. This will be announced in tandem with the
    * label when using a screen reader.
    */
-  helpText?: string
+  description?: string | React.ReactNode
   /**
    * Displayed in a tooltip beside the title. This field should be used
    * for auxiliary context that helps users understand extra context about
@@ -46,7 +46,7 @@ export type RadioFieldProps<
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-description
    */
-  description?: string
+  tooltipText?: string
   placeholder?: string
   units?: string
   control: Control<TFieldValues>
@@ -68,8 +68,8 @@ export function RadioField<
 >({
   name,
   label = capitalize(name),
-  helpText,
   description,
+  tooltipText,
   units,
   control,
   items,
@@ -81,12 +81,12 @@ export function RadioField<
     <div>
       <div className="mb-2">
         {label && (
-          <FieldLabel id={`${id}-label`} tip={description}>
+          <FieldLabel id={`${id}-label`} tip={tooltipText}>
             {label} {units && <span className="ml-1 text-secondary">({units})</span>}
           </FieldLabel>
         )}
         {/* TODO: Figure out where this hint field def should live */}
-        {helpText && <TextInputHint id={`${id}-help-text`}>{helpText}</TextInputHint>}
+        {description && <TextInputHint id={`${id}-help-text`}>{description}</TextInputHint>}
       </div>
       <Controller
         name={name}
@@ -95,9 +95,9 @@ export function RadioField<
           <RadioGroup
             defaultChecked={value}
             aria-labelledby={cn(`${id}-label`, {
-              [`${id}-help-text`]: !!description,
+              [`${id}-help-text`]: !!tooltipText,
             })}
-            aria-describedby={description ? `${id}-label-tip` : undefined}
+            aria-describedby={tooltipText ? `${id}-label-tip` : undefined}
             onChange={(e) =>
               parseValue ? onChange(parseValue(e.target.value)) : onChange(e)
             }
@@ -137,8 +137,8 @@ export function RadioFieldDyn<
 >({
   name,
   label = capitalize(name),
-  helpText,
   description,
+  tooltipText,
   units,
   control,
   children,
@@ -149,12 +149,12 @@ export function RadioFieldDyn<
     <div>
       <div className="mb-2">
         {label && (
-          <FieldLabel id={`${id}-label`} tip={description}>
+          <FieldLabel id={`${id}-label`} tip={tooltipText}>
             {label} {units && <span className="ml-1 text-secondary">({units})</span>}
           </FieldLabel>
         )}
         {/* TODO: Figure out where this hint field def should live */}
-        {helpText && <TextInputHint id={`${id}-help-text`}>{helpText}</TextInputHint>}
+        {description && <TextInputHint id={`${id}-help-text`}>{description}</TextInputHint>}
       </div>
       <Controller
         name={name}
@@ -163,9 +163,9 @@ export function RadioFieldDyn<
           <RadioGroup
             defaultChecked={value}
             aria-labelledby={cn(`${id}-label`, {
-              [`${id}-help-text`]: !!description,
+              [`${id}-help-text`]: !!tooltipText,
             })}
-            aria-describedby={description ? `${id}-label-tip` : undefined}
+            aria-describedby={tooltipText ? `${id}-label-tip` : undefined}
             onChange={onChange}
             name={name}
             {...props}

commit f3bb44b9131a47e03d13947f93cfbd8023de9948

Fix transit IPs link + link styling in TextInputHint (#3111)
Saw this while working on something else — didn't like the link text but
also noticed that the links highlight when you hover anywhere in the
hint text. This styling bug was introduced in #1898 with the user data
field on instance create. The fix is to put the hover and underline
styling on the link directly.

### Before

<img width="469" height="179" alt="Screenshot 2026-03-06 at 3 46 00 PM"
src="https://github.com/user-attachments/assets/a88338cc-631f-4748-8d9e-5e1f10a79eab"
/>

### After

<img width="468" height="180" alt="Screenshot 2026-03-06 at 3 44 37 PM"
src="https://github.com/user-attachments/assets/7774950c-faa5-4df9-b44e-b0c9c9f5c3fb"
/>

app/forms/instance-create.tsx
@@ -77,7 +77,7 @@ import { PageHeader, PageTitle } from '~/ui/lib/PageHeader'
 import { RadioCard } from '~/ui/lib/Radio'
 import { Slash } from '~/ui/lib/Slash'
 import { Tabs } from '~/ui/lib/Tabs'
-import { TextInputHint } from '~/ui/lib/TextInput'
+import { HintLink, TextInputHint } from '~/ui/lib/TextInput'
 import { TipIcon } from '~/ui/lib/TipIcon'
 import { Tooltip } from '~/ui/lib/Tooltip'
 import { Wrap } from '~/ui/util/wrap'
@@ -1122,12 +1122,8 @@ const PRESETS = [
 const UserDataDescription = () => (
   <>
     Data or scripts to be passed to cloud-init as{' '}
-    <a href={links.cloudInitFormat} target="_blank" rel="noreferrer">
-      user data
-    </a>{' '}
-    <a href={links.cloudInitExamples} target="_blank" rel="noreferrer">
-      (examples)
-    </a>{' '}
-    if the selected boot image supports it. Maximum size 32 KiB.
+    <HintLink href={links.cloudInitFormat}>user data</HintLink>{' '}
+    <HintLink href={links.cloudInitExamples}>(examples)</HintLink> if the selected boot
+    image supports it. Maximum size 32 KiB.
   </>
 )

app/forms/network-interface-edit.tsx
@@ -7,6 +7,7 @@
  */
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import { match } from 'ts-pattern'
 
 import {
   api,
@@ -28,7 +29,7 @@ import { FieldLabel } from '~/ui/lib/FieldLabel'
 import { Message } from '~/ui/lib/Message'
 import { ClearAndAddButtons, MiniTable } from '~/ui/lib/MiniTable'
 import { SideModalFormDocs } from '~/ui/lib/ModalLinks'
-import { TextInputHint } from '~/ui/lib/TextInput'
+import { HintLink, TextInputHint } from '~/ui/lib/TextInput'
 import { KEYS } from '~/ui/util/keys'
 import { parseIpNet, validateIpNet } from '~/util/ip'
 import { docLinks, links } from '~/util/links'
@@ -70,16 +71,14 @@ export function EditNetworkInterfaceForm({
 
   // Determine what IP versions this NIC supports
   const { ipStack } = editing
-  const supportsV4 = ipStack.type === 'v4' || ipStack.type === 'dual_stack'
-  const supportsV6 = ipStack.type === 'v6' || ipStack.type === 'dual_stack'
-  const supportedVersions =
-    supportsV4 && supportsV6 ? 'both IPv4 and IPv6' : supportsV4 ? 'IPv4' : 'IPv6'
-  const exampleIPs =
-    supportsV4 && supportsV6
-      ? '192.168.0.0/16 or fd00::/64'
-      : supportsV4
-        ? '192.168.0.0/16'
-        : 'fd00::/64'
+  const { supportedVersions, exampleIPs } = match(ipStack.type)
+    .with('v4', () => ({ supportedVersions: 'IPv4', exampleIPs: '192.168.0.0/16' }))
+    .with('v6', () => ({ supportedVersions: 'IPv6', exampleIPs: 'fd00::/64' }))
+    .with('dual_stack', () => ({
+      supportedVersions: 'both IPv4 and IPv6',
+      exampleIPs: '192.168.0.0/16 or fd00::/64',
+    }))
+    .exhaustive()
 
   const transitIpsForm = useForm({ defaultValues: { transitIp: '' } })
   const transitIpValue = transitIpsForm.watch('transitIp')
@@ -123,11 +122,8 @@ export function EditNetworkInterfaceForm({
             Transit IPs
           </FieldLabel>
           <TextInputHint id="transitIp-help-text" className="mb-2">
-            An IP network, like {exampleIPs}.
-            <br />
-            <a href={links.transitIpsDocs} target="_blank" rel="noreferrer">
-              Learn more about transit IPs.
-            </a>
+            These allow an instance to opt into traffic from a wider address range. Learn
+            more in the <HintLink href={links.transitIpsDocs}>Networking</HintLink> guide.
           </TextInputHint>
           <TextFieldInner
             id="transitIp"
@@ -143,18 +139,18 @@ export function EditNetworkInterfaceForm({
               const error = validateIpNet(value)
               if (error) return error
 
-              // Check if Transit IP version matches NIC's supported versions
+              // Check if transit IP version matches NIC's stack type
               const parsed = parseIpNet(value)
-              if (parsed.type === 'v4' && !supportsV4) {
+              if (parsed.type === 'v4' && ipStack.type === 'v6') {
                 return 'IPv4 transit IP not supported by this network interface'
               }
-              if (parsed.type === 'v6' && !supportsV6) {
+              if (parsed.type === 'v6' && ipStack.type === 'v4') {
                 return 'IPv6 transit IP not supported by this network interface'
               }
 
               if (transitIps.includes(value)) return 'Transit IP already in list'
             }}
-            placeholder="Enter an IP network"
+            placeholder={`An IP network, e.g., ${exampleIPs}`}
           />
         </div>
         <ClearAndAddButtons

app/ui/lib/TextInput.tsx
@@ -111,15 +111,18 @@ type HintProps = {
  * Pass id here and include that ID in aria-describedby on the TextField
  */
 export const TextInputHint = ({ id, children, className }: HintProps) => (
-  <div
-    id={id}
-    className={cn(
-      'text-sans-sm text-secondary hover:[&_>_a]:text-raise mt-1 [&_>_a]:underline',
-      className
-    )}
-  >
+  <div id={id} className={cn('text-sans-sm text-secondary mt-1', className)}>
     {children}
   </div>
+)
+
+export type HintLinkProps = { href: string; children: React.ReactNode }
+
+/** External link styled for use inside a TextInputHint */
+export const HintLink = ({ href, children }: HintLinkProps) => (
+  <a href={href} target="_blank" rel="noreferrer" className="hover:text-raise underline">
+    {children}
+  </a>
 )
 
 export const TextInputError = ({ children }: { children: string }) => {