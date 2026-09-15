4ca8bb37120e43a53bf01df5b9625fbeb7caab53
benjaminleonard <benji@oxide.computer>
2026-09-15 10:24:31 +0100

    Use 16 icon for empty state

--- app/pages/system/SupportBundlesPage.tsx
+++ app/pages/system/SupportBundlesPage.tsx
@@ -51,7 +51,7 @@ import {
 
 const EmptyState = () => (
   <EmptyMessage
-    icon={<Archive24Icon />}
+    icon={<Archive16Icon />}
     title="No support bundles"
     body="Create a support bundle to see it here"
     buttonText="New support bundle"
